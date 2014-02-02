617a54909256ea3295c69bc4b6bcc1d699b77b5c
volkan <volkanceylan@gmail.com>
2014-02-02 23:35:34 +0200

    samples klasörü eklendi.

---
+++ Documentation/Samples/SqlQuery/Sample1_Listing5_CrossJoin.cs
@@ -0,0 +1,20 @@
+﻿namespace Samples
+{
+    using Serenity;
+    using Serenity.Data;
+
+    public partial class SqlQuerySamples
+    {
+        public static string Sample1_Listing5_CrossJoin()
+        {
+            return new SqlQuery()
+                .Select("Firstname")
+                .Select("Surname")
+                .From("People")
+                .From("City")
+                .From("Country")
+                .OrderBy("p.Age")
+                .ToString();
+        }
+    }
+}
