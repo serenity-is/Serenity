51f3543513129652a4db6530cec2b5c6351c73e2
volkan.ceylan <volkanceylan@gmail.com>
2014-02-22 15:43:57 +0200

    fix namespace

--- Serenity.Core/Services/AuditLog/AuditUpdateRequest.cs
+++ Serenity.Core/Services/AuditLog/AuditUpdateRequest.cs
@@ -5,31 +5,34 @@ using Serenity.Data;
 using Serenity.Services;
 using EntityType = System.String;
 
-public class AuditUpdateRequest
+namespace Serenity.Services
 {
-    public EntityType EntityType { get; private set; }
-    public IIdRow OldEntity { get; private set; }
-    public IIdRow NewEntity { get; private set; }
-    public Field[] AuditFields { get; private set; }
-    public EntityType ParentTypeId { get; set; }
-    public Int64? OldParentId { get; set; }
-    public Int64? NewParentId { get; set; }
-    public AuditFileFieldInfo[] FileFieldInfos { get; set; }
-    public string FileSubFolder { get; set; }
-    public ICollection<string> FilesToDelete { get; set; }
-
-    public AuditUpdateRequest(EntityType entityType, IIdRow oldEntity, IIdRow newEntity, Field[] auditFields)
+    public class AuditUpdateRequest
     {
-        if (oldEntity == null)
-            throw new ArgumentNullException("oldEntity");
-        if (newEntity == null)
-            throw new ArgumentNullException("newEntity");
-        if (auditFields == null)
-            throw new ArgumentNullException("auditFields");
+        public EntityType EntityType { get; private set; }
+        public IIdRow OldEntity { get; private set; }
+        public IIdRow NewEntity { get; private set; }
+        public Field[] AuditFields { get; private set; }
+        public EntityType ParentTypeId { get; set; }
+        public Int64? OldParentId { get; set; }
+        public Int64? NewParentId { get; set; }
+        public AuditFileFieldInfo[] FileFieldInfos { get; set; }
+        public string FileSubFolder { get; set; }
+        public ICollection<string> FilesToDelete { get; set; }
 
-        EntityType = entityType;
-        OldEntity = oldEntity;
-        NewEntity = newEntity;
-        AuditFields = auditFields;
+        public AuditUpdateRequest(EntityType entityType, IIdRow oldEntity, IIdRow newEntity, Field[] auditFields)
+        {
+            if (oldEntity == null)
+                throw new ArgumentNullException("oldEntity");
+            if (newEntity == null)
+                throw new ArgumentNullException("newEntity");
+            if (auditFields == null)
+                throw new ArgumentNullException("auditFields");
+
+            EntityType = entityType;
+            OldEntity = oldEntity;
+            NewEntity = newEntity;
+            AuditFields = auditFields;
+        }
     }
 }
