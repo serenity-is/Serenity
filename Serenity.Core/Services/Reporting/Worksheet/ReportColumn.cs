bcb73808aa5350a15587d6ddc63c8dbda8961c56
yasemin.kaymak <yasemin.kaymak@marmara.edu.tr>
2014-03-28 16:05:34 +0200

    decorator types added

--- Serenity.Core/Services/Reporting/Worksheet/ReportColumn.cs
+++ Serenity.Core/Services/Reporting/Worksheet/ReportColumn.cs
@@ -1,5 +1,6 @@
 ﻿using Serenity.Data;
 using System;
+using System.Drawing;
 
 namespace Serenity.Reporting
 {
@@ -11,5 +12,42 @@ namespace Serenity.Reporting
         public Type DataType { get; set; }
         public string Format { get; set; }
         public bool WrapText { get; set; }
+        public ICellDecorator Decorator { get; set; }
     }
+
+    public interface ICellDecorator
+    {
+        object Item { get; set; }
+        string Name { get; set; }
+
+        object Value { get; set; }
+        Color Background { get; set; }
+        Color Foreground { get; set; }
+        string Format { get; set; }
+
+        void Decorate();
+    }
+
+    public abstract class BaseCellDecorator : ICellDecorator
+    {
+        public object Item { get; set; }
+        public string Name { get; set; }
+        public object Value { get; set; }
+        public Color Background { get; set; }
+        public Color Foreground { get; set; }
+        public string Format { get; set; }
+
+        public abstract void Decorate();
+    }
+
+    public class CellDecoratorAttribute : Attribute
+    {
+        public CellDecoratorAttribute(Type decorator)
+        {
+            this.DecoratorType = decorator;
+        }
+
+        public Type DecoratorType { get; set; }
+    }
+
 }
