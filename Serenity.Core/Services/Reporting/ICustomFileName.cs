d6ef99c50c161b7602aa5dc39588c43b31385804
volkan.ceylan <volkan.ceylan@gmail.com>
2014-04-22 11:49:37 +0300

    custom file name support for reports

---
+++ Serenity.Core/Services/Reporting/ICustomFileName.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Serenity.Reporting
+{
+    public interface ICustomFileName : IReport
+    {
+        string GetFileName();
+    }
+}
