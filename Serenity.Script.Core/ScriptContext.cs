9e852e165f66a72e408ea6ac2f64c453d7a9fc70
Volkan Ceylan <volkanceylan@gmail.com>
2014-05-16 15:10:05 +0300

    update toastr and newtonsoft json

--- Serenity.Script.Core/ScriptContext.cs
+++ Serenity.Script.Core/ScriptContext.cs
@@ -4,16 +4,22 @@
     using System.Runtime.CompilerServices;
 
     /// <summary>
-    /// Provides a shortcut to `jQuery.Select` (`$`) function as `J`
+    /// Base class that provides a shortcut to `jQuery.Select` (`$`) function as `J`
     /// </summary>
     public class ScriptContext
     {
+        /// <summary>
+        /// Calls jQuery function
+        /// </summary>
         [InlineCode("$({p})")]
         protected static jQueryObject J(object p)
         {
             return null;
         }
 
+        /// <summary>
+        /// Calls jQuery function with given context
+        /// </summary>
         [InlineCode("$({p}, {context})")]
         protected static jQueryObject J(object p, object context)
         {
--- Serenity.Script.UI/Widget/Widget.cs
+++ Serenity.Script.UI/Widget/Widget.cs
@@ -3,13 +3,29 @@ using System;
 
 namespace Serenity
 {
+    /// <summary>
+    /// Base class for classes that add behaviour to HTML elements. Similar to jQuery UI widget factory.
+    /// </summary>
     public abstract class Widget : ScriptContext
     {
+        /// <summary>
+        /// Each widget instance created gets a unique name. This static auto incrementing number is used to generate such names.
+        /// </summary>
         private static int NextWidgetNumber = 0;
+
         protected string widgetName;
         protected string uniqueName;
         protected jQueryObject element;
 
+        /// <summary>
+        /// Creates a widget on given element. Widget gets a unique name like MyNamespace_MyWidget1234.
+        /// </summary>
+        /// <remarks>
+        /// * A data value is added to element with this unique name as key, and the widget object as value.
+        /// * Only one widget instance of same class can be created on a element.
+        /// * All events attached by this widget class will be unbound when element is removed from document.
+        /// * Elements gets a css class like "s-MyWidget" (it has no namespace by default, but this can be customized)
+        /// </remarks>
         protected Widget(jQueryObject element)
         {
             this.element = element;
@@ -28,6 +44,9 @@ namespace Serenity
             OnInit();
         }
 
+        /// <summary>
+        /// Destroys the widget. Called automatically when the widget element is removed from document
+        /// </summary>
         public virtual void Destroy()
         {
             this.element.RemoveClass("s-" + this.GetType().Name);
@@ -38,25 +57,42 @@ namespace Serenity
             element = null;
         }
 
+        /// <summary>
+        /// Override this to change CSS class that is added to element.
+        /// </summary>
         protected virtual void AddCssClass()
         {
             this.element.AddClass("s-" + this.GetType().Name);
         }
 
+        /// <summary>
+        /// Can be overridden to do something after widget constructor (only the base one not derived) is run
+        /// </summary>
         protected virtual void OnInit()
         {
         }
 
+        /// <summary>
+        /// The element widget is atteched to
+        /// </summary>
         public jQueryObject Element 
         { 
             get { return element; } 
         }
 
+        /// <summary>
+        /// Unique name for this widget instance. Something like MyNamespace_MyWidget1234
+        /// </summary>
         public string UniqueName
         {
             get { return uniqueName; }
         }
 
+        /// <summary>
+        /// Widget name for this widget class. Element has a data value with this widget name as key,
+        /// and widget instance as value. It can be used to access widget instance given the
+        /// element.
+        /// </summary>
         public string WidgetName
         {
             get { return widgetName; }
