6d304c68682398fbc222241d0fa3a791cd45b603
volkan.ceylan <volkan.ceylan@gmail.com>
2014-09-19 20:56:56 +0300

    add lookupeditor type

---
+++ Serenity.Script.UI/Editor/LookupEditor.cs
@@ -0,0 +1,21 @@
+﻿using jQueryApi;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Serenity
+{
+    [Editor, OptionsType(typeof(LookupEditorOptions))]
+    public class LookupEditor : LookupEditorBase<LookupEditorOptions, object>
+    {
+        public LookupEditor(jQueryObject hidden, LookupEditorOptions opt)
+            : base(hidden, opt)
+        {
+        }
+    }
+
+    [Serializable, Reflectable]
+    public class LookupEditorOptions
+    {
+        public string LookupKey { get; set; }
+    }
+}
