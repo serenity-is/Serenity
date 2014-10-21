cf158e27fe1046f5ab667219fdf7f32efa978ba6
Victor Tomaili <victor.tomaili@marmara.edu.tr>
2014-10-21 14:10:58 +0300

    Override ServiceModelBinder for skip interfaces

---
+++ Serenity.Web/Mvc/ServiceModelBinder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Serenity.Services
+{
+    public class ServiceModelBinder : DefaultModelBinder
+    {
+        protected override object CreateModel(ControllerContext controllerContext, ModelBindingContext bindingContext, Type modelType)
+        {
+            if (modelType.IsInterface)
+                return null;
+
+            return base.CreateModel(controllerContext, bindingContext, modelType);
+        }
+    }
+}
