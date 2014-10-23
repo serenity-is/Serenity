d1df49234928732e4ee11c1e7c549bd3315039c5
volkan.ceylan <volkan.ceylan@gmail.com>
2014-10-23 13:35:14 +0300

    add dependency comments and tests

--- Serenity.Core/Dependency/Dependency.cs
+++ Serenity.Core/Dependency/Dependency.cs
@@ -2,20 +2,73 @@
 {
     using System;
     
+    /// <summary>
+    /// Service locator for Serenity. It requires setting an IoC container 
+    /// that implements IDependencyResolver interface through SetResolver 
+    /// method to operate normally.
+    /// </summary>
     public static class Dependency
     {
         private static IDependencyResolver resolver;
 
-        public static TType Resolve<TType>() where TType : class
+        /// <summary>
+        /// Maps TService service to its registered provider.
+        /// </summary>
+        /// <typeparam name="TService">Service type</typeparam>
+        /// <exception cref="System.KeyNotFoundException">
+        /// No provider is registered for TService</exception>
+        /// <exception cref="System.InvalidProgramException">
+        /// No dependency resolver is configured using SetResolver</exception>
+        public static TService Resolve<TService>() where TService : class
         {
-            return Resolver.Resolve<TType>();
+            return Resolver.Resolve<TService>();
         }
 
-        public static TType Resolve<TType>(string name) where TType : class
+        /// <summary>
+        /// Maps TService service to its registered provider for specified scope.
+        /// Use this method when an interface can be handled by different providers
+        /// in different scopes (e.g. Application / Server configuration scope)
+        /// </summary>
+        /// <typeparam name="TService">Service type</typeparam>
+        /// <param name="name">Scope name</param>
+        /// <exception cref="System.KeyNotFoundException">
+        /// No provider is registered for TService</exception>
+        /// <exception cref="System.InvalidProgramException">
+        /// No dependency resolver is configured using SetResolver</exception>
+        public static TService Resolve<TService>(string name) where TService : class
         {
-            return Resolver.Resolve<TType>(name);
+            return Resolver.Resolve<TService>(name);
         }
 
+        /// <summary>
+        /// Maps TService service to its registered provider. 
+        /// Returns null if registration for TService doesn't exist or 
+        /// no dependency resolver is configured using SetResolver.
+        /// </summary>
+        /// <typeparam name="TService">Service type</typeparam>
+        public static TService TryResolve<TService>() where TService : class
+        {
+            return resolver == null ? null : Resolver.TryResolve<TService>();
+        }
+
+        /// <summary>
+        /// Maps TService service to its registered provider for specified scope.
+        /// Returns null if registration for TService doesn't exist or 
+        /// no dependency resolver is configured using SetResolver.
+        /// Use this method when an interface can be handled by different providers
+        /// in different scopes (e.g. Application / Server configuration scope)
+        /// </summary>
+        /// <typeparam name="TService">Service type</typeparam>
+        /// <param name="name">Scope name</param>
+        public static TService TryResolve<TService>(string name) where TService : class
+        {
+            return resolver == null ? null : Resolver.TryResolve<TService>(name);
+        }
+
+        /// <summary>
+        /// Sets current dependency resolver and returns previous one if exists.
+        /// </summary>
+        /// <param name="value">Dependency resolver</param>
         public static IDependencyResolver SetResolver(IDependencyResolver value)
         {
             var old = resolver;
@@ -23,21 +76,21 @@
             return old;
         }
 
-        public static TType TryResolve<TType>() where TType : class
-        {
-            return resolver == null ? null : Resolver.TryResolve<TType>();
-        }
-
-        public static TType TryResolve<TType>(string name) where TType : class
-        {
-            return resolver == null ? null : Resolver.TryResolve<TType>(name);
-        }
-
+        /// <summary>
+        /// Returns true if a dependency resolver is set through SetResolver.
+        /// Use this property to check if there is a current resolver as Resolver 
+        /// property raises an exception if not.
+        /// </summary>
         public static bool HasResolver
         {
             get { return resolver != null; }
         }
 
+        /// <summary>
+        /// Returns currently registered IDependencyResolver implementation.
+        /// </summary>
+        /// <exception cref="System.InvalidProgramException">
+        /// No dependency resolver is configured using SetResolver</exception>
         public static IDependencyResolver Resolver
         {
             get
