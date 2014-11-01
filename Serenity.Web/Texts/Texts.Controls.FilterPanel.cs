5cbd7efba19411cf7a69cc7f1d37db4a1474dd28
volkan.ceylan <volkanceylan@gmail.com>
2014-11-01 21:35:44 +0200

    translate filter panel texts to english

--- Serenity.Web/Texts/Texts.Controls.FilterPanel.cs
+++ Serenity.Web/Texts/Texts.Controls.FilterPanel.cs
@@ -6,61 +6,61 @@
         {
             public static class FilterPanel
             {
-                public static LocalText AddFilter = "filtre koşulu ekle";
-                public static LocalText All = "<tümü>";
-                public static LocalText And = "ve";
-                public static LocalText AndInParens = "(ve)";
-                public static LocalText ChangeAndOr = "VE/VEYA arasında geçiş yapmak için tıklayın";
-                public static LocalText CurrentFilter = "Etkin Filtre: {0}";
-                public static LocalText SelectField = "---alan seçiniz---";
-                public static LocalText RemoveField = "bu koşulu sil";
-                public static LocalText InvalidOperator = "hatalı operatör";
-                public static LocalText InvalidDate = "geçersiz tarih!";
-                public static LocalText InvalidNumber = "geçersiz sayı!";
-                public static LocalText Or = "veya";
-                public static LocalText OrInParens = "(veya)";
-                public static LocalText ResetButton = "filtreyi temizle";
-                public static LocalText SearchButton = "filtreyi uygula";
-                public static LocalText ValueRequired = "alan için değer girilmeli!";
-                public static LocalText GroupBy = "--grupla--";
-                public static LocalText ThenBy = "--ve grupla--";
-                public static LocalText ApplyGroups = "gruplamayı uygula";
-                public static LocalText ClearGroups = "gruplamayı temizle";
+                public static LocalText AddFilter = "add criteria";
+                public static LocalText All = "<all>";
+                public static LocalText And = "and";
+                public static LocalText AndInParens = "(and)";
+                public static LocalText ChangeAndOr = "click to switch between AND/OR";
+                public static LocalText CurrentFilter = "Active Filter: {0}";
+                public static LocalText SelectField = "---select field---";
+                public static LocalText RemoveField = "remove criteria";
+                public static LocalText InvalidOperator = "invalid operator";
+                public static LocalText InvalidDate = "invalid date!";
+                public static LocalText InvalidNumber = "invalid number!";
+                public static LocalText Or = "or";
+                public static LocalText OrInParens = "(or)";
+                public static LocalText ResetButton = "clear filter";
+                public static LocalText SearchButton = "apply filter";
+                public static LocalText ValueRequired = "value required!";
+                public static LocalText GroupBy = "--group--";
+                public static LocalText ThenBy = "--then by--";
+                public static LocalText ApplyGroups = "apply grouping";
+                public static LocalText ClearGroups = "clear grouping";
 
                 public static class OperatorNames
                 {
-                    public static LocalText @true = "evet";
-                    public static LocalText @false = "hayır";
-                    public static LocalText contains = "içerir";
-                    public static LocalText startswith = "başlar";
-                    public static LocalText eq = "eşit";
-                    public static LocalText ne = "farklı";
-                    public static LocalText gt = "büyük";
-                    public static LocalText ge = "büyük eşit";
-                    public static LocalText lt = "küçük";
-                    public static LocalText le = "küçük eşit";
-                    public static LocalText bw = "arasında";
-                    public static LocalText @in = "içinden biri";
-                    public static LocalText isnull = "boş";
-                    public static LocalText isnotnull = "boş değil";
+                    public static LocalText @true = "yes";
+                    public static LocalText @false = "no";
+                    public static LocalText contains = "contains";
+                    public static LocalText startswith = "start with";
+                    public static LocalText eq = "equal";
+                    public static LocalText ne = "not equal";
+                    public static LocalText gt = "greater than";
+                    public static LocalText ge = "greater than or equal";
+                    public static LocalText lt = "less than";
+                    public static LocalText le = "less than or equal";
+                    public static LocalText bw = "between";
+                    public static LocalText @in = "in";
+                    public static LocalText isnull = "is null";
+                    public static LocalText isnotnull = "is not null";
                 }
 
                 public static class OperatorFormats
                 {
-                    public static LocalText @true = "{0} = evet";
-                    public static LocalText @false = "{0} = hayır";
-                    public static LocalText contains = "{0}, '{1}' içeren";
-                    public static LocalText startswith = "{0}, '{1}' ile başlayan";
+                    public static LocalText @true = "{0} = yes";
+                    public static LocalText @false = "{0} = no";
+                    public static LocalText contains = "{0} contains '{1}'";
+                    public static LocalText startswith = "{0} starts with '{1}'";
                     public static LocalText eq = "{0} = {1}";
                     public static LocalText ne = "{0} <> {1}";
                     public static LocalText gt = "{0} > {1}";
                     public static LocalText ge = "{0} >= {1}";
                     public static LocalText lt = "{0} < {1}";
                     public static LocalText le = "{0} <= {1}";
-                    public static LocalText bw = "{0} {1} ile {2} arasında";
-                    public static LocalText @in = "{0}, [{1}] içinden biri";
-                    public static LocalText isnull = "{0} boş";
-                    public static LocalText isnotnull = "{0} boş değil";
+                    public static LocalText bw = "{0} is between {1} and {2}";
+                    public static LocalText @in = "{0} is one of [{1}]";
+                    public static LocalText isnull = "{0} is null";
+                    public static LocalText isnotnull = "{0} is not null";
                 }
             }
         }
