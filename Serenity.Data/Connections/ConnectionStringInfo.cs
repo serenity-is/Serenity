9dd32297def6ed5224933f5acf9f40c1ce4695dd
volkan.ceylan <volkan.ceylan@gmail.com>
2014-11-04 11:34:03 +0200

    add obsolete Item1 and Item2 properties to ConnectionStringInfo for backward compability

--- Serenity.Data/Connections/ConnectionStringInfo.cs
+++ Serenity.Data/Connections/ConnectionStringInfo.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Data.Common;
 namespace Serenity.Data
 {
@@ -14,5 +15,10 @@ namespace Serenity.Data
         public string ConnectionString { get; private set; }
         public string ProviderName { get; private set; }
         public DbProviderFactory ProviderFactory { get; private set; }
+
+        [Obsolete("Use ConnectionString")]
+        public string Item1 { get { return ConnectionString; } }
+        [Obsolete("Use ProviderName")]
+        public string Item2 { get { return ProviderName; } }
     }
 }
