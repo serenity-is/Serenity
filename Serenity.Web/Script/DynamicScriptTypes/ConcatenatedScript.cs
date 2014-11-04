65c35f9f25f95e7b021a4e0f16707ec7c3ddcd7a
volkan.ceylan <volkan.ceylan@gmail.com>
2014-11-04 16:26:48 +0200

    remove unused minimize functionality from ConcatenatedScript

--- Serenity.Web/Script/DynamicScriptTypes/ConcatenatedScript.cs
+++ Serenity.Web/Script/DynamicScriptTypes/ConcatenatedScript.cs
@@ -7,30 +7,15 @@ namespace Serenity.Web
 {
     public class ConcatenatedScript : IDynamicScript
     {
-        private bool minimize;
         private EventHandler scriptChanged;
         private IEnumerable<Func<string>> scriptParts;
 
-        public ConcatenatedScript(
-            IEnumerable<Func<string>> scriptParts)
+        public ConcatenatedScript(IEnumerable<Func<string>> scriptParts)
         {
             if (scriptParts == null)
                 throw new ArgumentNullException("scriptParts");
 
             this.scriptParts = scriptParts;
-        }
-
-        public bool Minimize
-        {
-            get { return minimize; }
-            set 
-            {
-                if (minimize != value)
-                {
-                    minimize = value;
-                    Changed();
-                }
-            }
         }
 
         public string GetScript()
@@ -40,18 +25,6 @@ namespace Serenity.Web
             foreach (var part in scriptParts)
             {
                 string partSource = part();
-
-                /*if (minimize)
-                {
-                    try
-                    {
-                        partSource = JavaScriptMinifier.Minify(partSource);
-                    }
-                    catch
-                    {
-                        // ignore minification errors
-                    }
-                }*/
 
                 sb.AppendLine(partSource);
                 sb.AppendLine("\r\n");
