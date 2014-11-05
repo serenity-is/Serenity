dfe59008205c344cd959f67e31d268e2b07349bf
volkan <volkanceylan@gmail.com>
2014-11-05 22:17:31 +0200

    start enum local texts tests

---
+++ Serenity.Test/Core/Localization/EnumLocalTextsTests.cs
@@ -0,0 +1,63 @@
+﻿using FakeItEasy;
+using Serenity.Abstractions;
+using Serenity.ComponentModel;
+using Serenity.Localization;
+using Serenity.Testing;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xunit;
+
+namespace Serenity.Test
+{
+    public partial class EnumLocalTextsTests
+    {
+        [EnumKey("My.CoolEnumKey")]
+        public enum EnumWithKey
+        {
+            NoDescriptionKey,
+            [Description("Description for WithDescriptionKey")]
+            WithDescriptionKey
+        }
+
+        public enum EnumWithoutKey
+        {
+            [Description("Description for WithDescriptionNoKey")]
+            WithDescriptionNoKey,
+            NoDescriptionNoKey,
+        }
+
+        [Fact]
+        public void EnumLocalTexts_Initialize_ThrowsException_IfNoLocalTextRegistryIsRegistered()
+        {
+            using (new MunqContext())
+            {
+                var exception = Assert.Throws<KeyNotFoundException>(() => 
+                    EnumLocalTexts.Initialize(new[] { this.GetType().Assembly }));
+
+                Assert.Contains(typeof(ILocalTextRegistry).Name, exception.Message);
+            }
+        }
+
+        [Fact]
+        public void EnumLocalTexts_Initialize_UsesRegisteredLocalTextRegistry()
+        {
+            using (new MunqContext())
+            {
+                var registry = A.Fake<ILocalTextRegistry>();
+
+                Dependency.Resolve<IDependencyRegistrar>()
+                    .RegisterInstance(registry);
+
+                EnumLocalTexts.Initialize(new[] { this.GetType().Assembly });
+
+                A.CallTo(() => registry.Add(A<string>._, A<string>._, A<string>._))
+                    .MustHaveHappened(Repeated.AtLeast.Twice);
+            }
+        }
+
+        [Fact]
+        public void EnumLocalTexts_Initialize_SearchesOnlyGivenAssemblies()
+        {
+        }
+    }
+}
