3acdcac20737cb28837f70569278950239caf4bd
volkan.ceylan <volkan.ceylan@gmail.com>
2014-11-05 14:37:59 +0200

    LookupEditor is no longer async by default (user can subclass if needed)

--- Serenity.Script.UI/Editor/LookupEditor.cs
+++ Serenity.Script.UI/Editor/LookupEditor.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 namespace Serenity
 {
     [Editor, OptionsType(typeof(LookupEditorOptions))]
-    public class LookupEditor : LookupEditorBase<LookupEditorOptions, object>, IAsyncInit
+    public class LookupEditor : LookupEditorBase<LookupEditorOptions, object>
     {
         public LookupEditor(jQueryObject hidden, LookupEditorOptions opt)
             : base(hidden, opt)
