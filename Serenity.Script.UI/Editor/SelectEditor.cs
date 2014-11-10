d587a60fdf14d1ad873e7dfeac3752a80efb2365
volkan.ceylan <volkan.ceylan@gmail.com>
2014-11-10 11:46:37 +0200

    fix mixed files

--- Serenity.Script.UI/Editor/SelectEditor.cs
+++ Serenity.Script.UI/Editor/SelectEditor.cs
@@ -7,38 +7,58 @@ using System.Runtime.CompilerServices;
 
 namespace Serenity
 {
-    [Editor, DisplayName("Enumeration"), OptionsType(typeof(EnumEditorOptions))]
+    [Editor, DisplayName("Açılır Liste"), OptionsType(typeof(SelectEditorOptions))]
     [Element("<input type=\"hidden\"/>")]
-    public class EnumEditor : Select2Editor<EnumEditorOptions, Select2Item>, IStringValue
+    public class SelectEditor : Select2Editor<SelectEditorOptions, Select2Item>, IStringValue
     {
-        public EnumEditor(jQueryObject hidden, EnumEditorOptions opt)
+        public SelectEditor(jQueryObject hidden, SelectEditorOptions opt)
             : base(hidden, opt)
         {
             UpdateItems();
         }
 
+        protected virtual List<object> GetItems()
+        {
+            return options.Items ?? new List<object>();
+        }
+
+        protected override string EmptyItemText()
+        {
+            return options.EmptyOptionText;
+        }
+
         protected virtual void UpdateItems()
         {
+            var items = GetItems();
+            
             ClearItems();
 
-            var enumType = EnumTypeRegistry.Get(options.EnumKey);
-
-            foreach (var x in Enum.GetValues(enumType))
+            if (items.Count > 0)
             {
-                var name = Enum.ToString(enumType, x.As<Enum>());
-                AddItem(((int)x).ToString(), Q.TryGetText("Enums." +  options.EnumKey + "." + name) ?? name);
+                bool isStrings = Script.TypeOf(items[0]) == "string";
+
+                foreach (dynamic item in items)
+                {
+                    string key = isStrings ? item : item[0];
+                    string text = isStrings ? item : item[1] ?? item[0];
+                    AddItem(key, text, item);
+                }
             }
         }
     }
 
     [Serializable, Reflectable]
-    public class EnumEditorOptions
+    public class SelectEditorOptions
     {
-        public EnumEditorOptions()
+        public SelectEditorOptions()
         {
+            EmptyOptionText = "--seçiniz--";
+            Items = new List<object>();
         }
 
-        [DisplayName("Enum Type Key")]
-        public string EnumKey { get; set; }
+        [Hidden]
+        public List<object> Items { get; set; }
+        [DisplayName("Boş Eleman Metni")]
+        public string EmptyOptionText { get; set; }
     }
 }
