f4bd5c613863455fa14490b279baea3f0c4fc12e
volkan.ceylan <volkanceylan@gmail.com>
2015-01-01 18:13:05 +0200

    TryGetWidget and GetWidget works with non-widget types (e.g. interfaces) or base types now (first matching widget returned). Change method acts like ChangeSelect2 if widget element has a Select2 editor on it.

--- Serenity.Script.UI/Widget/WidgetExtensions.cs
+++ Serenity.Script.UI/Widget/WidgetExtensions.cs
@@ -7,51 +7,57 @@ namespace Serenity
     [ScriptName("WX")]
     public static class WidgetExtensions
     {
-        public static TWidget GetWidget<TWidget>(this jQueryObject element) where TWidget : Widget
-        {
-            return (TWidget)GetWidget(element, typeof(TWidget));
-        }
-
-        public static object GetWidget(this jQueryObject element, Type widgetType)
+        public static TWidget GetWidget<TWidget>(this jQueryObject element) where TWidget : class
         {
             if (element == null)
                 throw new ArgumentNullException("element");
 
             if (element.Length == 0)
-                throw new Exception(String.Format("Searching for widget of type '{0}' on a non-existent element!", GetWidgetName(widgetType)));
+                throw new Exception(String.Format("Searching for widget of type '{0}' on a non-existent element!", typeof(TWidget).FullName));
 
-            var widget = TryGetWidget(element, widgetType);
+            var widget = TryGetWidget<TWidget>(element);
             if (widget == null)
-                throw new Exception(String.Format("Element has no widget of type '{0}'!", GetWidgetName(widgetType)));
+                throw new Exception(String.Format("Element has no widget of type '{0}'!", typeof(TWidget).FullName));
 
             return widget;
         }
 
-        public static TWidget TryGetWidget<TWidget>(this jQueryObject element) where TWidget : Widget
+        [InlineCode("Serenity.WX.getWidget({widgetType})(element)")]
+        public static object GetWidget(this jQueryObject element, Type widgetType)
+        {
+            return null;
+        }
+
+        public static TWidget TryGetWidget<TWidget>(this jQueryObject element) where TWidget : class
         {
             if (element == null)
                 throw new Exception("Argument 'element' is null!");
 
-            var widgetName = WidgetExtensions.GetWidgetName(typeof(TWidget));
+            TWidget widget;
+            if (typeof(TWidget).IsAssignableFrom(typeof(Widget)))
+            {
+                var widgetName = WidgetExtensions.GetWidgetName(typeof(TWidget));
 
-            return element.GetDataValue(widgetName) as TWidget;
+                widget = element.GetDataValue(widgetName) as TWidget;
+                if (widget != null)
+                    return widget;
+            }
+
+            var data = element.GetData();
+            foreach (string key in data.Keys)
+            {
+                widget = data[key] as TWidget;
+                if (widget != null)
+                    return widget;
+            }
+
+            return null;
         }
 
+        [InlineCode("Serenity.WX.tryGetWidget({widgetType})(element)")]
         public static object TryGetWidget(this jQueryObject element, Type widgetType)
         {
-            if (widgetType == null)
-                throw new Exception("Argument 'widgetType' is null!");
-
-            if (element == null)
-                throw new Exception("Argument 'element' is null!");
-
-            var widgetName = WidgetExtensions.GetWidgetName(widgetType);
-            var widget = element.GetDataValue(widgetName);
-
-            if (widget != null && !widgetType.IsAssignableFrom(widget.GetType()))
-                return null;
-
-            return widget;
+            return null;
         }
 
         public static string GetWidgetName(Type type)
@@ -68,7 +74,18 @@ namespace Serenity
         public static void Change<TWidget>(this TWidget widget, jQueryEventHandler handler)
             where TWidget: Widget
         {
-            widget.Element.Bind("change." + widget.UniqueName, handler);
+            if (widget.Element.GetDataValue("select2") != null)
+            {
+                widget.Element.Bind2("change." + widget.UniqueName, (e, x) =>
+                {
+                    if (e.HasOriginalEvent() || Q.IsFalse(x))
+                    {
+                        handler(e);
+                    }
+                });
+            }
+            else
+                widget.Element.Bind("change." + widget.UniqueName, handler);
         }
 
         [IncludeGenericArguments(false)] // saltarelle bug ı var, değiştirme
