77ad572d66f51619821d525ea7a4eaa214b9aafd
volkan <volkanceylan@gmail.com>
2015-03-21 21:37:37 +0200

    add assemblytitle and description for serenity.caching.redis

---
+++ Serenity.Caching.Redis/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Serenity Platform - Redis Distributed Cache Implementation")]
+[assembly: AssemblyDescription("Contains RedisDistributedCache that implements IDistributedCache interface")]
+[assembly: ComVisible(false)]
