2b17f1119efa21f0e4316fbfde7084fcb98e0de7
volkan.ceylan <volkan.ceylan@gmail.com>
2015-08-19 12:26:17 +0300

    dictionary get default

--- Serenity.Core/Helpers/CollectionExtensions.cs
+++ Serenity.Core/Helpers/CollectionExtensions.cs
@@ -17,5 +17,23 @@ namespace Serenity
             foreach (T value in values)
                 list.Add(value);
         }
+
+        public static TValue Get<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key)
+        {
+            TValue value;
+            if (!dict.TryGetValue(key, out value))
+                return default(TValue);
+
+            return value;
+        }
+
+        public static TValue Get<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue defaultValue)
+        {
+            TValue value;
+            if (!dict.TryGetValue(key, out value))
+                return defaultValue;
+
+            return value;
+        }
     }
 }
