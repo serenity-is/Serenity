671b79540f6684ef47063d0f4372e4f20f761db3
volkan.ceylan <volkan.ceylan@marmara.edu.tr>
2015-10-13 00:12:46 +0300

    added CascadedWidgetLink for cascaded lookup editors

---
+++ Serenity.Script.UI/Widget/CascadedWidgetLink.cs
@@ -0,0 +1,66 @@
+﻿namespace Serenity
+{
+    using System;
+
+    public class CascadedWidgetLink<TParent>
+        where TParent: Widget
+    {
+        private Widget widget;
+        private Action<TParent> parentChange;
+
+        public CascadedWidgetLink(Widget widget, Action<TParent> parentChange)
+        {
+            this.widget = widget;
+            Bind();
+            this.widget.Element.Bind("remove." + widget.UniqueName + "cwh", (e) => {
+                Unbind();
+                this.widget = null;
+                this.parentChange = null;
+            });
+        }
+
+        public void Bind()
+        {
+            if (parentID.IsEmptyOrNull())
+                return;
+
+            var parent = Q.FindElementWithRelativeId(widget.Element, parentID).TryGetWidget<TParent>();
+            if (parent != null)
+            {
+                parent.Element.Bind("change." + widget.UniqueName, delegate
+                {
+                    parentChange(parent);
+                });
+            }
+        }
+
+        public void Unbind()
+        {
+            if (parentID.IsEmptyOrNull())
+                return;
+
+            var parent = Q.FindElementWithRelativeId(widget.Element, parentID).TryGetWidget<TParent>();
+            if (parent != null)
+                parent.Element.Unbind("." + widget.UniqueName);
+        }
+
+        private string parentID;
+
+        public string ParentID
+        {
+            get
+            {
+                return parentID;
+            }
+            set
+            {
+                if (parentID != value)
+                {
+                    Unbind();
+                    parentID = value;
+                    Bind();
+                }
+            }
+        }
+    }
+}
