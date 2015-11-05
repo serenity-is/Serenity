b68c5ce5785fb13c961fe7c78773a45d78021a91
volkan.ceylan <volkanceylan@gmail.com>
2015-11-06 00:21:40 +0200

    extract unique constraint checks from SaveRequestHandler as behaviors

---
+++ Serenity.Services/RequestHandlers/IntegratedFeatures/UniqueConstraints/UniqueConstraintSaveBehavior.cs
@@ -0,0 +1,62 @@
+﻿using Serenity.Data;
+using Serenity.Data.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Serenity.Services
+{
+    public class UniqueConstraintSaveBehavior : BaseSaveBehavior, IImplicitBehavior
+    {
+        private UniqueConstraintAttribute[] attrList;
+        private IEnumerable<Field>[] attrFields;
+
+        public bool ActivateFor(Row row)
+        {
+            var attr = row.GetType().GetCustomAttributes<UniqueConstraintAttribute>();
+
+            if (!attr.Any())
+                return false;
+
+            attr = attr.Where(x => x.CheckBeforeSave);
+            if (!attr.Any())
+                return false;
+
+            this.attrList = attr.ToArray();
+            return true;
+        }
+
+        public override void OnValidateRequest(ISaveRequestHandler handler)
+        {
+            if (attrList == null)
+                return;
+
+            if (attrFields == null)
+            {
+                attrFields = attrList.Select(attr =>
+                {
+                    return attr.Fields.Select(x =>
+                    {
+                        var field = handler.Row.FindFieldByPropertyName(x) ?? handler.Row.FindField(x);
+                        if (ReferenceEquals(null, field))
+                        {
+                            throw new InvalidOperationException(String.Format(
+                                "Can't find field '{0}' of unique constraint in row type '{1}'",
+                                    x, handler.Row.GetType().FullName));
+                        }
+                        return field;
+                    });
+                }).ToArray();
+            }
+
+            for (var i = 0; i < attrList.Length; i++)
+            {
+                var attr = attrList[i];
+                var fields = attrFields[i];
+
+                UniqueFieldSaveBehavior.ValidateUniqueConstraint(handler, fields, attr.ErrorMessage, Criteria.Empty);
+            }
+        }
+    }
+}
---
+++ Serenity.Services/RequestHandlers/IntegratedFeatures/UniqueConstraints/UniqueFieldSaveBehavior.cs
@@ -0,0 +1,73 @@
+﻿using Serenity.Data;
+using Serenity.Data.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Serenity.Services
+{
+    public class UniqueFieldSaveBehavior : BaseSaveBehavior, IImplicitBehavior, IFieldBehavior
+    {
+        public Field Target { get; set; }
+
+        private UniqueAttribute attr;
+
+        public bool ActivateFor(Row row)
+        {
+            if (ReferenceEquals(null, Target))
+                return false;
+
+            if (!Target.Flags.HasFlag(FieldFlags.Unique))
+                return false;
+
+            var attr = Target.GetAttribute<UniqueAttribute>();
+            if (attr != null && !attr.CheckBeforeSave)
+                return false;
+
+            this.attr = attr;
+            return true;
+        }
+
+        public override void OnValidateRequest(ISaveRequestHandler handler)
+        {
+            ValidateUniqueConstraint(handler, new Field[] { Target },
+                attr == null ? (string)null : attr.ErrorMessage,
+                Criteria.Empty);
+        }
+
+        internal static void ValidateUniqueConstraint(ISaveRequestHandler handler, IEnumerable<Field> fields,
+            string errorMessage = null, BaseCriteria groupCriteria = null)
+        {
+            if (handler.Old != null && !fields.Any(x => x.IndexCompare(handler.Old, handler.Row) != 0))
+                return;
+
+            var criteria = groupCriteria ?? Criteria.Empty;
+
+            foreach (var field in fields)
+                if (field.IsNull(handler.Row))
+                    criteria &= field.IsNull();
+                else
+                    criteria &= field == new ValueCriteria(field.AsObject(handler.Row));
+
+            var idField = ((IIdRow)handler.Row).IdField;
+
+            if (handler.Old != null)
+                criteria &= (Field)idField != idField[handler.Old].Value;
+
+            var row = handler.Row.CreateNew();
+            if (new SqlQuery().From(row)
+                    .Select("1")
+                    .Where(criteria)
+                    .Exists(handler.UnitOfWork.Connection))
+            { 
+                throw new ValidationError("UniqueViolation",
+                    String.Join(", ", fields.Select(x => x.PropertyName ?? x.Name)),
+                    String.Format(!string.IsNullOrEmpty(errorMessage) ?
+                        (LocalText.TryGet(errorMessage) ?? errorMessage) :
+                            LocalText.Get("Validation.UniqueConstraint"),
+                        String.Join(", ", fields.Select(x => x.Title))));
+            }
+        }
+    }
+}
