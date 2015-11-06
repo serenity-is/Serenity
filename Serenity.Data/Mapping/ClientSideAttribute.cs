5e5eaa2a81e36750e6f7d03bd4f2d4c143974a02
volkan.ceylan <volkanceylan@gmail.com>
2015-11-06 21:43:47 +0200

    add MasterDetailRelationAttribute and ClientSideAttribute

---
+++ Serenity.Data/Mapping/ClientSideAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Serenity.Data.Mapping
+{
+    public class ClientSideAttribute : SetFieldFlagsAttribute
+    {
+        public ClientSideAttribute()
+            : base(FieldFlags.ClientSide)
+        {
+        }
+    }
+}
---
+++ Serenity.Data/Mapping/MasterDetailRelationAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Serenity.Data.Mapping
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public sealed class MasterDetailRelationAttribute : Attribute
+    {
+        public MasterDetailRelationAttribute(string foreignKey)
+        {
+            this.ForeignKey = foreignKey;
+        }
+
+        public string ForeignKey { get; private set; }
+    }
+}
