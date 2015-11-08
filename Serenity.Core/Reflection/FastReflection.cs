c081ad8bab391c6998f261611af94143976bcd70
volkan.ceylan <volkanceylan@gmail.com>
2015-11-08 15:19:10 +0200

    fast reflection for constructor factory

---
+++ Serenity.Core/Reflection/FastReflection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace Serenity.Reflection
+{
+    public static class FastReflection
+    {
+        public static Func<object> DelegateForConstructor(Type type)
+        {
+            var ctor = type.GetConstructor(Type.EmptyTypes);
+
+            if (ctor == null)
+                throw new MissingMethodException(String.Format(
+                    "There is no parameterless constructor for type {0}", type));
+
+            var dm = new DynamicMethod("ctor0", type, Type.EmptyTypes);
+            ILGenerator il = dm.GetILGenerator();
+            il.DeclareLocal(type);
+            il.Emit(OpCodes.Newobj, ctor);
+            il.Emit(OpCodes.Stloc_0);
+            il.Emit(OpCodes.Ldloc_0);
+            il.Emit(OpCodes.Ret);
+
+            return (Func<object>)dm.CreateDelegate(typeof(Func<object>));
+        }
+    }
+}
