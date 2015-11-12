07d9ac6238314058b6e6b093883ca999bfa8b70d
volkan.ceylan <volkanceylan@gmail.com>
2015-11-12 17:42:09 +0200

    RegisterPermissionAttribute

---
+++ Serenity.Core/ComponentModel/Permission/RegisterPermissionAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Serenity.Data;
+using System;
+
+namespace Serenity.ComponentModel
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public class RegisterPermissionKeyAttribute : PermissionAttributeBase
+    {
+        public RegisterPermissionKeyAttribute(object permission)
+            : base(permission)
+        {
+        }
+
+        public RegisterPermissionKeyAttribute(object module, object permission)
+            : base(module, permission)
+        {
+        }
+
+        public RegisterPermissionKeyAttribute(object module, object submodule, object permission)
+            : base(module, submodule, permission)
+        {
+        }
+    }
+}
