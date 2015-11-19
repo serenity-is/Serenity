9cb7b116f713f520eaaa0a692ccfbd37b247b864
volkan.ceylan <volkanceylan@gmail.com>
2015-11-19 22:57:15 +0200

    add PropertyPanel and TemplatedPanel

---
+++ Serenity.Script.UI/PropertyDialog/PropertyPanel.cs
@@ -0,0 +1,201 @@
+﻿using jQueryApi;
+using System;
+using System.Collections.Generic;
+
+namespace Serenity
+{
+    public abstract partial class PropertyPanel<TEntity, TOptions> : TemplatedPanel<TOptions>
+        where TEntity : class, new()
+        where TOptions: class, new()
+    {
+        private TEntity entity;
+        private Int64? entityId;
+
+        protected PropertyPanel(jQueryObject container)
+            : base(container)
+        {
+        }
+
+        protected PropertyPanel(jQueryObject div, TOptions opt)
+            : base(div, opt)
+        {
+            if (!IsAsyncWidget())
+            {
+                #pragma warning disable 618
+                InitPropertyGrid();
+                LoadInitialEntity();
+                #pragma warning restore 618
+            }
+        }
+
+        protected override Promise InitializeAsync()
+        {
+            return base.InitializeAsync()
+                .ThenAwait(InitPropertyGridAsync)
+                .Then(() =>
+                {
+                    LoadInitialEntity();
+                });
+        }
+
+        protected virtual void LoadInitialEntity()
+        {
+            if (propertyGrid != null)
+                propertyGrid.Load(new object());
+        }
+
+        public override void Destroy()
+        {
+            if (propertyGrid != null)
+            {
+                propertyGrid.Destroy();
+                propertyGrid = null;
+            }
+
+            if (validator != null)
+            {
+                this.ById("Form").Remove();
+                validator = null;
+            }
+
+            base.Destroy();
+        }
+
+        protected TEntity Entity
+        {
+            get { return entity; }
+            set { entity = value ?? new TEntity(); }
+        }
+
+        protected internal Nullable<Int64> EntityId
+        {
+            get { return entityId; }
+            set { entityId = value; }
+        }
+
+        protected PropertyGrid propertyGrid;
+
+        private void InitPropertyGrid()
+        {
+            var pgDiv = this.ById("PropertyGrid");
+            if (pgDiv.Length <= 0)
+                return;
+
+            #pragma warning disable 618
+            var pgOptions = GetPropertyGridOptions();
+            #pragma warning restore 618
+
+            propertyGrid = new PropertyGrid(pgDiv, pgOptions).Init();
+            if (this.element.Closest(".ui-Panel").HasClass("s-Flexify"))
+                propertyGrid.Element.Children(".categories").FlexHeightOnly();
+        }
+
+        private Promise InitPropertyGridAsync()
+        {
+            return Promise.Void.ThenAwait(() =>
+            {
+                var pgDiv = this.ById("PropertyGrid");
+                if (pgDiv.Length <= 0)
+                    return Promise.Void;
+
+                return GetPropertyGridOptionsAsync()
+                    .ThenAwait(pgOptions =>
+                    {
+                        propertyGrid = new PropertyGrid(pgDiv, pgOptions);
+                        if (this.element.Closest(".ui-Panel").HasClass("s-Flexify"))
+                            propertyGrid.Element.Children(".categories").FlexHeightOnly();
+                        return propertyGrid.Initialize();
+                    });
+            });
+        }
+
+        protected virtual string GetFormKey()
+        {
+            var attributes = this.GetType().GetCustomAttributes(typeof(FormKeyAttribute), true);
+            if (attributes.Length >= 1)
+                return attributes[0].As<FormKeyAttribute>().Value;
+
+            var name = this.GetType().FullName;
+            var px = name.IndexOf(".");
+            if (px >= 0)
+                name = name.Substring(px + 1);
+
+            if (name.EndsWith("Panel"))
+                name = name.Substr(0, name.Length - 6);
+            else if (name.EndsWith("Panel"))
+                name = name.Substr(0, name.Length - 5);
+
+            return name;
+        }
+
+        protected virtual List<PropertyItem> GetPropertyItems()
+        {
+            #pragma warning disable 618
+            var formKey = GetFormKey();
+            return Q.GetForm(formKey);
+            #pragma warning restore 618
+        }
+
+        protected virtual Promise<List<PropertyItem>> GetPropertyItemsAsync()
+        {
+            return Promise.Void.ThenAwait(() => 
+            {
+                var formKey = GetFormKey();
+                return Q.GetFormAsync(formKey);
+            });
+        }
+
+        protected virtual PropertyGridOptions GetPropertyGridOptions()
+        {
+            #pragma warning disable 618
+            return new PropertyGridOptions
+            {
+                IdPrefix = this.idPrefix,
+                Items = GetPropertyItems(),
+                Mode = PropertyGridMode.Insert,
+                UseCategories = false,
+                LocalTextPrefix = "Forms." + GetFormKey() + "."
+            };
+            #pragma warning restore 618
+        }
+
+        protected virtual Promise<PropertyGridOptions> GetPropertyGridOptionsAsync()
+        {
+            return GetPropertyItemsAsync().ThenSelect(propertyItems =>
+            {
+                return new PropertyGridOptions
+                {
+                    IdPrefix = this.idPrefix,
+                    Items = propertyItems,
+                    Mode = PropertyGridMode.Insert,
+                    UseCategories = false,
+                    LocalTextPrefix = "Forms." + GetFormKey() + ".",
+                };
+            });
+        }
+
+        protected virtual bool ValidateBeforeSave()
+        {
+            return this.validator.ValidateForm();
+        }
+
+        protected virtual TEntity GetSaveEntity()
+        {
+            var entity = new TEntity();
+
+            if (this.propertyGrid != null)
+                this.propertyGrid.Save(entity);
+
+            return entity;
+        }
+    }
+
+    public abstract partial class PropertyPanel<TEntity> : PropertyPanel<TEntity, object>
+        where TEntity : class, new()
+    {
+        public PropertyPanel(jQueryObject container)
+            : base(container, null)
+        {
+        }
+    }
+}
---
+++ Serenity.Script.UI/Widget/TemplatedPanel.cs
@@ -0,0 +1,122 @@
+﻿using jQueryApi;
+using jQueryApi.UI.Widgets;
+using System.Collections.Generic;
+using System.Html;
+
+namespace Serenity
+{
+    [Element("<div/>")]
+    public abstract class TemplatedPanel<TOptions> : TemplatedWidget<TOptions>
+        where TOptions : class, new()
+    {
+        protected bool isPanel;
+        protected jQueryValidator validator;
+        protected TabsObject tabs;
+        protected Toolbar toolbar;
+
+        protected TemplatedPanel(jQueryObject div, TOptions opt)
+            : base(div, opt)
+        {
+            InitValidator();
+            InitTabs();
+            InitToolbar();
+        }
+
+        protected TemplatedPanel(jQueryObject div)
+            : this(div, null)
+        {
+        }
+
+        public override void Destroy()
+        {
+            if (tabs != null)
+                tabs.Destroy();
+
+            if (toolbar != null)
+            {
+                toolbar.Destroy();
+                toolbar = null;
+            }
+
+            if (validator != null)
+            {
+                this.ById("Form").Remove();
+                validator = null;
+            }
+
+            base.Destroy();
+        }
+
+        protected virtual void InitToolbar()
+        {
+            var toolbarDiv = this.ById("Toolbar");
+            if (toolbarDiv.Length == 0)
+                return;
+
+            var opt = new ToolbarOptions
+            {
+                Buttons = GetToolbarButtons()
+            };
+
+            toolbar = new Toolbar(toolbarDiv, opt);
+        }
+
+        protected virtual List<ToolButton> GetToolbarButtons()
+        {
+            return new List<ToolButton>();
+        }
+
+        protected virtual jQueryValidatorOptions GetValidatorOptions()
+        {
+            return new jQueryValidatorOptions();
+        }
+
+        protected virtual void InitValidator()
+        {
+            var form = this.ById("Form");
+            if (form.Length > 0)
+            {
+                var valOptions = GetValidatorOptions();
+                validator = form.As<jQueryValidationObject>().Validate(Q.Externals.ValidateOptions(valOptions));
+            }
+        }
+
+        protected virtual void ResetValidation()
+        {
+            if (validator != null)
+                ((dynamic)validator).resetAll();
+        }
+
+        protected virtual bool ValidateForm()
+        {
+            return validator == null || Q.IsTrue(validator.ValidateForm());
+        }
+
+        protected virtual void Arrange()
+        {
+            this.element.Find(".require-layout").Filter(":visible").Each((i, e) =>
+            {
+                J(e).TriggerHandler("layout");
+            });
+        }
+
+        protected virtual void InitTabs()
+        {
+            var tabsDiv = this.ById("Tabs");
+            if (tabsDiv.Length == 0)
+                return;
+
+            tabs = tabsDiv.Tabs(new TabsOptions());
+        }
+
+        public string IdPrefix { get { return idPrefix; } }
+    }
+
+    public abstract class TemplatedPanel : TemplatedPanel<object>
+    {
+        public TemplatedPanel(jQueryObject container)
+            : base(container)
+        {
+        }
+    }
+}
