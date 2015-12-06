ce6d607375b79d8699bfca9490ec87a3e4246d8c
volkan.ceylan <volkanceylan@gmail.com>
2015-12-06 12:31:25 +0200

    add RecaptchaValidation helper

---
+++ Serenity.Web/Security/RecaptchaValidation.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Serenity.Web
+{
+    using ComponentModel;
+    using Newtonsoft.Json;
+    using Serenity.Services;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net;
+    public static class RecaptchaValidation
+    {
+        /// <summary>
+        /// Validates a recaptcha token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <remarks>Inspired from https://github.com/tanveery/recaptcha-net/blob/master/src/Recaptcha.Web/RecaptchaVerificationHelper.cs</remarks>
+        public static void Validate(string token)
+        {
+            if (string.IsNullOrEmpty(token))
+                throw new ValidationError("Recaptcha", LocalText.Get("Validation.Recaptcha"));
+
+            var config = Config.Get<RecaptchaSettings>();
+
+            var postData = String.Format("secret={0}&response={1}",
+                Uri.EscapeDataString(config.SecretKey),
+                Uri.EscapeDataString(token));
+
+            byte[] postDataBuffer = System.Text.Encoding.ASCII.GetBytes(postData);
+            var verifyUri = new Uri("https://www.google.com/recaptcha/api/siteverify", UriKind.Absolute);
+
+            var webRequest = (HttpWebRequest)WebRequest.Create(verifyUri);
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+            webRequest.ContentLength = postDataBuffer.Length;
+            webRequest.Method = "POST";
+
+            var proxy = WebRequest.GetSystemWebProxy();
+            proxy.Credentials = CredentialCache.DefaultCredentials;
+            webRequest.Proxy = proxy;
+
+            using (var requestStream = webRequest.GetRequestStream())
+                requestStream.Write(postDataBuffer, 0, postDataBuffer.Length);
+
+            var webResponse = (HttpWebResponse)webRequest.GetResponse();
+
+            string responseJson;
+            using (var sr = new StreamReader(webResponse.GetResponseStream()))
+                responseJson = sr.ReadToEnd();
+
+            var response = JSON.Parse<RecaptchaResponse>(responseJson);
+            if (response == null ||
+                !response.Success)
+            {
+                throw new ValidationError("Recaptcha", LocalText.Get("Validation.Recaptcha"));
+            }
+        }
+
+        private class RecaptchaResponse
+        {
+            public bool Success { get; set; }
+            [JsonProperty("error-codes")]
+            public string[] ErrorCodes { get; set; }
+        }
+    }
+}
