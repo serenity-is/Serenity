122e3bed81155d6bd80a5ec2d53134814918ba3d
volkan.ceylan <volkanceylan@gmail.com>
2015-12-13 11:15:22 +0200

    try to resolve AppVeyor SqlLocalDB timeout errors

--- Serenity.Testing/DbSettings.cs
+++ Serenity.Testing/DbSettings.cs
@@ -1,10 +1,13 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.Configuration;
 
 namespace Serenity.Testing
 {
     public class DbSettings
     {
+        private bool initializedLocalDb;
+
         public DbSettings()
         {
             Server = @"(localdb)\test";
@@ -18,12 +21,35 @@ namespace Serenity.Testing
 
         public string ServerConnectionString
         {
-            get { return "Data Source=" + Server + ";Integrated Security=true;"; }
+            get
+            {
+                if (!initializedLocalDb &&
+                    Server.StartsWith("(localdb)\\", System.StringComparison.OrdinalIgnoreCase))
+                {
+                    initializedLocalDb = true;
+
+                    try
+                    {
+                        var instanceName = Server.Substring("(localdb)\\".Length);
+
+                        if (!System.Data.SqlLocalDb.SqlLocalDbApi.GetInstanceNames().Contains(instanceName))
+                            System.Data.SqlLocalDb.SqlLocalDbApi.CreateInstance(instanceName);
+
+                        System.Data.SqlLocalDb.SqlLocalDbApi.StartInstance(instanceName);
+                    }
+                    catch (Exception ex)
+                    {
+                        ex.Log();
+                    }
+                }
+
+                return "Data Source=" + Server + ";Integrated Security=true;Connection Timeout=180";
+            }
         }
 
         public string ConnectionStringFormat
         {
-            get { return "Data Source=" + Server + ";Initial Catalog={0};Integrated Security=true;"; }
+            get { return "Data Source=" + Server + ";Initial Catalog={0};Integrated Security=true;Connection Timeout=180"; }
         }
 
         private static DbSettings current;
