ad4126f7ee7501f141327959ec04bacdc7226b54
volkan.ceylan <volkanceylan@gmail.com>
2015-12-17 00:28:45 +0200

    add throttler

---
+++ Serenity.Core/Authorization/Throttler.cs
@@ -0,0 +1,48 @@
+﻿using Serenity.Abstractions;
+using System;
+
+namespace Serenity
+{
+    public class Throttler
+    {
+        public Throttler(string key, TimeSpan duration, int limit)
+        {
+            Key = key;
+            Duration = duration;
+            Limit = limit;
+            CacheKey = "Throttling:" + key + ":" + duration.Ticks.ToInvariant();
+        }
+
+        public string Key { get; private set; }
+        public TimeSpan Duration { get; private set; }
+        public int Limit { get; private set; }
+        public string CacheKey { get; private set; }
+
+        private class HitInfo
+        {
+            public int Counter;
+        }
+
+        public bool Check()
+        {
+            var hit = Dependency.Resolve<ILocalCache>().Get<object>(this.CacheKey) as HitInfo;
+            if (hit == null)
+            {
+                hit = new HitInfo { Counter = 1 };
+                LocalCache.Add(this.CacheKey, hit, this.Duration);
+            }
+            else
+            {
+                if (hit.Counter++ > this.Limit)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public void Reset()
+        {
+            Dependency.Resolve<ILocalCache>().Remove(CacheKey);
+        }
+    }
+}
