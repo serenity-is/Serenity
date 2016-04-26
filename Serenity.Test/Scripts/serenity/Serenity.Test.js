bc749a1911bda7d6d493d93c5a66a609c15c1531
volkan.ceylan <volkanceylan@gmail.com>
2016-04-26 21:37:56 +0300

    add integer editor tests

--- Serenity.Test/Scripts/serenity/Serenity.Test.js
+++ Serenity.Test/Scripts/serenity/Serenity.Test.js
@@ -4,6 +4,69 @@
     (function (Test) {
         var assert = QUnit.assert;
         QUnit.module('Editors');
+        QUnit.test('Create IntegerEditor with undefined options', function () {
+            var input = $('<input/>');
+            assert.notEqual(null, new Serenity.IntegerEditor(input), 'create on a input');
+            assert.ok(input.hasClass("integerQ"), 'should have integerQ class');
+            var autoNumeric = input.data('autoNumeric');
+            assert.ok(autoNumeric != null, 'should have autonumeric data');
+            assert.strictEqual(autoNumeric.vMin, 0, 'its min value should be 0');
+            assert.strictEqual(autoNumeric.vMax, 2147483647, 'its max value should be 2147483647 when options is undefined');
+            assert.strictEqual(autoNumeric.mDec, 0, 'shouldnt allow decimals');
+            assert.strictEqual(autoNumeric.mInt, 10, 'should allow 10 digits');
+        });
+        QUnit.test('Create IntegerEditor with empty options', function () {
+            var input = $('<input/>');
+            assert.notEqual(null, new Serenity.IntegerEditor(input, {}), 'create on a input');
+            assert.ok(input.hasClass("integerQ"), 'should have integerQ class');
+            var autoNumeric = input.data('autoNumeric');
+            assert.ok(autoNumeric != null, 'should have autoNumeric data');
+            assert.strictEqual(autoNumeric.vMin, 0, 'its min value should be 0');
+            assert.strictEqual(autoNumeric.vMax, 2147483647, 'its max value should be 2147483647');
+            assert.strictEqual(autoNumeric.mDec, 0, 'shouldnt allow decimals');
+            assert.strictEqual(autoNumeric.mInt, 10, 'should allow 10 digits');
+        });
+        QUnit.test('Create IntegerEditor with minValue option', function () {
+            var input = $('<input/>');
+            assert.notEqual(null, new Serenity.IntegerEditor(input, { minValue: 5 }), 'create on a input');
+            assert.ok(input.hasClass("integerQ"), 'should have integerQ class');
+            var autoNumeric = input.data('autoNumeric');
+            assert.ok(autoNumeric != null, 'should have autoNumeric data');
+            assert.strictEqual(autoNumeric.vMin, 5, 'its min value should be 5');
+            assert.strictEqual(autoNumeric.vMax, 2147483647, 'its max value should be 2147483647');
+            assert.strictEqual(autoNumeric.mDec, 0, 'shouldnt allow decimals');
+            assert.strictEqual(autoNumeric.mInt, 10, 'should allow 10 digits');
+        });
+        QUnit.test('Create IntegerEditor with maxValue option', function () {
+            var input = $('<input/>');
+            assert.notEqual(null, new Serenity.IntegerEditor(input, { maxValue: 79 }), 'create on a input');
+            assert.ok(input.hasClass("integerQ"), 'should have integerQ class');
+            var autoNumeric = input.data('autoNumeric');
+            assert.ok(autoNumeric != null, 'should have autoNumeric data');
+            assert.strictEqual(autoNumeric.vMin, 0, 'its min value should be 0');
+            assert.strictEqual(autoNumeric.vMax, 79, 'its max value should be 79');
+            assert.strictEqual(autoNumeric.mDec, 0, 'shouldnt allow decimals');
+            assert.strictEqual(autoNumeric.mInt, 2, 'should allow 2 digits');
+        });
+        QUnit.test('Create IntegerEditor with minValue and maxValue options', function () {
+            var input = $('<input/>');
+            assert.notEqual(null, new Serenity.IntegerEditor(input, { minValue: 10, maxValue: 999 }), 'create on a input');
+            assert.ok(input.hasClass("integerQ"), 'should have integerQ class');
+            var autoNumeric = input.data('autoNumeric');
+            assert.ok(autoNumeric != null, 'should have autoNumeric data');
+            assert.strictEqual(autoNumeric.vMin, 10, 'its min value should be 10');
+            assert.strictEqual(autoNumeric.vMax, 999, 'its max value should be 999');
+            assert.strictEqual(autoNumeric.mDec, 0, 'shouldnt allow decimals');
+            assert.strictEqual(autoNumeric.mInt, 3, 'should allow 3 digits');
+        });
+    })(Test = Serenity.Test || (Serenity.Test = {}));
+})(Serenity || (Serenity = {}));
+var Serenity;
+(function (Serenity) {
+    var Test;
+    (function (Test) {
+        var assert = QUnit.assert;
+        QUnit.module('Editors');
         QUnit.test('TextAreaEditor creation tests', function () {
             assert.notEqual(null, new Serenity.TextAreaEditor($('<textarea/>')), 'create on a textarea with undefined options');
             assert.notEqual(null, new Serenity.TextAreaEditor($('<textarea/>'), {}), 'create on a textarea with empty options');
