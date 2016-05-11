80bad429037c3dbce824b223d774e9c620133735
volkan.ceylan <volkanceylan@gmail.com>
2016-05-11 23:43:58 +0300

    add comments to Q.ArrayUtils

--- Serenity.Web/Scripts/serenity/CoreLib/Q/Q.ArrayUtils.ts
+++ Serenity.Web/Scripts/serenity/CoreLib/Q/Q.ArrayUtils.ts
@@ -2,10 +2,17 @@
 /// <reference path="Q.CommonTypes.ts" />
 
 namespace Q {
-    export function arrayClone<T>(a: T[]): T[] {
-        return (ss as any).arrayClone(a);
+
+    /**
+     * Clones an array
+     */
+    export function arrayClone<T>(array: T[]): T[] {
+        return (ss as any).arrayClone(array);
     }
 
+    /**
+     * Tests if any of array elements matches given predicate
+     */
     export function any<TItem>(array: TItem[], predicate: (x: TItem) => boolean): boolean {
         for (let x of array)
             if (predicate(x))
@@ -14,6 +21,9 @@ namespace Q {
         return false;
     }
 
+    /**
+     * Counts number of array elements that matches a given predicate
+     */
     export function count<TItem>(array: TItem[], predicate: (x: TItem) => boolean): number {
         let count = 0;
         for (let x of array)
@@ -23,6 +33,10 @@ namespace Q {
         return count;
     }
 
+    /**
+     * Gets first element in an array that matches given predicate.
+     * Throws an error if no match is found.
+     */
     export function first<TItem>(array: TItem[], predicate: (x: TItem) => boolean): TItem {
         for (let x of array)
             if (predicate(x))
@@ -31,6 +45,9 @@ namespace Q {
         throw new Error("first:No element satisfies the condition.!");
     }
 
+    /**
+     * Gets index of first element in an array that matches given predicate
+     */
     export function indexOf<TItem>(array: TItem[], predicate: (x: TItem) => boolean): number {
         for (var i = 0; i < array.length; i++)
             if (predicate(array[i]))
@@ -39,14 +56,24 @@ namespace Q {
         return -1;
     }
 
-    export function insert(obj: any, index: number, item: any): void {
-        (ss as any).insert(obj, index, item);
+    /**
+     * Inserts an item to the array at specified index
+     */
+    export function insert<TItem>(array: TItem[], index: number, item: TItem): void {
+        (ss as any).insert(array, index, item);
     }
 
-    export function isArray(a: any): boolean {
-        return (ss as any).isArray(a);
+    /**
+     * Determines if the object is an array
+     */
+    export function isArray(obj: any): boolean {
+        return (ss as any).isArray(obj);
     }
 
+     /**
+     * Gets first element in an array that matches given predicate.
+     * Throws an error if no matches is found, or there are multiple matches.
+     */
     export function single<TItem>(array: TItem[], predicate: (x: TItem) => boolean): TItem {
         let match: any;
         let found = false;
@@ -65,7 +92,10 @@ namespace Q {
         return match;
     }
 
-
+    /**
+     * Maps an array into a dictionary with keys determined by specified getKey() callback,
+     * and values that are arrays containing elements for a particular key.
+     */
     export function toGrouping<TItem>(items: TItem[], getKey: (x: TItem) => any): Q.Grouping<TItem> {
         let lookup: Grouping<TItem> = {};
         for (let x of items) {
@@ -80,6 +110,10 @@ namespace Q {
         return lookup;
     }
 
+    /**
+     * Gets first element in an array that matches given predicate.
+     * Returns null if no match is found.
+     */
     export function tryFirst<TItem>(array: TItem[], predicate: (x: TItem) => boolean): TItem {
         for (let x of array)
             if (predicate(x))
