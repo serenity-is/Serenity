029946e9951d916ce147caa8f0d310aa3ad99517
volkan.ceylan <volkanceylan@gmail.com>
2016-05-13 19:10:03 +0300

    do same for ServerImportsGenerator

--- Serenity.Web/CodeGeneration/ServerImports/ServerImportsGenerator.cs
+++ Serenity.Web/CodeGeneration/ServerImports/ServerImportsGenerator.cs
@@ -70,12 +70,14 @@ namespace Serenity.CodeGeneration
                 if (formScriptAttr != null)
                 {
                     GenerateForm(type, formScriptAttr);
+                    EnqueueTypeMembers(type);
                     return;
                 }
 
                 if (type.GetCustomAttribute<ColumnsScriptAttribute>() != null)
                 {
                     //GenerateColumns(type);
+                    EnqueueTypeMembers(type);
                     return;
                 }
 
