b6e32f2640e291351f1e8c4684d2d6e5e42deb5b
volkan.ceylan <volkanceylan@gmail.com>
2016-05-28 17:32:39 +0300

    add ListField<TItem> as an alias to CustomClassField<List<TItem>>

---
+++ Serenity.Data.Entity/FieldTypes/ListField.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Serenity.Data
+{
+    public class ListField<TItem> : CustomClassField<List<TItem>>
+    {
+        public ListField(ICollection<Field> collection, string name, LocalText caption = null, int size = 0, FieldFlags flags = FieldFlags.Default, 
+            Func<Row, List<TItem>> getValue = null, Action<Row, List<TItem>> setValue = null)
+            : base(collection, name, caption, size, flags, getValue, setValue)
+        {
+        }
+    }
+}
