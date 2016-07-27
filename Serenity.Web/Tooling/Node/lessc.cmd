ebb1a388677eb19accb3f8263cf3adb5d260edd4
volkan.ceylan <volkanceylan@gmail.com>
2016-07-27 20:09:04 +0300

    try to resolve problems with lessc as some users report it doesn't get copied under tools\node\lessc\bin, probably nuget and extensionless file problem

--- Serenity.Web/Tooling/Node/lessc.cmd
+++ Serenity.Web/Tooling/Node/lessc.cmd
@@ -1,13 +1,5 @@
 @IF EXIST "%~dp0\node.exe" (
-  @IF EXIST "%~dp0\less\lib\lessc" (
-    "%~dp0\node.exe"  "%~dp0\less\lib\lessc" %*
-  ) ELSE (
-    "%~dp0\node.exe" "%~dp0\less\bin\lessc" %*
-  )
+    "%~dp0\node.exe"  "%~dp0\less\lib\lessc.js" %*
 ) ELSE (
-  @IF EXIST "%~dp0\less\lib\lessc" (
-    node  "%~dp0\less\lib\lessc" %*
-  ) ELSE (
-    node  "%~dp0\less\bin\lessc" %*
-  )
+    node  "%~dp0\less\lib\lessc.js" %*
 )
