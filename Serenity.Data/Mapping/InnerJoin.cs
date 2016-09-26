8b40dbbe0aec4c8ddf259e72fe0b090397a6180d
Marco Bisio <marco.bisio@o3-tech.it>
2016-09-26 21:46:53 +0200

    Create InnerJoin.cs

---
+++ Serenity.Data/Mapping/InnerJoin.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Serenity.Data.Mapping
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property, AllowMultiple = true)]
+    public class InnerJoinAttribute : Attribute
+    {
+        /// <summary>
+        /// Adds a inner join on foreign key. Use this version only on properties with ForeignKey attribute.
+        /// </summary>
+        /// <param name="alias">Foreign join alias</param>
+        public InnerJoinAttribute(string alias)
+        {
+            this.Alias = alias;
+        }
+
+        /// <summary>
+        /// Adds a inner join
+        /// </summary>
+        /// <param name="alias">Join alias</param>
+        /// <param name="toTable">Join table</param>
+        /// <param name="onCriteria">If the attribute is used on a property, this parameter is a field name, if used on a class,
+        /// this parameter is the ON criteria of the inner join statement.</param>
+        public InnerJoinAttribute(string alias, string toTable, string onCriteria)
+        {
+            this.Alias = alias;
+            this.ToTable = toTable;
+            this.OnCriteria = onCriteria;
+        }
+
+        public String Alias { get; private set; }
+        public String ToTable { get; private set; }
+        public String OnCriteria { get; private set; }
+    }
+}
