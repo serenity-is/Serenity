92697943a62d6e76909c201c867b8efb1eaaf049
volkan.ceylan <volkanceylan@gmail.com>
2016-10-12 23:26:08 +0300

    added RowField type

---
+++ Serenity.Data.Entity/FieldTypes/RowField.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Serenity.Data
+{
+    public class RowField<TForeign> : CustomClassField<TForeign> where TForeign: Row
+    {
+        public RowField(ICollection<Field> collection, string name, LocalText caption = null, int size = 0, FieldFlags flags = FieldFlags.Default, 
+            Func<Row, TForeign> getValue = null, Action<Row, TForeign> setValue = null)
+            : base(collection, name, caption, size, flags, getValue, setValue)
+        {
+        }
+
+        protected override int CompareValues(TForeign value1, TForeign value2)
+        {
+            if (value1 == null && value2 == null)
+                return 0;
+
+            if (value1 == null)
+                return -1;
+
+            if (value2 == null)
+                return 1;
+
+            foreach (var f in value1.GetFields())
+            {
+                var c = f.IndexCompare(value1, value2);
+                if (c != 0)
+                    return c;
+            }
+
+            return 0;
+        }
+
+        protected override TForeign Clone(TForeign value)
+        {
+            if (value == null)
+                return null;
+
+            return value.Clone();
+        }
+    }
+}
