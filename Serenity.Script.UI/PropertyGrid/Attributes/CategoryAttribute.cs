11cdacc456a261e020a4d50c90df94a20f5f311b
Marco Bisio <marco.bisio@o3-tech.it>
2016-11-05 09:53:05 +0100

    Collapsible Categories

--- Serenity.Script.UI/PropertyGrid/Attributes/CategoryAttribute.cs
+++ Serenity.Script.UI/PropertyGrid/Attributes/CategoryAttribute.cs
@@ -5,13 +5,17 @@ namespace Serenity
 {
     public class CategoryAttribute : Attribute
     {
-        public CategoryAttribute(string category)
+        public CategoryAttribute(string category, bool collapsible = false, bool expanded = false)
         {
             Category = category;
+            Collapsible = collapsible;
+            Expanded = expanded;
         }
 
         [IntrinsicProperty]
         public string Category { get; private set; }
+        public bool Collapsible { get; private set; }
+        public bool Expanded { get; private set; }
     }
 }
 
