09d1ee805fe194bf4e5cea3bee1d9b1db5383532
volkan.ceylan <volkanceylan@gmail.com>
2016-12-17 17:06:42 +0300

    ConnectionKeyAttribute can now accept a type, e.g. a row type, to get its value from that source type, instead of explicitly listing the connection key string.

--- Serenity.Data/Mapping/ConnectionKeyAttribute.cs
+++ Serenity.Data/Mapping/ConnectionKeyAttribute.cs
@@ -9,6 +9,22 @@ namespace Serenity.Data
             this.Value = connectionKey;
         }
 
+        public ConnectionKeyAttribute(Type sourceType)
+        {
+            if (sourceType == null)
+                throw new ArgumentNullException("sourceType");
+
+            var attr = sourceType.GetAttribute<ConnectionKeyAttribute>(true);
+            if (attr == null)
+                throw new ArgumentOutOfRangeException("sourceType",
+                    "ConnectionKeyAttribute is created with source type " + sourceType.Name + 
+                    ", but that class has no ConnectionKey attribute");
+
+            this.Value = attr.Value;
+            this.SourceType = sourceType;
+        }
+
         public string Value { get; private set; }
+        public Type SourceType { get; private set; }
     }
 }
