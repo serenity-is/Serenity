bcade0109e279aadbad9b240925dfd51c9b022d4
volkan.ceylan <volkanceylan@gmail.com>
2016-12-17 12:59:33 +0300

    added comment to IgnoreAttribute

--- Serenity.Core/ComponentModel/PropertyGrid/IgnoreAttribute.cs
+++ Serenity.Core/ComponentModel/PropertyGrid/IgnoreAttribute.cs
@@ -2,6 +2,13 @@
 
 namespace Serenity.ComponentModel
 {
+    /// <summary>
+    /// Skips a property while generating grid column or form field list.
+    /// Use this to ignore a property for UI, but still use it for other 
+    /// purposes like JSON serialization.
+    /// This might be useful when a type is used as a Service Request and Form
+    /// Declaration at the same time.
+    /// </summary>
     public class IgnoreAttribute : Attribute
     {
     }
