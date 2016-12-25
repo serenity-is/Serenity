4cbcdc8c5a525f1621074721c017bd00cb4fa737
volkan.ceylan <volkanceylan@gmail.com>
2016-12-25 21:51:50 +0300

    try pdbgit (fail)

--- Serenity.Core/Properties/AssemblyInfo.cs
+++ Serenity.Core/Properties/AssemblyInfo.cs
@@ -1,9 +1,4 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Serenity Platform - Core Library")]
-[assembly: AssemblyDescription(
-    "Contains abstractions and implementations for " +
-    "some basic Serenity features like authorization, " +
-    "caching, localization, dependency resolution, reflection...")]
 [assembly: ComVisible(false)]
