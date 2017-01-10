3b61dfac7407e5543214051bde23235201b274d4
volkan.ceylan <volkanceylan@gmail.com>
2017-01-10 16:07:08 +0300

    switching to NPM for TypeScript @types instead of TypeScript.DefinitelyTyped packages

---
+++ Serenity.TypeScript.CoreLib/Q/Q.BlockUI.ts
@@ -0,0 +1,53 @@
+﻿
+namespace Q {
+    let blockUICount: number = 0;
+
+    function blockUIWithCheck(opt: JQBlockUIOptions) {
+        if (blockUICount > 0) {
+            blockUICount++;
+            return;
+        }
+
+        $.blockUI(opt);
+        blockUICount++;
+    }
+
+    /**
+     * Uses jQuery BlockUI plugin to block access to whole page (default) or 
+     * a part of it, by using a transparent overlay covering the whole area.
+     * @param options Parameters for the BlockUI plugin
+     * @remarks If options are not specified, this function blocks 
+     * whole page with a transparent overlay. Default z-order of the overlay
+     * div is 2000, so a higher z-order shouldn't be used in page.
+     */
+    export function blockUI(options: JQBlockUIOptions) {
+        options = $.extend<JQBlockUIOptions>({
+            baseZ: 2000,
+            message: '',
+            overlayCSS: {
+                opacity: '0.0',
+                zIndex: 2000,
+                cursor: 'wait'
+            }, fadeOut: 0
+        }, options);
+
+        if (options.useTimeout) {
+            window.setTimeout(function () {
+                blockUIWithCheck(options);
+            }, 0);
+        }
+        else {
+            blockUIWithCheck(options);
+        }
+    }
+
+    export function blockUndo() {
+        if (blockUICount > 1) {
+            blockUICount--;
+            return;
+        }
+
+        blockUICount--;
+        $.unblockUI({ fadeOut: 0 });
+    }
+}
---
+++ Serenity.TypeScript.CoreLib/Q/Q.Notify.ts
@@ -0,0 +1,56 @@
+﻿
+namespace Q {
+    export let defaultNotifyOptions: ToastrOptions = {
+        timeOut: 3000,
+        showDuration: 250,
+        hideDuration: 500,
+        extendedTimeOut: 500,
+        positionClass: 'toast-top-full-width'
+    }
+
+    function getToastrOptions(options: ToastrOptions) {
+        options = $.extend<ToastrOptions>({}, defaultNotifyOptions, options);
+        positionToastContainer(true);
+        return options;
+    }
+
+    export function notifyWarning(message: string, title?: string, options?: ToastrOptions): void {
+        toastr.warning(message, title, getToastrOptions(options));
+    }
+
+    export function notifySuccess(message: string, title?: string, options?: ToastrOptions): void {
+        toastr.success(message, title, getToastrOptions(options));
+    }
+
+    export function notifyInfo(message: string, title?: string, options?: ToastrOptions): void {
+        toastr.info(message, title, getToastrOptions(options));
+    }
+
+    export function notifyError(message: string, title?: string, options?: ToastrOptions): void {
+        toastr.error(message, title, getToastrOptions(options));
+    }
+
+    export function positionToastContainer(create: boolean) {
+        if (typeof toastr === 'undefined') {
+            return;
+        }
+
+        var dialog = $(window.document.body).children('.ui-dialog:visible').last();
+        var container = toastr.getContainer(null, create);
+        if (container.length === 0) {
+            return;
+        }
+        if (dialog.length > 0) {
+            var position = dialog.position();
+            container.addClass('positioned-toast toast-top-full-width');
+            container.css({ position: 'absolute', top: position.top + 28 + 'px', left: position.left + 6 + 'px', width: dialog.width() - 12 + 'px' });
+        }
+        else {
+            container.addClass('toast-top-full-width');
+            if (container.hasClass('positioned-toast')) {
+                container.removeClass('positioned-toast');
+                container.css({ position: '', top: '', left: '', width: '' });
+            }
+        }
+    }
+}
