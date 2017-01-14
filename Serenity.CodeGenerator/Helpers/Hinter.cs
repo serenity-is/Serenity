3e57830c36b6c7e55c4712c6153c172e78f4f314
volkanceylan <volkanceylan@gmail.com>
2017-01-15 00:37:55 +0300

    well, no intellisense for sergen, thanks to .net cli run

--- Serenity.CodeGenerator/Helpers/Hinter.cs
+++ Serenity.CodeGenerator/Helpers/Hinter.cs
@@ -11,6 +11,8 @@ namespace Serenity.CodeGenerator
     /// </summary>
     public class Hinter
     {
+        private static bool failedRedirect = false;
+
         public static string ReadHintedLine<T, TResult>(IEnumerable<T> hintSource, 
             Func<T, TResult> hintField, string inputRegex = ".", ConsoleColor hintColor = ConsoleColor.DarkGray,
             string userInput = null)
@@ -21,11 +23,35 @@ namespace Serenity.CodeGenerator
             var suggestion = string.Empty;
             var readLine = string.Empty;
             userInput = userInput ?? String.Empty;
+            CheckRedirect();
+
             if (userInput.Length > 0)
             {
                 readLine = userInput;
                 ClearCurrentConsoleLine();
-                Console.Write(userInput);
+                if (!failedRedirect)
+                    Console.Write(userInput);
+            }
+
+            Func<string> getSuggestion = () =>
+            {
+                suggestion = hintSource.Select(item => hintField(item).ToString())
+                    .FirstOrDefault(item => item.Length > userInput.Length && item.Substring(0, userInput.Length) == userInput);
+
+                if (suggestion == null)
+                    suggestion = hintSource.Select(item => hintField(item).ToString())
+                        .FirstOrDefault(item => item.Length > userInput.Length &&
+                            String.Compare(item.Substring(0, userInput.Length), userInput, StringComparison.OrdinalIgnoreCase) == 0);
+
+                return suggestion == null ? userInput : suggestion;
+            };
+
+            if (failedRedirect)
+            {
+                userInput = Console.ReadLine();
+                readLine = getSuggestion();
+                Console.WriteLine(readLine);
+                return readLine;
             }
 
             while (ConsoleKey.Enter != (input = Console.ReadKey()).Key)
@@ -39,51 +65,57 @@ namespace Serenity.CodeGenerator
                 else if (input != null && input.KeyChar != '\0' && Regex.IsMatch(input.KeyChar.ToString(), inputRegex))
                     userInput += input.KeyChar;
 
-                suggestion = hintSource.Select(item => hintField(item).ToString())
-                    .FirstOrDefault(item => item.Length > userInput.Length && item.Substring(0, userInput.Length) == userInput);
-
-                if (suggestion == null)
-                    suggestion = hintSource.Select(item => hintField(item).ToString())
-                        .FirstOrDefault(item => item.Length > userInput.Length && 
-                            String.Compare(item.Substring(0, userInput.Length), userInput, StringComparison.OrdinalIgnoreCase) == 0);
-
-                readLine = suggestion == null ? userInput : suggestion;
+                readLine = getSuggestion();
 
                 ClearCurrentConsoleLine();
-
                 Console.Write(userInput);
-
                 var originalColor = Console.ForegroundColor;
-
                 Console.ForegroundColor = hintColor;
-
                 if (userInput.Any())
                     Console.Write(readLine.Substring(userInput.Length, readLine.Length - userInput.Length));
-
                 Console.SetCursorPosition(userInput.Length, Console.CursorTop);
                 Console.ForegroundColor = originalColor;
             }
 
             Console.WriteLine(readLine);
-
             return readLine;
+        }
+
+        private static void CheckRedirect()
+        {
+            try
+            {
+                var y = Console.CursorTop;
+                var x = Console.CursorLeft;
+                var w = Console.WindowWidth;
+                if (w >= 0)
+                    Console.SetCursorPosition(x, y);
+                failedRedirect = true;
+            }
+            catch
+            {
+                failedRedirect = true;
+            }
         }
 
         private static void ClearCurrentConsoleLine()
         {
-            int currentLineCursor = Console.CursorTop;
-            Console.SetCursorPosition(0, Console.CursorTop);
-            int consoleWidth;
+            if (failedRedirect)
+                return;
+
             try
             {
+                int currentLineCursor = Console.CursorTop;
+                Console.SetCursorPosition(0, Console.CursorTop);
+                int consoleWidth;
                 consoleWidth = Console.WindowWidth;
+                Console.Write(new string(' ', consoleWidth));
+                Console.SetCursorPosition(0, currentLineCursor);
             }
             catch (Exception)
             {
-                consoleWidth = 80;
+                failedRedirect = true;
             }
-            Console.Write(new string(' ', consoleWidth));
-            Console.SetCursorPosition(0, currentLineCursor);
         }
     }
 }
