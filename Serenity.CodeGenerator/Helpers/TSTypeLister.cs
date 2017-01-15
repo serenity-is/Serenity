716ac4f164ac3145143ea0cb44b851ecfe5800d1
volkanceylan <volkanceylan@gmail.com>
2017-01-15 12:44:02 +0300

    remove dependency on javascriptengineswitcher/chakracore, use node instead

--- Serenity.CodeGenerator/Helpers/TSTypeLister.cs
+++ Serenity.CodeGenerator/Helpers/TSTypeLister.cs
@@ -1,11 +1,9 @@
-﻿using JavaScriptEngineSwitcher.ChakraCore;
-using Serenity.CodeGeneration;
-using System;
+﻿using Serenity.CodeGeneration;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.Loader;
 using System.Text;
 
 namespace Serenity.CodeGenerator
@@ -28,23 +26,6 @@ namespace Serenity.CodeGenerator
             }
         }
 
-        private JavaScriptEngineSwitcher.Core.IJsEngine SetupJsEngine()
-        {
-            var jsEngine = new ChakraCoreJsEngineFactory().CreateEngine();
-            try
-            {
-                jsEngine.Evaluate(GetEmbeddedScript("typeScriptServices.js"));
-                jsEngine.Evaluate(GetEmbeddedScript("Serenity.CodeGeneration.js"));
-
-                return jsEngine;
-            }
-            catch
-            {
-                jsEngine.Dispose();
-                throw;
-            }
-        }
-
         public List<ExternalType> List()
         {
             var files = Directory.GetFiles(Path.Combine(projectDir, @"Modules"), "*.ts", SearchOption.AllDirectories)
@@ -52,24 +33,43 @@ namespace Serenity.CodeGenerator
                 .Concat(Directory.GetFiles(Path.Combine(projectDir, @"typings"), "*.ts", SearchOption.AllDirectories))
                 .Where(x => !x.EndsWith(".d.ts") || x.IndexOf("Serenity") >= 0);
 
-            using (var jsEngine = SetupJsEngine())
+            var tempDirectory = Path.ChangeExtension(Path.GetTempFileName(), null) + "__";
+            Directory.CreateDirectory(tempDirectory);
+            try
             {
-                jsEngine.SetVariableValue("fileName", "/lib.d.ts");
-                jsEngine.SetVariableValue("sourceText", GetEmbeddedScript("lib.d.ts_"));
-                jsEngine.Evaluate("Serenity.CodeGeneration.addSourceFile(fileName, sourceText)");
+                File.WriteAllText(Path.Combine(tempDirectory, "typeScriptServices.js"), GetEmbeddedScript("typeScriptServices.js") + "\n\nif (exports)\nexports.ts = ts;\n");
+                File.WriteAllText(Path.Combine(tempDirectory, "Serenity.CodeGeneration.js"), GetEmbeddedScript("Serenity.CodeGeneration.js") + "\n\nif (exports)\nexports.Serenity = Serenity;\n");
+                File.WriteAllText(Path.Combine(tempDirectory, "lib.d.ts_"), GetEmbeddedScript("lib.d.ts_"));
 
-                foreach (var file in files)
+                var sb = new StringBuilder();
+                sb.AppendLine("var fs = require('fs');");
+                sb.AppendLine("global.ts = require('./typescriptServices.js');");
+                sb.AppendLine("global.Serenity = require('./Serenity.CodeGeneration.js').Serenity;");
+                sb.AppendLine("var libdts = fs.readFileSync('./lib.d.ts_').toString();");
+                sb.AppendLine("var inputFiles = " + JSON.StringifyIndented(files.Select(x => x.Replace('\\', '/'))) + ";");
+                sb.AppendLine("for (var i = 0; i < inputFiles.length; i++) {");
+                sb.AppendLine("  var sourceText = fs.readFileSync(inputFiles[i]);");
+                sb.AppendLine("  Serenity.CodeGeneration.addSourceFile(inputFiles[i], sourceText.toString());");
+                sb.AppendLine("}");
+                sb.AppendLine("var types = JSON.stringify(Serenity.CodeGeneration.parseTypes(sourceText));");
+                sb.AppendLine("fs.writeFileSync('./typeList.json', types);");
+                File.WriteAllText(Path.Combine(tempDirectory, "index.js"), sb.ToString());
+
+                var process = Process.Start(new ProcessStartInfo()
                 {
-                    var name = file.Substring(projectDir.Length - 1).Replace("\\", "/");
-                    jsEngine.SetVariableValue("fileName", name);
-                    jsEngine.SetVariableValue("sourceText", File.ReadAllText(file));
-                    jsEngine.Evaluate("Serenity.CodeGeneration.addSourceFile(fileName, sourceText)");
-                }
+                    FileName = "node",
+                    Arguments = "index.js",
+                    WorkingDirectory = tempDirectory,
+                    CreateNoWindow = true
+                });
+                process.WaitForExit(60000);
 
-                var json = jsEngine.Evaluate<string>(
-                    "JSON.stringify(Serenity.CodeGeneration.parseTypes(sourceText))");
-
+                var json = File.ReadAllText(Path.Combine(tempDirectory, "typeList.json"));
                 return JSON.Parse<List<ExternalType>>(json);
+            }
+            finally
+            {
+                Directory.Delete(tempDirectory, true);
             }
         }
     }
