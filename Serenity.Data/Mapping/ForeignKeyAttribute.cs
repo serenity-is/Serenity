1ec938db18e90521ba5a5ac0a8560e72a56952e3
volkanceylan <volkanceylan@gmail.com>
2017-01-27 05:55:41 +0300

    allow specifying a [ForeignKey] by using a row type that has a [TableName] attribute. ID field can also be automatically determined if row has a property with [Identity] attribute, or a single property with [PrimaryKey] attribute. Implementing IIdField doesn't help ID field detection.

--- Serenity.Data/Mapping/ForeignKeyAttribute.cs
+++ Serenity.Data/Mapping/ForeignKeyAttribute.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Serenity.Data.Mapping
 {
@@ -16,11 +17,68 @@ namespace Serenity.Data.Mapping
         /// <param name="field">Matching column in primary key table</param>
         public ForeignKeyAttribute(string table, string field)
         {
+            Check.NotNull(table, nameof(table));
+            Check.NotNull(field, nameof(field));
+
             this.Field = field;
             this.Table = table;
         }
 
-        public string Field { get; private set; }
+        /// <summary>
+        /// Specifies that this property is a foreign key to another field in a primary key table.
+        /// </summary>
+        /// <param name="rowType">Entity for primary key table. Row must have a [TableName] attribute.</param>
+        /// <param name="field">If field parameter is not specified, the row type must have a field with 
+        /// [Identity] attribute or single property with [PrimaryKey] attribute.
+        /// (implementing IIdRow won't help)</param>
+        public ForeignKeyAttribute(Type rowType, string field = null)
+        {
+            Check.NotNull(rowType, nameof(rowType));
+
+            var attr = rowType.GetAttribute<TableNameAttribute>(true);
+            if (attr == null || string.IsNullOrEmpty(attr.Name))
+                throw new ArgumentOutOfRangeException(nameof(rowType),
+                    String.Format("Type '{0}' is specified for a ForeignKey attribute" +
+                        "but it has no [TableName] attribute", rowType.FullName));
+
+            this.Table = attr.Name;
+
+            if (string.IsNullOrEmpty(field))
+            {
+                var identityOrPrimaryKey = rowType.GetProperties()
+                    .Where(x => x.GetAttribute<IdentityAttribute>() != null ||
+                        x.GetAttribute<PrimaryKeyAttribute>() != null).ToArray();
+
+                if (identityOrPrimaryKey.Length == 0)
+                    throw new ArgumentOutOfRangeException(nameof(rowType),
+                        String.Format("Type '{0}' is specified for a ForeignKey attribute" + 
+                            "but it has no property with [Identity] or [PrimaryKey] attribute",
+                            rowType.FullName));
+
+                if (identityOrPrimaryKey.Length > 1)
+                {
+                    var identity = identityOrPrimaryKey.Where(x =>
+                        x.GetAttribute<IdentityAttribute>() != null);
+
+                    if (identity.Count() != 1)
+                        throw new ArgumentOutOfRangeException(nameof(rowType),
+                            String.Format("Type '{0}' is specified for a ForeignKey attribute" + 
+                            "but it has multiple [Identity] or [PrimaryKey] attributes",
+                            rowType.FullName));
+
+                    identityOrPrimaryKey = identity.ToArray();
+                }
+
+                Field = identityOrPrimaryKey[0].GetAttribute<ColumnAttribute>()?.Name ??
+                    identityOrPrimaryKey[0].Name;
+            }
+            else
+                Field = field;
+        }
+
         public string Table { get; private set; }
+        public string Field { get; private set; }
+        public Type RowType { get; set; }
+        public string Prefix { get; set; }
     }
 }
