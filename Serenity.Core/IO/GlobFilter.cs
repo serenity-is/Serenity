cb43b24e9773a4dba9b039b1d5f66a539a73fe20
volkan.ceylan <volkan.ceylan@marmara.edu.tr>
2017-04-19 14:55:45 +0300

    added .gitignore compatible GlobFilter class

---
+++ Serenity.Core/IO/GlobFilter.cs
@@ -0,0 +1,406 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Serenity.IO
+{
+    /// <summary>
+    /// A fast glob implementation, compatible with .gitignore patterns of GIT
+    /// </summary>
+    public class GlobFilter
+    {
+        private GlobFilter excludeFilter;
+        private HashSet<string> extensions;
+        private List<string> startsWith;
+        private List<string> endsWith;
+        private List<string> contains;
+        private HashSet<string> exactMatch;
+        private List<Tuple<string, bool, string>> startsWithAndEndsWith;
+        private List<Tuple<string, bool, string>> containsAndEndsWith;
+
+        private bool isActive;
+        private List<Func<string, bool>> matchers;
+
+        private char[] DotAsteriskSlashBackQue = new char[] { '.', '*', '/', '\\', '?' };
+        private char[] AsteriskSlashBackQue = new char[] { '*', '/', '\\', '?' };
+        private char[] AsteriskQue = new char[] { '*', '?' };
+        private char[] FolderSeps = new char[] { '\\', '/' };
+
+        /// <summary>
+        /// Creates a new GlobFilter, containing both include and exclude patterns.
+        /// When "include" is null / empty, all files are included by default, unless
+        /// they match a pattern in "exclude" list.
+        /// When "exclude" is null, it is ignored.
+        /// </summary>
+        /// <param name="include">List of include patterns</param>
+        /// <param name="exclude">List of exclude patterns</param>
+        public GlobFilter(IEnumerable<string> include, IEnumerable<string> exclude)
+            : this(include)
+        {
+            if (exclude != null)
+            {
+                var excludes = exclude.Where(x => !string.IsNullOrEmpty(x));
+                if (excludes.Any())
+                    this.excludeFilter = new GlobFilter(excludes);
+            }
+        }
+
+        /// <summary>
+        /// Creates a new GlobFilter, with just "include" globs.
+        /// </summary>
+        /// <param name="globs">List of patterns</param>
+        public GlobFilter(IEnumerable<string> globs)
+        {
+            this.matchers = new List<Func<string, bool>>();
+
+            if (globs == null)
+                return;
+
+            foreach (var glob in globs)
+            {
+                if (string.IsNullOrEmpty(glob))
+                    continue;
+
+                isActive = true;
+
+                var sep = System.IO.Path.DirectorySeparatorChar;
+
+                var s = glob;
+                if (sep == '\\')
+                    s = glob.Replace('/', '\\');
+
+                var starDotIndex = s.IndexOf("*.");
+
+                // exact match
+                if (s[0] == sep &&
+                    s[s.Length - 1] != '/' &&
+                    s[s.Length - 1] != '\\' &&
+                    s.IndexOfAny(AsteriskQue, 1) < 0)
+                {
+                    if (exactMatch == null)
+                        exactMatch = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+                    exactMatch.Add(s.Substring(1));
+                    continue;
+                }
+
+                // simple extension filter (*.txt)
+                if (starDotIndex == 0 &&
+                    s.IndexOfAny(DotAsteriskSlashBackQue, 2) < 0)
+                {
+                    if (extensions == null)
+                        extensions = new HashSet<string>();
+
+                    extensions.Add(s.Substring(1));
+                    continue;
+                }
+
+                // simple extension filter with extra dot (*.csproj.user)
+                if (starDotIndex == 0 &&
+                    s.IndexOfAny(AsteriskSlashBackQue, 2) < 0)
+                {
+                    if (endsWith == null)
+                        endsWith = new List<string>();
+
+                    endsWith.Add(s.Substring(1));
+                    continue;
+                }
+
+                // subdirectory under root and extension (/bin/some/*.txt)
+                if (starDotIndex > 1 &&
+                    s[starDotIndex - 1] == sep &&
+                    s[0] == sep &&
+                    s.IndexOfAny(AsteriskSlashBackQue, starDotIndex + 2) < 0 &&
+                    s.LastIndexOfAny(AsteriskQue, starDotIndex - 1) < 0)
+                {
+                    if (startsWithAndEndsWith == null)
+                        startsWithAndEndsWith = new List<Tuple<string, bool, string>>();
+
+                    startsWithAndEndsWith.Add(new Tuple<string, bool, string>(
+                        s.Substring(1, starDotIndex - 1), false, s.Substring(starDotIndex + 1)));
+
+                    continue;
+                }
+
+                // extension under root (/*.txt)
+                if (starDotIndex == 1 &&
+                    s[0] == sep &&
+                    s.IndexOfAny(AsteriskSlashBackQue, starDotIndex + 2) < 0)
+                {
+                    if (startsWithAndEndsWith == null)
+                        startsWithAndEndsWith = new List<Tuple<string, bool, string>>();
+
+                    startsWithAndEndsWith.Add(new Tuple<string, bool, string>(null, false, s.Substring(starDotIndex + 1)));
+                    continue;
+                }
+
+                // directory filter at any depth (.git/)
+                if (s[s.Length - 1] == sep &&
+                    s[0] != sep &&
+                    s.IndexOfAny(AsteriskQue, 1) < 0)
+                {
+                    if (contains == null)
+                        contains = new List<string>();
+
+                    if (startsWith == null)
+                        startsWith = new List<string>();
+
+                    contains.Add(sep.ToString() + s);
+                    startsWith.Add(s);
+
+                    continue;
+                }
+
+                // directory filter at root (/Imports/)
+                if (s[s.Length - 1] == sep &&
+                    s[0] == sep &&
+                    s.IndexOfAny(AsteriskQue) < 0)
+                {
+                    startsWith.Add(s.Substring(1));
+                    continue;
+                }
+
+                // folder than any then extension (App_Data/**/*.log) or (/App_Code/**/*.xyz)
+                if (starDotIndex > 4 &&
+                    s[starDotIndex - 1] == sep &&
+                    s[starDotIndex - 2] == '*' &&
+                    s[starDotIndex - 3] == '*' &&
+                    s[starDotIndex - 4] == sep &&
+                    s.LastIndexOfAny(AsteriskQue, starDotIndex - 5) < 0 &&
+                    s.IndexOfAny(AsteriskSlashBackQue, starDotIndex + 2) < 0)
+                {
+                    if (startsWithAndEndsWith == null)
+                        startsWithAndEndsWith = new List<Tuple<string, bool, string>>();
+
+                    if (s[0] == sep)
+                    {
+                        startsWithAndEndsWith.Add(new Tuple<string, bool, string>(
+                            s.Substring(1, starDotIndex - 4), true, s.Substring(starDotIndex + 1)));
+                    }
+                    else
+                    {
+                        if (containsAndEndsWith == null)
+                            containsAndEndsWith = new List<Tuple<string, bool, string>>();
+
+                        startsWithAndEndsWith.Add(new Tuple<string, bool, string>(
+                            s.Substring(0, starDotIndex - 3), true, s.Substring(starDotIndex + 1)));
+
+                        containsAndEndsWith.Add(new Tuple<string, bool, string>(
+                            sep.ToString() + s.Substring(0, starDotIndex - 3), true, s.Substring(starDotIndex + 1)));
+                    }
+
+                    continue;
+                }
+
+                if (s.IndexOfAny(AsteriskQue) < 0)
+                {
+                    if (exactMatch == null)
+                        exactMatch = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+                    if (s[0] == sep)
+                    {
+                        exactMatch.Add(s.Substring(0));
+                    }
+                    else
+                    {
+                        exactMatch.Add(s.Substring(0));
+
+                        if (endsWith == null)
+                            endsWith = new List<string>();
+
+                        endsWith.Add(sep + s.Substring(0));
+                    }
+
+                    continue;
+                }
+
+                if (s[0] == '*' && s.IndexOfAny(AsteriskSlashBackQue, 1) < 0)
+                {
+                    if (endsWith == null)
+                        endsWith = new List<string>();
+
+                    endsWith.Add(s.Substring(1));
+                    continue;
+                }
+
+                if (s[0] == sep &&
+                    s.Length > 2 &&
+                    s[1] == '*' &&
+                    s.IndexOfAny(AsteriskSlashBackQue, 2) < 0)
+                {
+                    if (startsWithAndEndsWith == null)
+                        startsWithAndEndsWith = new List<Tuple<string, bool, string>>();
+
+                    startsWithAndEndsWith.Add(new Tuple<string, bool, string>(null, false, s.Substring(2)));
+                    continue;
+                }
+
+                matchers.Add(ToMatcher(glob));
+            }
+        }
+
+        private static Func<string, bool> ToMatcher(string glob)
+        {
+            return ToMatcherRegex(glob);
+        }
+
+        private static Func<string, bool> ToMatcherRegex(string glob)
+        {
+            var regex = WildcardToRegex(NormalizeGlob(glob));
+            return regex.IsMatch;
+        }
+
+        public bool IsMatch(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return false;
+
+            if (excludeFilter != null &&
+                excludeFilter.IsMatch(path))
+                return false;
+
+            if (!isActive)
+                return true;
+
+            if (extensions != null)
+            {
+                var extension = Path.GetExtension(path);
+                if (extension != null &&
+                    extensions.Contains(extension))
+                    return true;
+            }
+
+            if (contains != null &&
+                contains.Any(x => path.IndexOf(x, StringComparison.OrdinalIgnoreCase) >= 0))
+                return true;
+
+            if (exactMatch != null &&
+                exactMatch.Contains(path))
+                return true;
+
+            if (startsWith != null &&
+                startsWith.Any(x => path.StartsWith(x, StringComparison.OrdinalIgnoreCase)))
+                return true;
+
+            if (endsWith != null &&
+                endsWith.Any(x => path.EndsWith(x, StringComparison.OrdinalIgnoreCase)))
+                return true;
+
+            if (startsWithAndEndsWith != null &&
+                startsWithAndEndsWith.Any(x =>
+                {
+                    if (!path.EndsWith(x.Item3, StringComparison.OrdinalIgnoreCase))
+                        return false;
+
+                    if (x.Item1 == null)
+                    {
+                        if (path.Length > x.Item3.Length &&
+                            path.LastIndexOfAny(FolderSeps, path.Length - x.Item3.Length - 1) >= 0)
+                            return false;
+                    }
+                    else
+                    {
+                        if (!path.StartsWith(x.Item1, StringComparison.OrdinalIgnoreCase))
+                            return false;
+
+                        if (!x.Item2 &&
+                            path.IndexOfAny(FolderSeps, x.Item1.Length) >= 0)
+                        {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }))
+            {
+                return true;
+            }
+
+            if (containsAndEndsWith != null &&
+                containsAndEndsWith.Any(x =>
+                {
+                    if (!path.EndsWith(x.Item3, StringComparison.OrdinalIgnoreCase))
+                        return false;
+
+                    if (x.Item1 == null)
+                    {
+                        if (path.Length > x.Item3.Length &&
+                            path.LastIndexOfAny(FolderSeps, path.Length - x.Item3.Length - 1) >= 0)
+                            return false;
+                    }
+                    else
+                    {
+                        var idx = path.IndexOf(x.Item1, StringComparison.OrdinalIgnoreCase);
+                        if (idx < 0)
+                            return false;
+
+                        if (!x.Item2 &&
+                            path.IndexOfAny(FolderSeps, idx + x.Item1.Length) >= 0)
+                        {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }))
+            {
+                return true;
+            }
+
+            if (matchers != null && matchers.Any(x => x(path)))
+                return true;
+
+            return false;
+        }
+
+        // this method is inspired from NuGet source code
+        public static Regex WildcardToRegex(string wildcard)
+        {
+            var pattern = Regex.Escape(wildcard);
+            if (System.IO.Path.DirectorySeparatorChar == '/')
+            {
+                // regex wildcard adjustments for *nix-style file systems
+                pattern = pattern
+                    .Replace(@"\.\*\*", @"\.[^/.]*") // .** should not match on ../file or ./file but will match .file
+                    .Replace(@"\*\*/", "(.+/)*") //For recursive wildcards /**/, include the current directory.
+                    .Replace(@"\*\*", ".*") // For recursive wildcards that don't end in a slash e.g. **.txt would be treated as a .txt file at any depth
+                    .Replace(@"\*", @"[^/]*(/)?") // For non recursive searches, limit it any character that is not a directory separator
+                    .Replace(@"\?", "."); // ? translates to a single any character
+            }
+            else
+            {
+                // regex wildcard adjustments for Windows-style file systems
+                pattern = pattern
+                    .Replace("/", @"\\") // On Windows, / is treated the same as \.
+                    .Replace(@"\.\*\*", @"\.[^\\.]*") // .** should not match on ../file or ./file but will match .file
+                    .Replace(@"\*\*\\", @"(.+\\)*") //For recursive wildcards \**\, include the current directory.
+                    .Replace(@"\*\*", ".*") // For recursive wildcards that don't end in a slash e.g. **.txt would be treated as a .txt file at any depth
+                    .Replace(@"\*", @"[^\\]*") // For non recursive searches, limit it any character that is not a directory separator
+                    .Replace(@"\?", "."); // ? translates to a single any character
+            }
+
+            return new Regex('^' + pattern + '$', RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture | RegexOptions.CultureInvariant);
+        }
+
+        public static string NormalizeGlob(string glob)
+        {
+            if (string.IsNullOrEmpty(glob))
+                return glob;
+
+            if (glob.StartsWith("/") ||
+                glob.StartsWith("\\"))
+                glob = glob.Substring(1);
+            else if (glob.IndexOf("**") < 0)
+                glob = "**/" + glob;
+
+            if (glob.EndsWith("/") ||
+                glob.EndsWith("\\"))
+                glob = glob + "**/*";
+
+            return glob;
+        }
+    }
+}
---
+++ Serenity.Test/Core/IO/GlobFilterTests.cs
@@ -0,0 +1,134 @@
+﻿using Serenity.IO;
+using Xunit;
+
+namespace Serenity.Test
+{
+    public class GlobFilterTests
+    {
+        static string[] filters = new string[]
+        {
+            ".git/",
+            "/web.config",
+            "/Web.Debug.config",
+            "/wkhtmltopdf-license.txt",
+            "/Welcome.htm",
+            "/Readme.CodeGenerator.txt",
+            "/package.json",
+            "/*.conf",
+            "/bin/*.xml",
+            "/Imports/",
+            "TestResults/",
+            "*.cs",
+            "App_Data/**/*.log",
+            "/App_Code/**/*.xyz",
+            "node_modules/",
+            ".csslintrc",
+            "_references.js",
+            "*.csproj.user",
+            "*.dsk",
+            "*.suo",
+            "*-vsdoc.js",
+            "/*-dat.js"
+        };
+
+        [Theory]
+        [InlineData(@".git\", true)]
+        [InlineData(@"a.git\", false)]
+        [InlineData(@"d\e\.git\", true)]
+        [InlineData(@"d\e\.git\obj\abc", true)]
+        [InlineData(@"d\e\a.git\", false)]
+        [InlineData(@"Web.config", true)]
+        [InlineData(@"WEB.CONFIG", true)]
+        [InlineData(@"views\WEB.CONFIG", false)]
+        [InlineData(@"abc\def\WEB.CONFIG", false)]
+        [InlineData(@"a\b\c\WEB.CONFIG", false)]
+        [InlineData(@"Web.debug.config", true)]
+        [InlineData(@"WEB.debug.CONFIG", true)]
+        [InlineData(@"views\WEB.debug.CONFIG", false)]
+        [InlineData(@"abc\def\WEB.debug.CONFIG", false)]
+        [InlineData(@"a\b\c\WEB.debug.CONFIG", false)]
+        [InlineData(@"wkhtmltopdf-license.txt", true)]
+        [InlineData(@"somewkhtmltopdf-license.txt", false)]
+        [InlineData(@"sub\wkhtmltopdf-license.txt", false)]
+        [InlineData(@"Readme.CodeGenerator.txt", true)]
+        [InlineData(@"someReadme.CodeGenerator.txt", false)]
+        [InlineData(@"sub\Readme.CodeGenerator.txt", false)]
+        [InlineData(@"test.suo", true)]
+        [InlineData(@"abcd\test.suo", true)]
+        [InlineData(@"abc\def\test.suo", true)]
+        [InlineData(@".dsk", true)]
+        [InlineData(@"test.dsk", true)]
+        [InlineData(@"a\test.dsk", true)]
+        [InlineData(@"a\.dsk", true)]
+        [InlineData(@"test.dsk\x.t", false)]
+        [InlineData(@"some.dsk.txt", false)]
+        [InlineData(@"dummy.csproj.user", true)]
+        [InlineData(@"abc\d.csproj.user", true)]
+        [InlineData(@"abc\def\d.csproj.user", true)]
+        [InlineData(@"abc\d.csproj.user\e.txt", false)]
+        [InlineData(@"package.json", true)]
+        [InlineData(@"test\\package.json", false)]
+        [InlineData(@"abc.conf", true)]
+        [InlineData(@".conf", true)]
+        [InlineData(@"test\abc.conf", false)]
+        [InlineData(@"test\.conf", false)]
+        [InlineData(@"bin\test.xml", true)]
+        [InlineData(@"bin\.xml", true)]
+        [InlineData(@"test.xml", false)]
+        [InlineData(@"bin\subfolder\.xml", false)]
+        [InlineData(@"bin\subfolder\test.xml", false)]
+        [InlineData(@"some\folder\test.xml", false)]
+        [InlineData(@"Imports\test.txt", true)]
+        [InlineData(@"Imports\SomeSub\test.txt", true)]
+        [InlineData(@"SomeSub\Imports\test.txt", false)]
+        [InlineData(@"SomeSub\Imports\abc\test.txt", false)]
+        [InlineData(@"Imports", false)]
+        [InlineData(@"TestResults\test.txt", true)]
+        [InlineData(@"TestResults\SomeSub\test.txt", true)]
+        [InlineData(@"SomeSub\TestResults\test.txt", true)]
+        [InlineData(@"SomeSub\TestResults\abc\test.txt", true)]
+        [InlineData(@"TestResults", false)]
+        [InlineData(@"test.cs", true)]
+        [InlineData(@"abc\def\test.cs", true)]
+        [InlineData(@"test.cs\dummy.txt", false)]
+        [InlineData(@"abc\test.csx", false)]
+        [InlineData(@"abc\def\test.csx", false)]
+        [InlineData(@"App_Code\test.xyz", true)]
+        [InlineData(@"App_Code\x\test.xyz", true)]
+        [InlineData(@"abc\App_Code\x\test.xyz", false)]
+        [InlineData(@"App_Data\test.log", true)]
+        [InlineData(@"App_Data\abc\def.log", true)]
+        [InlineData(@"App_Data\abc\def\xyz.log", true)]
+        [InlineData(@"App_Dat\xyz.log", false)]
+        [InlineData(@"App_Data.log", false)]
+        [InlineData(@"xyz\App_Data.log", false)]
+        [InlineData(@"xyz\test.log", false)]
+        [InlineData(@"test.xyz", false)]
+        [InlineData(@"node_modules\", true)]
+        [InlineData(@"a.node_modules\", false)]
+        [InlineData(@"d\e\node_modules\", true)]
+        [InlineData(@"d\e\node_modules\obj\abc", true)]
+        [InlineData(@"d\e\a.node_modules\", false)]
+        [InlineData(@".csslintrc", true)]
+        [InlineData(@"x\.csslintrc", true)]
+        [InlineData(@".csslintrc\x.txt", false)]
+        [InlineData(@"_references.js", true)]
+        [InlineData(@"x\_references.js", true)]
+        [InlineData(@"_references.js\x.txt", false)]
+        [InlineData(@"test-vsdoc.js", true)]
+        [InlineData(@"abc\xyz-vsdoc.js", true)]
+        [InlineData(@"-vsdoc.js", true)]
+        [InlineData(@"-vsdoc.test.js", false)]
+        [InlineData(@"-vsdoc.xyz", false)]
+        [InlineData(@"test-dat.js", true)]
+        [InlineData(@"abc\xyz-dat.js", false)]
+        [InlineData(@"-dat.js", true)]
+        [InlineData(@"-dat.test.js", false)]
+        [InlineData(@"-dat.xyz", false)]
+        public void Glob_Filter_Works_Properly(string path, bool result)
+        {
+            var filter = new GlobFilter(filters);
+            Assert.Equal(result, filter.IsMatch(path));
+        }
+    }
+}
