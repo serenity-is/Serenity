b643d8252da9c2a21ca789922486cb4ae265c0ec
volkanceylan <volkanceylan@gmail.com>
2017-05-23 01:29:48 +0300

    TriggerDataChange overload that takes jQuery object is renamed to triggerDataChanged

--- Serenity.TypeScript.CoreLib/UI/Helpers/SubDialogHelper.ts
+++ Serenity.TypeScript.CoreLib/UI/Helpers/SubDialogHelper.ts
@@ -1,8 +1,8 @@
 ﻿declare namespace Serenity {
     namespace SubDialogHelper {
         function bindToDataChange(dialog: any, owner: Serenity.Widget<any>, dataChange: (p1: any, p2: DataChangeInfo) => void, useTimeout?: boolean): any;
-        function triggerDataChange(dialog: any): any;
-        function triggerDataChange(element: JQuery): JQuery;
+        function triggerDataChange(dialog: Serenity.Widget<any>): any;
+        function triggerDataChanged(element: JQuery): JQuery;
         function bubbleDataChange(dialog: any, owner: Serenity.Widget<any>, useTimeout?: boolean): any;
         function cascade(cascadedDialog: any, ofElement: JQuery): any;
         function cascadedDialogOffset(element: JQuery): any;
