cb824b45166ccf8dfa7cabdcd968450b2071deff
volkan.ceylan <volkanceylan@gmail.com>
2017-07-13 17:56:35 +0300

    pass navigation item class from attribute

--- Serenity.Web/Navigation/NavigationItem.cs
+++ Serenity.Web/Navigation/NavigationItem.cs
@@ -7,6 +7,7 @@
         public string Title { get; set; }
         public string FullPath { get; set; }
         public string IconClass { get; set; }
+        public string ItemClass { get; set; }
         public string Url { get; set; }
         public string Target { get; set; }
         public List<NavigationItem> Children { get; private set; }
