aacdb7abb6878bb59dad063ef4391c3f1b700f0f
volkan.ceylan <volkan.ceylan@marmara.edu.tr>
2017-10-24 12:43:11 +0300

    try cdn

--- Serenity.Web/Mvc/ContentHashCache.cs
+++ Serenity.Web/Mvc/ContentHashCache.cs
@@ -4,6 +4,10 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Web.Hosting;
 using System.Web;
+using Serenity.ComponentModel;
+using System.Collections.Generic;
+using Serenity.IO;
+using System.Collections.Concurrent;
 #if ASPNETCORE
 using Microsoft.AspNetCore.WebUtilities;
 #endif
@@ -15,10 +19,33 @@ namespace Serenity.Web
     public static class ContentHashCache
     {
         private static Hashtable hashByContentPath;
+        private static bool cdnEnabled;
+        private static string cdnHttp;
+        private static string cdnHttps;
+        private static GlobFilter cdnFilter;
+
+        [SettingKey("CDNSettings"), SettingScope("Application")]
+        private class CDNSettings
+        {
+            public bool? Enabled { get; set; }
+            public string Url { get; set; }
+            public string HttpsUrl { get; set; }
+            public List<string> Include { get; set; }
+            public List<string> Exclude { get; set; }
+        }
 
         static ContentHashCache()
         {
             hashByContentPath = new Hashtable(StringComparer.OrdinalIgnoreCase);
+            var cdn = Config.Get<CDNSettings>();
+            cdnEnabled = cdn.Enabled == true && !string.IsNullOrEmpty(cdn.Url);
+            cdnHttp = cdn.Url;
+            if (string.IsNullOrEmpty(cdn.HttpsUrl))
+                cdnHttps = (cdnHttp ?? "").Replace("http://", "https://");
+            else
+                cdnHttps = cdn.HttpsUrl;
+
+            cdnFilter = new GlobFilter(cdn.Include, cdn.Exclude);
         }
 
         public static void ScriptsChanged()
@@ -49,25 +76,64 @@ namespace Serenity.Web
             if (contentUrl.IsNullOrEmpty())
                 throw new ArgumentNullException("contentUrl");
 
-            if (contentUrl.IndexOf(".axd/", StringComparison.OrdinalIgnoreCase) >= 0)
+            if (contentUrl[0] != '/' &&
+                (contentUrl[0] != '~' || contentUrl.Length < 2 || contentUrl[1] != '/'))
                 return contentUrl;
 
-            var path = contentUrl;
-            path = HostingEnvironment.MapPath(path);
+            string cdnMatch = contentUrl;
 
-            object hash;
-            hash = hashByContentPath[path];
-            if (hash == null)
+            if (contentUrl.IndexOf(".axd/", StringComparison.OrdinalIgnoreCase) >= 0)
             {
-                if (File.Exists(path))
-                    hash = GetFileSHA1(path);
-                else
-                    hash = DateTime.Now.ToString("yyyymmddhh");
+                if (!cdnEnabled)
+                    return contentUrl;
 
-                Hashtable.Synchronized(hashByContentPath)[path] = hash;
+                cdnMatch = contentUrl.Split('?')[0];
+            }
+            else
+            {
+                var path = contentUrl;
+                path = HostingEnvironment.MapPath(path);
+
+                object hash;
+                hash = hashByContentPath[path];
+                if (hash == null)
+                {
+                    if (File.Exists(path))
+                        hash = GetFileSHA1(path);
+                    else
+                        hash = DateTime.Now.ToString("yyyymmddhh");
+
+                    Hashtable.Synchronized(hashByContentPath)[path] = hash;
+                }
+
+                contentUrl = VirtualPathUtility.ToAbsolute(contentUrl) + "?v=" + (string)hash;
+                if (!cdnEnabled)
+                    return contentUrl;
             }
 
-            return VirtualPathUtility.ToAbsolute(contentUrl) + "?v=" + (string)hash;
+            if (cdnMatch[0] == '/')
+            {
+                var root = VirtualPathUtility.ToAbsolute("~/");
+                if (cdnMatch.StartsWith(root, StringComparison.OrdinalIgnoreCase))
+                    cdnMatch = cdnMatch.Substring(root.Length);
+            }
+            else
+                cdnMatch = cdnMatch.Substring(2);
+
+            if (!cdnFilter.IsMatch(cdnMatch.Replace('/', Path.DirectorySeparatorChar)))
+                return contentUrl;
+
+#if ASPNETCORE
+            var contextAccessor = Dependency.TryResolve<Microsoft.AspNetCore.Http.IHttpContextAccessor>();
+            bool isSecureConnection = contextAccessor != null && contextAccessor.HttpContext != null &&
+                contextAccessor.HttpContext.Request.IsHttps;
+#else
+            bool isSecureConnection = HttpContext.Current != null && 
+                HttpContext.Current.Request.IsSecureConnection;
+#endif
+            string cdnRoot = isSecureConnection ? cdnHttps : cdnHttp;
+            contentUrl = VirtualPathUtility.ToAbsolute(contentUrl);
+            return UriHelper.Combine(cdnRoot, contentUrl);
         }
     }
 }
