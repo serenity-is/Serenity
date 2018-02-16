a53f91bfc1326b94408a9796841ace8d17812d34
volkan.ceylan <volkan.ceylan@marmara.edu.tr>
2018-02-16 09:11:23 +0300

    added module attribute that will be used to auto determine local text prefix and lookup script keys

---
+++ Serenity.Core/ComponentModel/Common/ModuleAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Serenity.ComponentModel
+{
+    public class ModuleAttribute : Attribute
+    {
+        public ModuleAttribute(string module)
+        {
+            this.Value = module;
+        }
+
+        public string Value { get; private set; }
+    }
+}
