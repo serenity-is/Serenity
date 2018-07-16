d4a3169dcb76b14af8520640ca63f8855c09702b
volkan.ceylan <volkan.ceylan@marmara.edu.tr>
2018-07-16 20:21:19 +0300

    check value of readonly property before setting item read only, closes #3726

--- Serenity.Data.Entity/PropertyGrid/BasicPropertyProcessor/BasicPropertyProcessor.ReadOnly.cs
+++ Serenity.Data.Entity/PropertyGrid/BasicPropertyProcessor/BasicPropertyProcessor.ReadOnly.cs
@@ -8,7 +8,7 @@ namespace Serenity.PropertyGrid
         private void SetReadOnly(IPropertySource source, PropertyItem item)
         {
             var attr = source.GetAttribute<ReadOnlyAttribute>();
-            if (attr != null)
+            if (attr != null && attr.IsReadOnly)
                 item.ReadOnly = true;
         }
     }
