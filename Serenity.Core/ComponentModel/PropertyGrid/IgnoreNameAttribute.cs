691ff2c0de296f98b353caa23c19384070004ce4
volkanceylan <volkanceylan@gmail.com>
2019-03-30 16:40:24 +0300

    recommended method to ignore checking a property name when form/columns class has a BasedOnRow(CheckNames = true) is now [IgnoreName], not [NotMapped] attribute, though old one still works for compability.

---
+++ Serenity.Core/ComponentModel/PropertyGrid/IgnoreNameAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Serenity.ComponentModel
+{
+    /// <summary>
+    /// Skips checking a property name. This is usually used along with BasedOnRow attribute
+    /// CheckNames = true to skip checking property name if it matches a field property name in the row.
+    /// </summary>
+    public class IgnoreNameAttribute : Attribute
+    {
+    }
+}
