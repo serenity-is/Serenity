7f0cd45c7560e040008be5ec6f61a805931ef437
Jin <it.minhhung@gmail.com>
2019-06-06 20:17:10 +0700

    ability to add custom data

--- Serenity.Services/Models/ServiceRequest.cs
+++ Serenity.Services/Models/ServiceRequest.cs
@@ -1,7 +1,10 @@
 ﻿
+using System.Collections.Generic;
+
 namespace Serenity.Services
 {
     public class ServiceRequest
     {
+        public Dictionary<string, object> CustomData { get; set; }
     }
 }
