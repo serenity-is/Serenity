9dcf90aaf3d8eb27dd3762c4f7624b0f0ed3171c
volkanceylan <volkanceylan@gmail.com>
2019-12-08 17:52:08 +0300

    texts for new view mode

--- Serenity.Web/Texts/Texts.Controls.EntityDialog.cs
+++ Serenity.Web/Texts/Texts.Controls.EntityDialog.cs
@@ -16,11 +16,13 @@ namespace Serenity.Web
                 public static LocalText UpdateButton = "Update";
                 public static LocalText ApplyChangesButton = "Apply Changes";
                 public static LocalText DeleteButton = "Delete";
+                public static LocalText EditButton = "Edit";
                 public static LocalText LocalizationButton = "Localization";
                 public static LocalText LocalizationBack = "Back to Form";
                 public static LocalText LocalizationConfirmation = "Save changes to translations?";
                 public static LocalText NewRecordTitle = "New {0}";
                 public static LocalText EditRecordTitle = "Edit {0}{1}";
+                public static LocalText ViewRecordTitle = "View {0}{1}";
             }
         }
     }
