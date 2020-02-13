5098352d114d518ef3e028b5ef8dce94b9b47b9a
arifsamitekinkaya <arif.tekinkaya@serenity.is>
2020-02-13 11:31:18 +0300

    adding missing field HeaderHtmlUrl

--- Serenity.Web/Reporting/HtmlToPdfConverter.cs
+++ Serenity.Web/Reporting/HtmlToPdfConverter.cs
@@ -108,6 +108,12 @@ namespace Serenity.Reporting
             else
                 args.Add("--no-background");
 
+            if (HeaderHtmlUrl != null)
+            {
+                args.Add("--header-html");
+                args.Add(HeaderHtmlUrl);
+            }
+
             if (FooterHtmlUrl != null)
             {
                 args.Add("--footer-html");
@@ -197,6 +203,7 @@ namespace Serenity.Reporting
         public string MarginRight { get; set; }
         public string MarginBottom { get; set; }
         public string MarginTop { get; set; }
+        public string HeaderHtmlUrl { get; set; }
         public string FooterHtmlUrl { get; set; }
 
         public Dictionary<string, string> FooterHeaderReplace { get; private set; }
