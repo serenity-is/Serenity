8717aba47e12ace5d9d3c834f83897118d0afa9e
volkanceylan <volkanceylan@gmail.com>
2020-05-06 16:16:00 +0300

    test for bs3/4 detection

---
+++ Serenity.Scripts/Tests/CoreLib/Q/Q.Dialogs-bs3-4-detection.test.js
@@ -0,0 +1,111 @@
+beforeEach(() => {
+    jest.resetModules();
+});
+
+afterEach(() => {
+    jest.resetModules();
+});
+
+function setupDummyJQueryForModal(callback) {
+    global.$ = global.jQuery = function dummyJQueryForModal(html) {
+        return {
+            _html: html,
+            html: function() {
+                return null;
+            },
+            eq: function(index) {
+                return this;
+            },
+            find: function(selector) {
+                return this;
+            },
+            click: function() {
+            },
+            appendTo: function(target) {
+                return this;
+            },
+            modal: function() {
+                expect(this._html).toBe(html);
+                callback(html);
+            }
+        }
+    }
+}
+
+test('BS3 is detected when modal version starts with 3', function() {
+
+    var Q = require("SerenityCoreLibBase").Q;
+
+    var passedHtml;
+    setupDummyJQueryForModal(function(html) {
+        passedHtml = html;
+    });
+    global.$.fn = {
+        modal: {
+            Constructor: {
+                VERSION: '3.3.1'
+            }
+        }
+    }    
+
+    Q.alert("hello");
+
+    expect(passedHtml).not.toBeNull();
+
+    var idx1 = passedHtml.indexOf('class="close"');
+    var idx2 = passedHtml.indexOf('<h5');
+    expect(idx1).toBeGreaterThan(-1);
+    expect(idx2).toBeGreaterThan(idx1);
+});
+
+
+test('BS4 is detected when modal version does not exist', function() {
+    
+    var Q = require("SerenityCoreLibBase").Q;
+
+    var passedHtml;
+    setupDummyJQueryForModal(function(html) {
+        passedHtml = html;
+    });
+    global.$.fn = {
+        modal: {
+        }
+    }        
+
+    Q.alert("hello");
+
+    expect(passedHtml).not.toBeNull();
+
+    var idx1 = passedHtml.indexOf('class="close"');
+    var idx2 = passedHtml.indexOf('<h5');
+    expect(idx1).toBeGreaterThan(-1);
+    expect(idx2).toBeGreaterThan(-1);
+    expect(idx1).toBeGreaterThan(idx2);
+});
+
+test('BS4 is detected when modal version is something other than 3', function() {
+    
+    var Q = require("SerenityCoreLibBase").Q;
+
+    var passedHtml;
+    setupDummyJQueryForModal(function(html) {
+        passedHtml = html;
+    });
+    global.$.fn = {
+        modal: {
+            Constructor: {
+                VERSION: '4.1.0'
+            }
+        }
+    }           
+
+    Q.alert("hello");
+
+    expect(passedHtml).not.toBeNull();
+
+    var idx1 = passedHtml.indexOf('class="close"');
+    var idx2 = passedHtml.indexOf('<h5');
+    expect(idx1).toBeGreaterThan(-1);
+    expect(idx2).toBeGreaterThan(-1);
+    expect(idx1).toBeGreaterThan(idx2);
+});
