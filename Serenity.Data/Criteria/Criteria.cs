8fc2e264db7b84fa53b0a01ed9e048f43cb5b4a0
volkanceylan <volkanceylan@gmail.com>
2020-07-17 17:17:48 +0300

    change parameter name as c# analyzer thinks this is localizable so raises a warning

--- Serenity.Data/Criteria/Criteria.cs
+++ Serenity.Data/Criteria/Criteria.cs
@@ -41,11 +41,11 @@
         ///   field name, but it can also be a criteria text pre-generated.</summary>
         /// <remarks>
         ///   Usually used like: <c>new Criteria("fieldname") >= 5</c>.</remarks>
-        /// <param name="text">
+        /// <param name="expression">
         ///   A field name or criteria condition (can be null)</param>
-        public Criteria(string text)
+        public Criteria(string expression)
         {
-            this.expression = text;
+            this.expression = expression;
         }
 
         /// <summary>
