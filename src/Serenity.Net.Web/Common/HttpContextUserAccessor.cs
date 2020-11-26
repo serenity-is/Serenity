dd297e4217c7ed49f7a3c3f28c55d63e4a64cc82
volkanceylan <volkanceylan@gmail.com>
2020-11-27 02:13:03 +0300

    add HttpContextUserAccessor

---
+++ src/Serenity.Net.Web/Common/HttpContextUserAccessor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Serenity.Abstractions;
+using System.Security.Claims;
+
+namespace Serenity.Web
+{
+    public class HttpContextUserAccessor : IUserAccessor
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public HttpContextUserAccessor(IHttpContextAccessor httpContextAccessor = null)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public ClaimsPrincipal User => httpContextAccessor?.HttpContext?.User;
+    }
+}
+
