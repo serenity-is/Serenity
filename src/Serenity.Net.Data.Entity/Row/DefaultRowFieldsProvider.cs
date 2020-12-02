a5fb7e7bec003ce21e356781384c074ead32f989
volkanceylan <volkanceylan@gmail.com>
2020-12-02 12:20:22 +0300

    create dictionary

--- src/Serenity.Net.Data.Entity/Row/DefaultRowFieldsProvider.cs
+++ src/Serenity.Net.Data.Entity/Row/DefaultRowFieldsProvider.cs
@@ -12,6 +12,7 @@ namespace Serenity.Data
 
         public DefaultRowFieldsProvider(IServiceProvider serviceProvider)
         {
+            this.byType = new ConcurrentDictionary<Type, RowFieldsBase>();
             this.serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
         }
 
