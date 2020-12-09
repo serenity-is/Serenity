16644b7edfa8c024a8d91399ab484c3f2e870989
volkanceylan <volkanceylan@gmail.com>
2020-12-09 18:34:23 +0300

    :up: 5.0.3

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -3,5 +3,5 @@
 [assembly: AssemblyCompany("Serenity")]
 [assembly: AssemblyProduct("Serenity Platform")]
 [assembly: AssemblyCopyright("Copyright © Volkan Ceylan")]
-[assembly: AssemblyVersion("5.0.2")]
-[assembly: AssemblyFileVersion("5.0.2")]
+[assembly: AssemblyVersion("5.0.3")]
+[assembly: AssemblyFileVersion("5.0.3")]
