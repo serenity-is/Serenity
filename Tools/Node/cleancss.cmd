8ebe31d3480130b6acb62e378b320301a2402a4e
volkanceylan <volkanceylan@gmail.com>
2020-12-09 17:01:23 +0300

    changing build system for Serenity.Scripts

--- Tools/Node/cleancss.cmd
+++
@@ -1,6 +0,0 @@
-:: Created by npm, please don't edit manually.
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe" "%~dp0\node_modules\clean-css\bin\cleancss" %*
-) ELSE (
-  node "%~dp0\node_modules\Clean-css\bin\cleancss" %*
-)
--- Tools/Node/lessc.cmd
+++
@@ -1,5 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\less\bin\lessc" %*
-) ELSE (
-  node  "%~dp0\node_modules\less\bin\lessc" %*
-)
