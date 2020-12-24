17ffd175ca659e2bef1b871e28d57e3782bb485d
volkanceylan <volkanceylan@gmail.com>
2020-12-24 19:31:35 +0300

    add ability to set a local default dialect, just like row fields provider

--- src/Serenity.Net.Data/SqlHelpers/SqlSettings.cs
+++ src/Serenity.Net.Data/SqlHelpers/SqlSettings.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Threading;
 
 namespace Serenity.Data
 {
@@ -8,7 +9,14 @@ namespace Serenity.Data
     /// </summary>
     public static class SqlSettings
     {
-        private static ISqlDialect defaultDialect = new SqlServer2012Dialect();
+        private static ISqlDialect defaultDialect;
+        private static readonly AsyncLocal<ISqlDialect> localDialect;
+
+        static SqlSettings()
+        {
+            defaultDialect = new SqlServer2012Dialect();
+            localDialect = new AsyncLocal<ISqlDialect>();
+        }
 
         /// <summary>
         /// Gets or sets a value indicating whether to automatically quote identifiers.
@@ -25,14 +33,31 @@ namespace Serenity.Data
         /// The default command timeout.
         /// </value>
         public static int? DefaultCommandTimeout { get; set; }
-
+       
         /// <summary>
-        /// The default dialect
+        /// The default dialect, returns the local dialect if any set through
+        /// SetLocal, the default dialect otherwise.
+        /// This should be only set on application start.
+        /// Local dialect should be used for unit tests.
         /// </summary>
         public static ISqlDialect DefaultDialect
         {
-            get => defaultDialect;
+            get => localDialect.Value ?? defaultDialect; 
             set => defaultDialect = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        /// <summary>
+        /// Sets local dialect for current thread and async context. 
+        /// Useful for background tasks, async methods, and testing to 
+        /// set dialect locally and for auto spawned threads.
+        /// </summary>
+        /// <param name="dialect">Dialect. Can be null.</param>
+        /// <returns>Old local dialect if any.</returns>
+        public static ISqlDialect SetLocalDialect(ISqlDialect dialect)
+        {
+            var old = localDialect.Value;
+            localDialect.Value = dialect;
+            return old;
         }
     }
 }
