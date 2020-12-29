9bcea59782fe8b0a60a36934021ffa58e3dbb9ed
volkanceylan <volkanceylan@gmail.com>
2020-12-29 09:25:28 +0300

    converted Serenity.Assets into RCL

--- src/Serenity.Net.Web/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Serenity Platform - Web Library")]
-[assembly: AssemblyDescription(
-    "Contains classes for MVC based Web environment...")]
-[assembly: ComVisible(false)]
--- src/SharedAssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyCompany("Serenity")]
-[assembly: AssemblyProduct("Serenity Platform")]
-[assembly: AssemblyCopyright("Copyright © Volkan Ceylan")]
-[assembly: AssemblyVersion("5.0.13")]
-[assembly: AssemblyFileVersion("5.0.13")]
