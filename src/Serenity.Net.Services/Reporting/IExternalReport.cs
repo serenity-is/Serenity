c25fb26ffe04f1ed057583d5fea629f8c7d8dcc9
volkanceylan <volkanceylan@gmail.com>
2021-01-10 15:08:49 +0300

    move IExternalReport interface to Serenity.Services

---
+++ src/Serenity.Net.Services/Reporting/IExternalReport.cs
@@ -0,0 +1,10 @@
+﻿namespace Serenity.Reporting
+{
+    /// <summary>
+    /// This interface marks a report class as a report that should open an external URL, e.g. an SSRS report url, or any arbitrary site
+    /// The URL should be returned from GetData() method of report class.
+    /// </summary>
+    public interface IExternalReport
+    {
+    }
+}
