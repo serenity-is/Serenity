aca2e7426757613bfd5d39604f634c18ba53c037
volkanceylan <volkanceylan@gmail.com>
2021-01-10 17:06:24 +0300

    move DataAuditLogAttribute to Serenity.Net.Data

---
+++ src/Serenity.Net.Data/Mapping/DataAuditLogAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Serenity.Data
+{
+    /// <summary>
+    /// Indicates that the entity should be audit logged.
+    /// This feature is only available in premium app.
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
+    public class DataAuditLogAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataAuditLogAttribute"/> class.
+        /// </summary>
+        public DataAuditLogAttribute()
+        {
+        }
+    }
+}
