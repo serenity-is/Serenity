841e515971fa6029c8087bce868c9effd0bb8ace
volkanceylan <volkanceylan@gmail.com>
2021-01-10 15:03:18 +0300

    add IDataReportRenderer abstraction for rendering a data only report to excel format

---
+++ src/Serenity.Net.Services/Reporting/IDataReportExcelRenderer.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Serenity.Reporting
+{
+    public interface IDataReportExcelRenderer
+    {
+        /// <summary>
+        /// Renders the specified report to Excel format.
+        /// </summary>
+        /// <param name="report">The report.</param>
+        /// <returns></returns>
+        byte[] Render(IDataOnlyReport report);
+    }
+}
