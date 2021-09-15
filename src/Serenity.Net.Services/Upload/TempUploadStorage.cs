afb5b9fa6f30f8abd5dab1c758e21a1fe631b0c4
volkanceylan <volkanceylan@gmail.com>
2021-09-15 18:06:26 +0300

    swallow exception as this may cause startup errors and result in application pool crash while trying to create temporary folder, better to ignore than crash

--- src/Serenity.Net.Services/Upload/TempUploadStorage.cs
+++ src/Serenity.Net.Services/Upload/TempUploadStorage.cs
@@ -10,8 +10,17 @@ namespace Serenity.Web
         {
             if (!Directory.Exists(RootPath))
             {
-                Directory.CreateDirectory(RootPath);
-                File.WriteAllText(Path.Combine(RootPath, ".temporary"), "");
+                try
+                {
+                    Directory.CreateDirectory(RootPath);
+                    File.WriteAllText(Path.Combine(RootPath, ".temporary"), "");
+                }
+                catch
+                {
+                    // swallow exception as this causes startup errors
+                    // and application pool crashes if upload folder
+                    // can't be accessed, better to ignore
+                }
             }
         }
 
