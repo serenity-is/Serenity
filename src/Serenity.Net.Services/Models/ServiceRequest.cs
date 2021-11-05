d5151f36e43ea7c7e1ca85dbcbec0368539f07a5
Furkan Evran <furkan.evran@serenity.is>
2021-11-05 17:32:55 +0300

    added ShouldDeserializeExtension to JsonRowConverter for deserializing extension fields
    added CustomData to ServiceRequest and ServiceResponse

--- src/Serenity.Net.Services/Models/ServiceRequest.cs
+++ src/Serenity.Net.Services/Models/ServiceRequest.cs
@@ -1,7 +1,18 @@
 ﻿
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
 namespace Serenity.Services
 {
     public class ServiceRequest
     {
+        /// <summary>
+        /// A custom data dictionary. Please consider subclassing the service request
+        /// before passing data via this property. This should be only used in limited 
+        /// cases where subclassing is not feasible.
+        /// Another option could be to add a [JsonExtensionData] but that could open
+        /// way to the side effect ignoring typos.
+        /// </summary>
+        public Dictionary<string, object> CustomData { get; set; }
     }
 }
--- src/Serenity.Net.Services/Models/ServiceResponse.cs
+++ src/Serenity.Net.Services/Models/ServiceResponse.cs
@@ -1,8 +1,18 @@
-﻿
+﻿using System.Collections.Generic;
+
 namespace Serenity.Services
 {
     public class ServiceResponse
     {
         public ServiceError Error { get; set; }
+
+        /// <summary>
+        /// A custom data dictionary. Please consider subclassing the service response
+        /// before passing data via this property. This should be only used in limited 
+        /// cases where subclassing is not feasible.
+        /// Another option could be to add a [JsonExtensionData] but that could open
+        /// way to the side effect ignoring typos.
+        /// </summary>
+        public Dictionary<string, object> CustomData { get; set; }
     }
 }
