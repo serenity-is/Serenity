e33a9a997ab7fd2ab10e6726ac9917880d010ea1
D Faruque Ahmad <dfaruque@gmail.com>
2021-12-26 16:32:27 +0600

    made SqlKeywords public so that it can be used in SqlQuery ToString method overriding

--- src/Serenity.Net.Data/FluentSql/SqlKeywords.cs
+++ src/Serenity.Net.Data/FluentSql/SqlKeywords.cs
@@ -1,17 +1,53 @@
 ﻿namespace Serenity.Data
 {
-    internal static class SqlKeywords
+    /// <summary>
+    /// The SQL Keywords
+    /// </summary>
+    public static class SqlKeywords
     {
+        /// <summary>
+        /// SELECT
+        /// </summary>
         public const string Select = "SELECT ";
+        /// <summary>
+        /// DISTINCT
+        /// </summary>
         public const string Distinct = "DISTINCT ";
+        /// <summary>
+        /// FROM
+        /// </summary>
         public const string From = " \nFROM ";
+        /// <summary>
+        /// WHERE
+        /// </summary>
         public const string Where = " \nWHERE ";
+        /// <summary>
+        /// ORDER BY
+        /// </summary>
         public const string OrderBy = " \nORDER BY ";
+        /// <summary>
+        /// GROUP BY
+        /// </summary>
         public const string GroupBy = " \nGROUP BY ";
+        /// <summary>
+        /// HAVING
+        /// </summary>
         public const string Having = " \nHAVING ";
+        /// <summary>
+        /// AND
+        /// </summary>
         public const string And = " AND ";
+        /// <summary>
+        /// DESC
+        /// </summary>
         public const string Desc = " DESC";
+        /// <summary>
+        /// AS
+        /// </summary>
         public const string As = " AS ";
+        /// <summary>
+        /// NULL
+        /// </summary>
         public const string Null = "NULL";
     }
 }
