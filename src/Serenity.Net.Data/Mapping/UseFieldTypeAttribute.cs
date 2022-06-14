99d9da5f9d51ba228495f1ba8c03c26eb9f87bbc
volkanceylan <volkanceylan@gmail.com>
2022-06-14 21:03:58 +0300

    add UseFieldTypeAttribute for source gen

---
+++ src/Serenity.Net.Data/Mapping/UseFieldTypeAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace Serenity.Data
+{
+    /// <summary>
+    /// Declares that the field generated for this template property should
+    /// be of type specified.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class UseFieldTypeAttribute : Attribute
+    {
+        /// <summary>
+        /// Creates an instance of FieldTypeAttribute attribute
+        /// </summary>
+        /// <param name="fieldType">Field type to use for this property 
+        /// in generated source.</param>
+        public UseFieldTypeAttribute(Type fieldType)
+        {
+            FieldType = fieldType ?? throw new ArgumentNullException(nameof(fieldType));
+        }
+
+        /// <summary>
+        /// The field type
+        /// </summary>
+        public Type FieldType { get; private set; }
+    }
+}
