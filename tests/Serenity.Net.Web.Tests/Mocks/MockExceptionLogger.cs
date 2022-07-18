ac19bca00919dc7aef7bc30725d93e44912b5789
Furkan Evran <furkan.evran@serenity.is>
2022-07-18 12:04:25 +0300

    add tests for sensitive error messages

---
+++ tests/Serenity.Net.Web.Tests/Mocks/MockExceptionLogger.cs
@@ -0,0 +1,13 @@
+﻿namespace Serenity.Tests;
+
+public class MockExceptionLogger : IExceptionLogger
+{
+    public Exception LastException { get; private set; }
+    public string LastCategory { get; private set; }
+    
+    public void Log(Exception exception, string category)
+    {
+        LastException = exception;
+        LastCategory = category;
+    }
+}
