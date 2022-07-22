7de32bedd4bcbdb760a2d4d8dbeddfe2cbabf82e
Furkan Evran <furkan.evran@serenity.is>
2022-07-22 11:34:43 +0300

    add mocks

---
+++ tests/Serenity.Net.Web.Tests/Mocks/MockDeleteHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Serenity.Tests;
+
+public class MockDeleteHandler<TRow> : IDeleteRequestHandler
+    where TRow: IRow, new()
+{
+    public IRow Row { get; set; } = new TRow();
+
+    public DeleteRequest Request { get; } = new DeleteRequest();
+    public DeleteResponse Response { get; } = new DeleteResponse();
+    
+    public IDictionary<string, object> StateBag { get; set; } = new Dictionary<string, object>();
+    public IDbConnection Connection { get; set; }
+    public IUnitOfWork UnitOfWork { get; set; }
+    public IRequestContext Context { get; set; }
+    
+    
+}
---
+++ tests/Serenity.Net.Web.Tests/Mocks/MockUnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿namespace Serenity.Tests;
+
+public class MockUnitOfWork : IUnitOfWork
+{
+    public IDbConnection Connection { get; }
+    public event Action OnCommit;
+    public event Action OnRollback;
+    
+    public MockUnitOfWork(IDbConnection connection = null,
+        Action onCommit = null,
+        Action onRollback = null)
+    {
+        Connection = connection;
+        OnCommit = onCommit; 
+        OnRollback = onRollback;
+    }
+    
+    public IEnumerable<Delegate> OnCommitInvocationList => OnCommit?.GetInvocationList() ?? Array.Empty<Delegate>();
+    public IEnumerable<Delegate> OnRollbackInvocationList => OnRollback?.GetInvocationList() ?? Array.Empty<Delegate>();
+    
+    public void Commit()
+    {
+        OnCommit?.Invoke();
+    }
+    
+    public void Rollback()
+    {
+        OnRollback?.Invoke();
+    }
+}
