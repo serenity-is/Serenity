f36dbec72ec10d68217b8f1546f3ddf1de86792b
volkanceylan <volkanceylan@gmail.com>
2022-12-06 02:10:37 +0300

    added AutoValidateAntiforgeryIgnoreBearerAttribute

---
+++ src/Serenity.Net.Web/Mvc/AutoValidateAntiforgeryIgnoreBearerAttribute.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Serenity.Web
+{
+    /// <summary>
+    /// An attribute that causes validation of antiforgery tokens for all unsafe HTTP methods. An antiforgery
+    /// token is required for HTTP methods other than GET, HEAD, OPTIONS, and TRACE, and when
+    /// there is no cookie header and authorization is Bearer, e.g. JWT etc.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="AutoValidateAntiforgeryIgnoreBearerAttribute"/> can be applied as a global filter to trigger
+    /// validation of antiforgery tokens by default for an application. Use
+    /// <see cref="Microsoft.AspNetCore.Mvc.IgnoreAntiforgeryTokenAttribute"/> to suppress validation of the antiforgery token for
+    /// a controller or action.
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class AutoValidateAntiforgeryIgnoreBearerAttribute : Attribute, IFilterFactory, IOrderedFilter
+    {
+        /// <inheritdoc/>
+        public int Order { get; set; } = 1000;
+
+        /// <inheritdoc />
+        public bool IsReusable => true;
+
+        /// <inheritdoc />
+        public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
+        {
+            return ActivatorUtilities.CreateInstance<AutoValidateAntiforgeryIgnoreBearerFilter>(serviceProvider);
+        }
+    }
+}
---
+++ src/Serenity.Net.Web/Mvc/AutoValidateAntiforgeryIgnoreBearerFilter.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Serenity.Web
+{
+    internal class AutoValidateAntiforgeryIgnoreBearerFilter : IAsyncAuthorizationFilter, IAntiforgeryPolicy
+    {
+        private const string NotMostEffectiveFilter = "NotMostEffectiveFilter: {interface}";
+        private const string AntiforgeryTokenInvalid = "Skipping the execution of current filter as its not the most effective filter implementing the policy {FilterPolicy}.";
+
+        private readonly IAntiforgery antiforgery;
+        private readonly ILogger logger;
+
+        public AutoValidateAntiforgeryIgnoreBearerFilter(IAntiforgery antiforgery, ILoggerFactory loggerFactory)
+        {
+            if (antiforgery == null)
+                throw new ArgumentNullException(nameof(antiforgery));
+
+            this.antiforgery = antiforgery ?? throw new ArgumentNullException(nameof(antiforgery));
+            logger = loggerFactory.CreateLogger(GetType());
+        }
+
+        public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (!context.IsEffectivePolicy<IAntiforgeryPolicy>(this))
+            {
+                logger.LogWarning(NotMostEffectiveFilter, nameof(IAntiforgeryPolicy));
+                return;
+            }
+
+            if (ShouldValidate(context))
+            {
+                try
+                {
+                    await antiforgery.ValidateRequestAsync(context.HttpContext);
+                }
+                catch (AntiforgeryValidationException exception)
+                {
+                    logger.LogWarning(exception, AntiforgeryTokenInvalid, exception.Message);
+                    context.Result = new AntiforgeryValidationFailedResult();
+                }
+            }
+        }
+
+        protected virtual bool ShouldValidate(AuthorizationFilterContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            var method = context.HttpContext.Request.Method;
+            if (HttpMethods.IsGet(method) ||
+                HttpMethods.IsHead(method) ||
+                HttpMethods.IsTrace(method) ||
+                HttpMethods.IsOptions(method))
+            {
+                return false;
+            }
+
+            string authorization = context.HttpContext.Request.Headers[HeaderNames.Authorization];
+            var cookie = context.HttpContext.Request.Headers[HeaderNames.Cookie];
+            
+            if (!string.IsNullOrEmpty(authorization) && 
+                authorization.StartsWith("Bearer ", StringComparison.InvariantCulture) && 
+                string.IsNullOrEmpty(cookie))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
