454c6afff731d69e92ad112bd108d2e7ad6292cd
volkanceylan <volkanceylan@gmail.com>
2022-12-30 00:24:11 +0300

    Added the TransactionlessUnitOfWork which is an implementation of IUnitOfWork that does not actually start any transaction. Should only be used in edge cases to call methods that expect a IUnitOfWork instance, but you are sure that the whole operation will be readonly, or you don't really need a transaction. If using with save handlers etc it might be risky. Make sure there are no service behaviors that run behind the scenes which may write or require a transaction.

---
+++ src/Serenity.Net.Data/Connections/TransactionlessUnitOfWork.cs
@@ -0,0 +1,90 @@
+﻿namespace Serenity.Data
+{
+    /// <summary>
+    /// Unit of work implementation without an underlying actual transaction.
+    /// Use with care only to pass a IUnitOfWork instance to some methods
+    /// that you don't want to actually start a transaction.
+    /// </summary>
+    /// <seealso cref="IDisposable" />
+    /// <seealso cref="IUnitOfWork" />
+    public class TransactionlessUnitOfWork : IDisposable, IUnitOfWork
+    {
+        private readonly IDbConnection connection;
+        private Action onCommit;
+        private Action onRollback;
+        private bool commitCalled;
+
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        /// <param name="connection">The connection.</param>
+        /// <exception cref="ArgumentNullException">connection</exception>
+        public TransactionlessUnitOfWork(IDbConnection connection)
+        {
+            this.connection = connection ?? throw new ArgumentNullException(nameof(connection));
+        }
+
+        /// <summary>
+        /// Gets the connection.
+        /// </summary>
+        /// <value>
+        /// The connection.
+        /// </value>
+        public IDbConnection Connection => connection;
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            onCommit = null;
+            try
+            {
+                onRollback?.Invoke();
+            }
+            finally
+            {
+                onRollback = null;
+            }
+        }
+
+        /// <summary>
+        /// Does nothing other than calling onCommit events as there
+        /// is no underlying transaction.
+        /// </summary>
+        public void Commit()
+        {
+            if (commitCalled)
+                throw new InvalidOperationException("Commit is already called!");
+
+            commitCalled = true;
+
+            try
+            {
+                onCommit?.Invoke();
+            }
+            finally
+            {
+                onCommit = null;
+            }
+        }
+
+        /// <summary>
+        /// Occurs when Commit is called as there is no underlying transaction.
+        /// </summary>
+        public event Action OnCommit
+        {
+            add { onCommit += value; }
+            remove { onCommit -= value; }
+        }
+
+        /// <summary>
+        /// Occurs when Dispose is called as there is no underlying transaction.
+        /// </summary>
+        public event Action OnRollback
+        {
+            add { onRollback += value; }
+            remove { onRollback -= value; }
+        }
+    }
+}
