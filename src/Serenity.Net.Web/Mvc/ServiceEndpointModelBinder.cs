a185c6d808452e2a5e3379cd465e15e56cdb44b4
volkanceylan <volkanceylan@gmail.com>
2023-01-04 02:13:34 +0300

    add back ServiceEndpointModelBinderProvider as it might be referenced in Serene / StartSharp

---
+++ src/Serenity.Net.Web/Mvc/ServiceEndpointModelBinder.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Serenity.Services
+{
+    /// <summary>
+    /// Provides model binding for service endpoints
+    /// </summary>
+    public class ServiceEndpointModelBinderProvider : IModelBinderProvider
+    {
+        /// <inheritdoc/>
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            var type = context.Metadata.ModelType;
+            if (typeof(IDbConnection).IsAssignableFrom(type) ||
+                typeof(IUnitOfWork).IsAssignableFrom(type))
+            {
+                return ServiceEndpointNullModelBinder.Instance;
+            }
+
+            return null;
+        }
+    }
+}
