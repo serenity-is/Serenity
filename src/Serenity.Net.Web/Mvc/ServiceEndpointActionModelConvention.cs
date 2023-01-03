95a4d871974335a6d6d301335f6bb3cd02f15790
volkanceylan <volkanceylan@gmail.com>
2023-01-04 00:41:52 +0300

    try improving binding convention of IDbConnection and IUnitOfWork and ServiceRequest types in ServiceEndpoints so that api browsers like swagger etc. can analyse it better

---
+++ src/Serenity.Net.Web/Mvc/ServiceEndpointActionModelConvention.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Serenity.Services
+{
+    /// <summary>
+    /// Action model convention for <see cref="ServiceEndpoint"/> actions
+    /// </summary>
+    public class ServiceEndpointActionModelConvention : IActionModelConvention
+    {
+        /// <inheritdoc/>
+        public void Apply(ActionModel action)
+        {
+            if (!action.Controller.ControllerType.IsSubclassOf(typeof(ServiceEndpoint)))
+                return;
+
+            foreach (var parameter in action.Parameters)
+            {
+                var paramType = parameter.ParameterInfo.ParameterType;
+                
+                var isServiceRequest = typeof(ServiceRequest).IsAssignableFrom(paramType);
+                if (isServiceRequest && 
+                    !action.Filters.OfType<JsonRequestAttribute>().Any())
+                {
+                    action.Filters.Add(new JsonRequestAttribute());
+                }
+
+                var bindingSource = parameter.BindingInfo?.BindingSource;
+                if (bindingSource == null)
+                {
+                    if (isServiceRequest)
+                    {
+                        parameter.BindingInfo ??= new BindingInfo() 
+                        { 
+                            BindingSource = BindingSource.Body,
+                            BinderType = typeof(ServiceEndpointNullModelBinder)
+                        };
+                    }
+                    else if (typeof(IDbConnection).IsAssignableFrom(paramType) ||
+                        typeof(IUnitOfWork).IsAssignableFrom(paramType))
+                    {
+                        parameter.BindingInfo ??= new BindingInfo() 
+                        { 
+                            BindingSource = BindingSource.Special,
+                            BinderType = typeof(ServiceEndpointNullModelBinder)
+                        };
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Serenity.Net.Web/Mvc/ServiceEndpointNullModelBinder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Threading.Tasks;
+
+namespace Serenity.Services
+{
+    /// <summary>
+    /// Null model binder for interface arguments
+    /// </summary>
+    public class ServiceEndpointNullModelBinder : IModelBinder
+    {
+        /// <summary>
+        /// Default instance
+        /// </summary>
+        public static readonly ServiceEndpointNullModelBinder Instance = new();
+
+        /// <inheritdoc/>
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            bindingContext.Result = ModelBindingResult.Success(null);
+            return Task.CompletedTask;
+        }
+    }
+}
