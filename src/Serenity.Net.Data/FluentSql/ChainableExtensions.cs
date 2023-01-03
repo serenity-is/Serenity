defe7fdd6dfcfa0f8034fd05c49f652332916596
volkanceylan <volkanceylan@gmail.com>
2023-01-04 00:51:47 +0300

    [BREAKING CHANGE] rename MethodChainingExtensions to ChainableExtensions and only support single action overload as that is the only one which is used anywhere so far

---
+++ src/Serenity.Net.Data/FluentSql/ChainableExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Serenity
+{
+    /// <summary>
+    /// Contains method chaining extensions for objects like SQL queries that implement
+    /// <see cref="IChainable"/>
+    /// </summary>
+    public static class ChainableExtensions
+    {
+        /// <summary>
+        /// Allows to reference the call chain object itself without breaking a call chain.
+        /// </summary>
+        /// <param name="chain">Chaining object</param>
+        /// <param name="action">An action that will be called with the chain object as parameter.</param>
+        /// <returns>
+        /// The query itself.</returns>
+        public static TChain With<TChain>(this TChain chain, Action<TChain> action)
+            where TChain : IChainable
+        {
+            if (action == null)
+                throw new ArgumentNullException("action");
+
+            action(chain);
+
+            return chain;
+        }
+
+    }
+}
