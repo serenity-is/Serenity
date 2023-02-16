db1fd49db31a7369737226fe2b37c15e281f8c1f
volkanceylan <volkanceylan@gmail.com>
2023-02-16 13:11:49 +0300

    typo

--- src/Serenity.Net.Services/Reporting/HtmlToPdf/IHtmlReportRenderUrlBuilder.cs
+++ src/Serenity.Net.Services/Reporting/HtmlToPdf/IHtmlReportRenderUrlBuilder.cs
@@ -15,7 +15,7 @@ public interface IHtmlReportRenderUrlBuilder
     /// <param name="reportKey">The report key, if it is not specified in report type as an attribute.
     /// Will be ignored if report will be rendered directly, e.g. not via a callback.</param>
     /// <param name="reportParams">The set of report params usually a serialized JSON object 
-    /// to be passed to the URL callback. These params should be already be applied to the passed
+    /// to be passed to the URL callback. These params should already be applied to the passed
     /// report instance. Will be ignored if the report is rendered directly, e.g. without a callback.</param>
     string GetRenderUrl(IReport report, string reportKey, string reportParams);
 
