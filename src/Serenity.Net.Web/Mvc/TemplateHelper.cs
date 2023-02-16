324d8a2d324817cfe639663032e77427ebeb2847
volkanceylan <volkanceylan@gmail.com>
2023-02-16 17:02:20 +0300

    improve TemplateHelper.RenderViewToString, so that it uses ActionContext if any, and other context variables etc.

--- src/Serenity.Net.Web/Mvc/TemplateHelper.cs
+++ src/Serenity.Net.Web/Mvc/TemplateHelper.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -17,49 +18,66 @@ namespace Serenity.Web;
 /// </summary>
 public static class TemplateHelper
 {
-    private static ActionContext GetActionContext(IServiceProvider serviceProvider)
-    {
-        var httpContext = new DefaultHttpContext
-        {
-            RequestServices = serviceProvider
-        };
-        return new ActionContext(httpContext, new RouteData(), new ActionDescriptor());
-    }
-
     /// <summary>
     /// Renders a CSHTML view to string
     /// </summary>
     /// <param name="serviceProvider">Service provider</param>
     /// <param name="viewName">View name</param>
-    /// <param name="model">Model object</param>
+    /// <param name="model">Model object, can also be ViewDataDictionary containing the actual model</param>
+    /// <param name="beforeRender">An optional callback which will be passed the view context before rendering starts</param>
     /// <exception cref="InvalidOperationException"></exception>
-    public static string RenderViewToString(IServiceProvider serviceProvider, string viewName, object model)
+    public static string RenderViewToString(IServiceProvider serviceProvider, string viewName, object model,
+        Action<ViewContext> beforeRender = null)
     {
-        var actionContext = GetActionContext(serviceProvider);
-        var viewEngine = serviceProvider.GetService<IRazorViewEngine>();
-        var viewEngineResult = viewEngine.GetView(executingFilePath: viewName, viewPath: viewName, isMainPage: false);
+        if (serviceProvider is null)
+            throw new ArgumentNullException(nameof(serviceProvider));
 
-        if (!viewEngineResult.Success)
-            throw new InvalidOperationException(string.Format(CultureInfo.CurrentCulture, 
-                "Couldn't find view '{0}'", viewName));
+        var viewEngine = serviceProvider.GetRequiredService<IRazorViewEngine>();
+        var actionContextAccessor = serviceProvider.GetService<IActionContextAccessor>();
+        var tempDataProvider = serviceProvider.GetRequiredService<ITempDataProvider>();
 
-        var viewData = new ViewDataDictionary(
-            metadataProvider: new EmptyModelMetadataProvider(),
-            modelState: new ModelStateDictionary())
+        ViewEngineResult viewEngineResult;
+        if (actionContextAccessor?.ActionContext is ActionContext actionContext)
+            viewEngineResult = viewEngine.FindView(actionContext, viewName, false);
+        else
         {
-            Model = model
-        };
+            viewEngineResult = viewEngine.GetView(executingFilePath: viewName, viewPath: viewName, isMainPage: false);
 
-        using StringWriter sw = new();
-        var engine = serviceProvider.GetService<ICompositeViewEngine>();
+            var httpContext = serviceProvider?.GetService<IHttpContextAccessor>()?.HttpContext ??
+                new DefaultHttpContext
+                {
+                    RequestServices = serviceProvider
+                };
 
-        var viewContext = new ViewContext(actionContext, viewEngineResult.View, viewData,
-            new TempDataDictionary(actionContext.HttpContext, serviceProvider.GetService<ITempDataProvider>()),
-                sw, new HtmlHelperOptions());
+            actionContext = new ActionContext(httpContext, new RouteData(), new ActionDescriptor());
+        }
 
-        var t = viewEngineResult.View.RenderAsync(viewContext);
-        t.Wait();
+        if (viewEngineResult.View == null || (!viewEngineResult.Success))
+            throw new ArgumentNullException($"Unable to find view '{viewName}'");
 
+        if (model is not ViewDataDictionary viewData)
+        {
+            var metadataProvider = serviceProvider.GetService<IModelMetadataProvider>()
+                ?? serviceProvider.GetService<IModelMetadataProvider>() ??
+                new EmptyModelMetadataProvider();
+
+            viewData = new ViewDataDictionary(metadataProvider, actionContext.ModelState ?? new ModelStateDictionary())
+            {
+                Model = model
+            };
+        }
+
+        var view = viewEngineResult.View;
+        var tempData = new TempDataDictionary(actionContext.HttpContext, tempDataProvider);
+        
+        using var sw = new StringWriter();
+        var viewContext = new ViewContext(actionContext, view, viewData, tempData, sw, new HtmlHelperOptions());
+        
+        beforeRender?.Invoke(viewContext);
+        
+        view.RenderAsync(viewContext).Wait();
+        
+        sw.Flush();
         return sw.GetStringBuilder().ToString();
     }
 }
