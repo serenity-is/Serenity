ec39a50e2eb5c9368183f739f5f7256a91a4a07f
volkanceylan <volkanceylan@gmail.com>
2023-02-16 13:07:30 +0300

    better argument names

--- src/Serenity.Net.Services/Reporting/HtmlToPdf/IHtmlReportRenderUrlBuilder.cs
+++ src/Serenity.Net.Services/Reporting/HtmlToPdf/IHtmlReportRenderUrlBuilder.cs
@@ -12,11 +12,12 @@ public interface IHtmlReportRenderUrlBuilder
     /// Gets the render URL for the specified report
     /// </summary>
     /// <param name="report">The report.</param>
-    /// <param name="key">The report key, if it is not specified in report type as an attribute.
+    /// <param name="reportKey">The report key, if it is not specified in report type as an attribute.
     /// Will be ignored if report will be rendered directly, e.g. not via a callback.</param>
-    /// <param name="options">The set of options to be passed to the URL callback.
-    /// Will not be used if the report will be rendered directly.</param>
-    string GetRenderUrl(IReport report, string key, string options);
+    /// <param name="reportParams">The set of report params usually a serialized JSON object 
+    /// to be passed to the URL callback. These params should be already be applied to the passed
+    /// report instance. Will be ignored if the report is rendered directly, e.g. without a callback.</param>
+    string GetRenderUrl(IReport report, string reportKey, string reportParams);
 
     /// <summary>
     /// Gets cookies to forward like authentication / language preference etc. to the target converter
