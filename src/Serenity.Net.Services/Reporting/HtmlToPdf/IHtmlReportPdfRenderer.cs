c155a1ea238621cb10cdcb1802755f06f9d487fd
volkanceylan <volkanceylan@gmail.com>
2023-02-16 13:11:08 +0300

    also naming for renderer params

--- src/Serenity.Net.Services/Reporting/HtmlToPdf/IHtmlReportPdfRenderer.cs
+++ src/Serenity.Net.Services/Reporting/HtmlToPdf/IHtmlReportPdfRenderer.cs
@@ -10,9 +10,10 @@ public interface IHtmlReportPdfRenderer
     /// Renders the specified report to PDF.
     /// </summary>
     /// <param name="report">The report.</param>
-    /// <param name="key">The report key, if it is not specified in report type as an attribute.
+    /// <param name="reportKey">The report key, if it is not specified in report type as an attribute.
     /// Will be ignored if report will be rendered directly, e.g. not via a callback.</param>
-    /// <param name="options">The set of options to be passed to the URL callback.
-    /// Will not be used if report will be rendered directly.</param>
-    byte[] Render(IReport report, string key, string options);
+    /// <param name="reportParams">The set of report params usually a serialized JSON object 
+    /// to be passed to the URL callback. These params should be already be applied to the passed
+    /// report instance. Will be ignored if the report is rendered directly, e.g. without a callback.</param>
+    byte[] Render(IReport report, string reportKey, string reportParams);
 }
