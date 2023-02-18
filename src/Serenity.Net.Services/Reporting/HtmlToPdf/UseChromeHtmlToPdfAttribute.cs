7f76f0e1d245201e1240b1550332f98ea537e649
volkanceylan <volkanceylan@gmail.com>
2023-02-18 14:21:26 +0300

    add UseWKHtmlToPdfAttribute instead of UseChromeHtmlToPdfAttribute which is obsolete

--- src/Serenity.Net.Services/Reporting/HtmlToPdf/UseChromeHtmlToPdfAttribute.cs
+++ src/Serenity.Net.Services/Reporting/HtmlToPdf/UseChromeHtmlToPdfAttribute.cs
@@ -3,6 +3,14 @@
 /// <summary>
 /// Marks a report so that it should use Chrome instead of WKHTML
 /// </summary>
-public class UseChromeHtmlToPdfAttribute : Attribute
+[Obsolete("Prefer UseWKHtmlToPdfAttribute(false)")]
+public class UseChromeHtmlToPdfAttribute : UseWKHtmlToPdfAttribute
 {
+    /// <summary>
+    /// Creates a new instance of the attribute with False as the value
+    /// </summary>
+    public UseChromeHtmlToPdfAttribute()
+        : base(false)
+    {
+    }
 }
---
+++ src/Serenity.Net.Services/Reporting/HtmlToPdf/UseWKHtmlToPdfAttribute.cs
@@ -0,0 +1,23 @@
+﻿namespace Serenity.Reporting;
+
+/// <summary>
+/// Marks a report to determine if it should use WKHTML instead
+/// of another converter, e.g. Puppeeteer etc. (only if configured
+/// in service provider).
+/// </summary>
+public class UseWKHtmlToPdfAttribute : Attribute
+{
+    /// <summary>
+    /// Creates a new instance of the attribute
+    /// </summary>
+    /// <param name="value"></param>
+    public UseWKHtmlToPdfAttribute(bool value = true)
+    {
+        Value = value;
+    }
+
+    /// <summary>
+    /// Gets the value
+    /// </summary>
+    public bool Value { get; private set; }
+}
