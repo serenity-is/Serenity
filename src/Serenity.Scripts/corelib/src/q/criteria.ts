483fac7e69a72cd90eec914b1fa3e56fe13ff11e
Furkan Evran <furkan.evran@serenity.is>
2023-03-03 20:27:47 +0300

    add a fluent TypeScript criteria builder

--- src/Serenity.Scripts/corelib/src/q/criteria.ts
+++ src/Serenity.Scripts/corelib/src/q/criteria.ts
@@ -1,4 +1,6 @@
-﻿export {}
+﻿import { deepClone } from "./system";
+
+export {}
 
 export function Criteria(field: string): any[] {
     return [field];
@@ -69,5 +71,238 @@ export namespace Criteria {
         notIn = "not in",
         like = "like",
         notLike = "not like"
+    }
+
+
+    interface ICriteriaBuilderGate extends Omit<ICriteriaBuilder, "and" | "or" | "xor" | "build"> { }
+    interface ICriteriaBuilderParen extends Omit<ICriteriaBuilder, "paren" | "not" | "build"> {
+        not: Omit<ICriteriaBuilderNot, "paren">;
+    }
+    interface ICriteriaBuilderNot extends Omit<ICriteriaBuilder, "not" | "paren" | "build"> {
+        paren: Omit<ICriteriaBuilderParen, "not">;
+    }
+
+    interface ICriteriaBuilderCondition {
+        // Joins the next condition with an AND
+        and: ICriteriaBuilderGate;
+        // Joins the next condition with an OR
+        or: ICriteriaBuilderGate;
+        // Joins the next condition with an XOR
+        xor: ICriteriaBuilderGate;
+        // Builds the criteria
+        build(): any[];
+    }
+
+    interface ICriteriaBuilder {
+        // Inverts the next condition
+        not: ICriteriaBuilderNot;
+        // Parenthesizes the next condition
+        paren: ICriteriaBuilderParen;
+        // Creates an equals condition with the given field and value (value can be a field name but it must be used with the Criteria function)
+        equals(fieldName: string, value: any): ICriteriaBuilderCondition;
+        // Creates a not greater than condition with the given field and value (value can be a field name but it must be used with the Criteria function)
+        greaterThan(fieldName: string, value: any): ICriteriaBuilderCondition;
+        // Creates a not greater than or equal condition with the given field and value (value can be a field name but it must be used with the Criteria function)
+        greaterThanOrEqual(fieldName: string, value: any): ICriteriaBuilderCondition;
+        // Creates a not less than condition with the given field and value (value can be a field name but it must be used with the Criteria function)
+        lessThan(fieldName: string, value: any): ICriteriaBuilderCondition;
+        // Creates a not less than or equal condition with the given field and value (value can be a field name but it must be used with the Criteria function)
+        lessThanOrEqual(fieldName: string, value: any): ICriteriaBuilderCondition;
+        // Creates a between condition with the given field and value (values can be a field names but they must be used with the Criteria function)
+        between(fieldName: string, fromInclusive: any, toInclusive: any): ICriteriaBuilderCondition;
+        // Creates an in condition with the given field and value (values can be a field name but they must be used with the Criteria function)
+        in(fieldName: string, values: any[]): ICriteriaBuilderCondition;
+        // Creates a like condition with the given field and value (value can be a field name but it must be used with the Criteria function)
+        like(fieldName: string, value: any): ICriteriaBuilderCondition;
+        // Creates an is null condition with the given field
+        isNull(fieldName: string): ICriteriaBuilderCondition;
+        // Creates an exists condition with the given sub-query
+        exists(subQuery: any[]): ICriteriaBuilderCondition;
+    }
+
+    export function Builder(): ICriteriaBuilder;
+    export function Builder(criteria: any[]): ICriteriaBuilderCondition;
+    export function Builder(criteria?: any[]): ICriteriaBuilder | ICriteriaBuilderCondition {
+        return new CriteriaBuilderImpl(criteria);
+    }
+
+    const NegatedOperatorMap: { [key: string]: Operator } = {
+        [Operator.eq]: Operator.ne,
+        [Operator.in]: Operator.notIn,
+        [Operator.like]: Operator.notLike,
+        [Operator.isNull]: Operator.isNotNull,
+        [Operator.gt]: Operator.le,
+        [Operator.ge]: Operator.lt,
+        [Operator.lt]: Operator.ge,
+        [Operator.le]: Operator.gt
+    };
+
+    class CriteriaBuilderImpl implements ICriteriaBuilder {
+        private condition: any[];
+        private notFlag: boolean = false
+        private andFlag: boolean = false;
+        private orFlag: boolean = false;
+        private parenFlag: boolean = false;
+        private xorFlag: boolean = false;
+
+        constructor(condition?: any[]) {
+            this.condition = condition ?? [];
+        }
+
+        isEmpty(c?: any[]): boolean {
+            c ??= this.condition;
+            return c == null ||
+                c.length === 0 ||
+                (c.length === 1 &&
+                    typeof c[0] == "string" &&
+                    c[0].length === 0);
+        }
+
+        private andCondition(c1: any[], c2: any[]): any[] {
+            return [c1, Operator.and, c2];
+        }
+
+        private orCondition(c1: any[], c2: any[]): any[] {
+            return [c1, Operator.or, c2];
+        }
+
+        private xorCondition(c1: any[], c2: any[]): any[] {
+            return [c1, Operator.xor, c2];
+        }
+
+        get not(): ICriteriaBuilderGate {
+            this.notFlag = true;
+            return this;
+        }
+
+        get and(): ICriteriaBuilderGate {
+            this.andFlag = true;
+            return this;
+        }
+
+        get or(): ICriteriaBuilderGate {
+            this.orFlag = true;
+            return this;
+        }
+
+        get xor(): ICriteriaBuilderGate {
+            this.xorFlag = true;
+            return this;
+        }
+
+        get paren(): ICriteriaBuilderParen {
+            this.parenFlag = true;
+            return this;
+        }
+
+        private parenthesize(condition: any[]): any[] {
+            return this.isEmpty(condition) ? condition : [Operator.paren, condition];
+        }
+
+        private appendCondition(condition: any[]): ICriteriaBuilder {
+            if (this.notFlag) {
+                const operatorIndex = condition.length === 3 ? 1 : 0;
+                const operator: Operator = NegatedOperatorMap[condition[operatorIndex]];
+
+                if (operator)
+                    condition[operatorIndex] = operator;
+                else
+                    condition = [Operator.not, condition];
+
+                this.notFlag = false;
+            }
+
+            if (this.parenFlag) {
+                condition = this.parenthesize(condition)
+                this.parenFlag = false;
+            }
+
+            if (this.andFlag) {
+                condition = this.andCondition(this.condition, condition);
+                this.andFlag = false;
+            } else if (this.orFlag) {
+                condition = this.orCondition(this.condition, condition);
+                this.orFlag = false;
+            } else if (this.xorFlag) {
+                condition = this.xorCondition(this.condition, condition);
+                this.xorFlag = false;
+            }
+
+            this.condition = condition;
+            return this;
+        }
+
+        equals(fieldName: string, value: any): ICriteriaBuilderCondition {
+            this.appendCondition([[fieldName], Operator.eq, value]);
+            return this;
+        }
+
+        isNull(fieldName: string): ICriteriaBuilderCondition {
+            this.appendCondition([Operator.isNull, [fieldName]]);
+            return this;
+        }
+
+        exists(subQuery: any[]): ICriteriaBuilderCondition {
+            this.appendCondition([Operator.exists, subQuery]);
+            return this;
+        }
+
+        greaterThan(fieldName: string, value: any): ICriteriaBuilderCondition {
+            this.appendCondition([[fieldName], Operator.gt, value]);
+            return this;
+        }
+
+        greaterThanOrEqual(fieldName: string, value: any): ICriteriaBuilderCondition {
+            this.appendCondition([[fieldName], Operator.ge, value]);
+            return this;
+        }
+
+        lessThan(fieldName: string, value: any): ICriteriaBuilderCondition {
+            this.appendCondition([[fieldName], Operator.lt, value]);
+            return this;
+        }
+
+        lessThanOrEqual(fieldName: string, value: any): ICriteriaBuilderCondition {
+            this.appendCondition([[fieldName], Operator.le, value]);
+            return this;
+        }
+
+        between(fieldName: string, fromInclusive: any, toInclusive: any): ICriteriaBuilderCondition {
+            if (fromInclusive == null || toInclusive == null)
+                throw new Error("fromInclusive and toInclusive must not be null");
+
+            if (fromInclusive > toInclusive)
+                throw new Error("fromInclusive must be less than or equal to toInclusive");
+
+            const parenFlag = this.parenFlag;
+            this.parenFlag = false;
+            let condition;
+
+            if (this.notFlag) {
+                this.notFlag = false;
+                condition = this.orCondition(this.lessThan(fieldName, fromInclusive).build(), this.greaterThan(fieldName, toInclusive).build());
+            }
+            else
+                condition = this.andCondition(this.greaterThanOrEqual(fieldName, fromInclusive).build(), this.lessThanOrEqual(fieldName, toInclusive).build());
+
+            this.parenFlag = parenFlag;
+            this.appendCondition(condition);
+
+            return this;
+        }
+
+        in(fieldName: string, values: any[]): ICriteriaBuilderCondition {
+            this.appendCondition([[fieldName], Operator.in, values]);
+            return this;
+        }
+
+        like(fieldName: string, value: any): ICriteriaBuilderCondition {
+            this.appendCondition([[fieldName], Operator.like, value]);
+            return this;
+        }
+
+        build(): any[] {
+            return deepClone(this.condition);
+        }
     }
 }
---
+++ src/Serenity.Scripts/corelib/test/q/criteria/criteriabuilder.spec.ts
@@ -0,0 +1,398 @@
+import { Criteria } from "@/q";
+
+const Operator = Criteria.Operator;
+const Builder = Criteria.Builder;
+
+it('should correctly return equals condition', () => {
+    expect(
+        Builder()
+            .equals('a', 'b')
+            .build()
+    ).toEqual([['a'], Operator.eq, 'b']);
+});
+
+it('should correctly return equals condition with another field', () => {
+    expect(
+        Builder()
+            .equals('a', Criteria('b'))
+            .build()
+    ).toEqual([['a'], Operator.eq, ['b']]);
+});
+
+it('should correctly return not equals condition', () => {
+    expect(
+        Builder()
+            .not.equals('a', 'b')
+            .build()
+    ).toEqual([['a'], Operator.ne, 'b']);
+});
+
+it('should correctly return greater than condition', () => {
+    expect(
+        Builder()
+            .greaterThan('a', Criteria('b'))
+            .build()
+    ).toEqual([['a'], Operator.gt, ['b']]);
+});
+
+it('should correctly return not greater than condition', () => {
+    expect(
+        Builder()
+            .not.greaterThan('a', 2)
+            .build()
+    ).toEqual([['a'], Operator.le, 2]);
+});
+
+it('should correctly return greater than or equal condition', () => {
+    expect(
+        Builder()
+            .greaterThanOrEqual('a', 'b')
+            .build()
+    ).toEqual([['a'], Operator.ge, 'b']);
+});
+
+it('should correctly return not greater than or equal condition', () => {
+    expect(
+        Builder()
+            .not.greaterThanOrEqual('a', 'b')
+            .build()
+    ).toEqual([['a'], Operator.lt, 'b']);
+});
+
+it('should correctly return less than condition', () => {
+    expect(
+        Builder()
+            .lessThan('a', 'b')
+            .build()
+    ).toEqual([['a'], Operator.lt, 'b']);
+});
+
+it('should correctly return not less than condition', () => {
+    expect(
+        Builder()
+            .not.lessThan('a', 'b')
+            .build()
+    ).toEqual([['a'], Operator.ge, 'b']);
+});
+
+it('should correctly return less than or equal condition', () => {
+    expect(
+        Builder()
+            .lessThanOrEqual('a', 'b')
+            .build()
+    ).toEqual([['a'], Operator.le, 'b']);
+});
+
+it('should correctly return not less than or equal condition', () => {
+    expect(
+        Builder()
+            .not.lessThanOrEqual('a', 'b')
+            .build()
+    ).toEqual([['a'], Operator.gt, 'b']);
+});
+
+it('should correctly return in condition', () => {
+    expect(
+        Builder()
+            .in('a', ['b', 'c'])
+            .build()
+    ).toEqual([['a'], Operator.in, ['b', 'c']]);
+});
+
+it('should correctly return not in condition', () => {
+    expect(
+        Builder()
+            .not.in('a', ['b', 'c'])
+            .build()
+    ).toEqual([['a'], Operator.notIn, ['b', 'c']]);
+});
+
+it('should correctly return like condition', () => {
+    expect(
+        Builder()
+            .like('a', 'b%')
+            .build()
+    ).toEqual([['a'], Operator.like, 'b%']);
+});
+
+it('should correctly return not like condition', () => {
+    expect(
+        Builder()
+            .not.like('a', 'b%')
+            .build()
+    ).toEqual([['a'], Operator.notLike, 'b%']);
+});
+
+it('should correctly return is null condition', () => {
+    expect(
+        Builder()
+            .isNull('a')
+            .build()
+    ).toEqual([Operator.isNull, ['a']]);
+});
+
+it('should correctly return is not null condition', () => {
+    expect(
+        Builder()
+            .not.isNull('a')
+            .build()
+    ).toEqual([Operator.isNotNull, ['a']]);
+});
+
+it('should correctly return exists condition', () => {
+    expect(
+        Builder()
+            .exists(
+                Builder()
+                    .equals('a', 'b')
+                    .build())
+            .build()
+    ).toEqual([
+        Operator.exists,
+        [['a'], Operator.eq, 'b']
+    ]);
+});
+
+it('should correctly return not exists condition', () => {
+    expect(
+        Builder()
+            .not.exists(
+            Builder()
+                .equals('a', 'b')
+                .build())
+            .build()
+    ).toEqual([
+        Operator.not,
+        [Operator.exists,
+            [['a'], Operator.eq, 'b']]
+    ]);
+});
+
+it('should correctly return between condition', () => {
+    expect(
+        Builder()
+            .between('a', 'b', 'c')
+            .build()
+    ).toEqual([
+        [['a'], Operator.ge, 'b'],
+        Operator.and,
+        [['a'], Operator.le, 'c']
+    ]);
+});
+
+it('should correctly return not between condition', () => {
+    expect(
+        Builder()
+            .not.between('a', 'b', 'c')
+            .build()
+    ).toEqual([
+        [['a'], Operator.lt, 'b'],
+        Operator.or,
+        [['a'], Operator.gt, 'c']
+    ]);
+});
+
+it('should correctly return not paren between', () => {
+    expect(
+        Builder()
+            .not.paren.between('a', 'b', 'c')
+            .build()
+    ).toEqual([
+        Operator.paren,
+        [
+            [['a'], Operator.lt, 'b'],
+            Operator.or,
+            [['a'], Operator.gt, 'c']
+        ]
+    ]);
+});
+
+it('should correctly return paren condition', () => {
+    expect(
+        Builder()
+            .paren.equals('a', 'b')
+            .build()
+    ).toEqual([
+        Operator.paren,
+        [['a'], Operator.eq, 'b']
+    ]);
+});
+
+it('should correctly return not equals condition', () => {
+    expect(
+        Builder()
+            .not.equals('a', 'b')
+            .build()
+    ).toEqual([['a'], Operator.ne, 'b']);
+});
+
+it('should correctly return and condition', () => {
+    expect(
+        Builder()
+            .equals('a', 'b')
+            .and.equals('c', 'd')
+            .build()
+    ).toEqual([
+        [['a'], Operator.eq, 'b'],
+        Operator.and,
+        [['c'], Operator.eq, 'd']
+    ]);
+});
+
+it('should correctly return or condition', () => {
+    expect(
+        Builder()
+            .equals('a', 'b')
+            .or.equals('c', 'd')
+            .build()
+    ).toEqual([
+        [['a'], Operator.eq, 'b'],
+        Operator.or,
+        [['c'], Operator.eq, 'd']
+    ]);
+});
+
+it('should correctly return and condition with not', () => {
+    expect(
+        Builder()
+            .not.equals('a', 'b')
+            .and.equals('c', 'd')
+            .build()
+    ).toEqual([
+        [['a'], Operator.ne, 'b'],
+        Operator.and,
+        [['c'], Operator.eq, 'd']
+    ]);
+});
+
+it('should correctly return or condition with not', () => {
+    expect(
+        Builder()
+            .not.equals('a', 'b')
+            .or.equals('c', 'd')
+            .build()
+    ).toEqual([
+        [['a'], Operator.ne, 'b'],
+        Operator.or,
+        [['c'], Operator.eq, 'd']
+    ]);
+});
+
+it('should correctly return xor condition', () => {
+    expect(
+        Builder()
+            .equals('a', 'b')
+            .xor.equals('c', 'd')
+            .build()
+    ).toEqual([
+        [['a'], Operator.eq, 'b'],
+        Operator.xor,
+        [['c'], Operator.eq, 'd']
+    ]);
+});
+
+it('should correctly return xor condition with not', () => {
+    expect(
+        Builder()
+            .not.equals('a', 'b')
+            .xor.equals('c', 'd')
+            .build()
+    ).toEqual([
+        [['a'], Operator.ne, 'b'],
+        Operator.xor,
+        [['c'], Operator.eq, 'd']
+    ]);
+});
+
+
+it('should correctly return and condition with not and paren', () => {
+    expect(
+        Builder()
+            .not.equals('a', 'b')
+            .and.paren.equals('c', 'd')
+            .build()
+    ).toEqual([
+        [['a'], Operator.ne, 'b'],
+        Operator.and,
+        [Operator.paren,
+            [['c'], Operator.eq, 'd']]
+    ]);
+});
+
+it('should correctly return or condition with not and paren', () => {
+    expect(
+        Builder()
+            .not.equals('a', 'b')
+            .or.paren.equals('c', 'd')
+            .build()
+    ).toEqual([
+        [['a'], Operator.ne, 'b'],
+        Operator.or,
+        [Operator.paren,
+            [['c'], Operator.eq, 'd']]
+    ]);
+});
+
+it('should correctly return exists and paren', () => {
+    expect(
+        Builder()
+            .paren.exists(
+            Builder()
+                .equals('a', 'b')
+                .build()
+        )
+            .build()
+    ).toEqual([
+        Operator.paren,
+        [Operator.exists,
+            [['a'], Operator.eq, 'b']]
+    ]);
+});
+
+it('should be able to use existing criteria with and', () => {
+    const request = {
+        criteria: [['a'], Operator.eq, 'b']
+    };
+
+    expect(
+        Builder(request.criteria)
+            .and.equals('c', 'd')
+            .build()
+    ).toEqual([
+        [['a'], Operator.eq, 'b'],
+        Operator.and,
+        [['c'], Operator.eq, 'd']
+    ]);
+});
+
+it('should be able to use existing criteria with or', () => {
+    const request = {
+        criteria: [['a'], Operator.eq, 'b']
+    };
+
+    expect(
+        Builder(request.criteria)
+            .or.equals('c', 'd')
+            .build()
+    ).toEqual([
+        [['a'], Operator.eq, 'b'],
+        Operator.or,
+        [['c'], Operator.eq, 'd']
+    ]);
+});
+
+it('should be able to use existing criteria with xor', () => {
+    const request = {
+        criteria: [['a'], Operator.eq, 'b']
+    };
+
+    expect(
+        Builder(request.criteria)
+            .xor.equals('c', 'd')
+            .build()
+    ).toEqual([
+        [['a'], Operator.eq, 'b'],
+        Operator.xor,
+        [['c'], Operator.eq, 'd']
+    ]);
+});
