f633df6051dda1dbae4c5f9f48f68b677c9a89a9
volkanceylan <volkanceylan@gmail.com>
2023-03-05 14:11:23 +0300

    make MVC also support file scoped namespaces

--- src/Serenity.Net.CodeGenerator/Commands/MvcCommand.cs
+++ src/Serenity.Net.CodeGenerator/Commands/MvcCommand.cs
@@ -29,9 +29,9 @@ public class MvcCommand : BaseFileSystemCommand
         };
 
         var rootDir = projectDir + System.IO.Path.DirectorySeparatorChar;
-        var searchViewPaths = (config.MVC.SearchViewPaths ?? 
-            new string[] { 
-                "Modules/", 
+        var searchViewPaths = (config.MVC.SearchViewPaths ??
+            new string[] {
+                "Modules/",
                 "Views/",
                 fileSystem.GetFileNameWithoutExtension(csproj) + "/",
                 "Areas/" + fileSystem.GetFileNameWithoutExtension(csproj) + "/"
@@ -64,87 +64,107 @@ public class MvcCommand : BaseFileSystemCommand
 
         files = files.OrderBy(x => getName(x));
 
-        StringBuilder sb = new();
-        sb.AppendLine("");
-        sb.Append("namespace ");
-        if (config.MVC.UseRootNamespace == true ||
+        var cw = new CodeWriter()
+        {
+            FileScopedNamespaces = config.FileScopedNamespaces == true,
+            IsCSharp = true
+        };
+        cw.AppendLine("");
+        var ns = (config.MVC.UseRootNamespace == true ||
             (config.MVC.UseRootNamespace == null &&
-             fileSystem.ReadAllText(csproj).Contains("Sdk=\"Microsoft.NET.Sdk.Razor\"", StringComparison.CurrentCulture)))
+             fileSystem.ReadAllText(csproj).Contains("Sdk=\"Microsoft.NET.Sdk.Razor\"", StringComparison.CurrentCulture))) ?
+             config.GetRootNamespaceFor(fileSystem, csproj) + ".MVC" : "MVC";
+
+        cw.InNamespace(ns, () =>
         {
-            sb.Append(config.GetRootNamespaceFor(fileSystem, csproj));
-            sb.Append('.');
-        }
-        sb.AppendLine("MVC");
-        sb.AppendLine("{");
-        sb.AppendLine("    public static class Views");
-        sb.AppendLine("    {");
-
-        var last = Array.Empty<string>();
-        var processed = new HashSet<string>();
-
-        foreach (var file in files)
-        {
-            var path = file[rootDir.Length..];
-            var name = getName(file);
-            if (name.StartsWith("App_Code/", StringComparison.OrdinalIgnoreCase) ||
-                name.EndsWith("_ViewStart", StringComparison.OrdinalIgnoreCase) ||
-                name.EndsWith("_ViewImports", StringComparison.OrdinalIgnoreCase) ||
-                processed.Contains(name))
-                continue;
-
-            processed.Add(name);
-            var parts = name.Split(new char[] { '/' });
-
-            if (parts.Length == 0)
-                continue;
-
-            for (var i = last.Length; i > parts.Length; i--)
+            cw.IndentedLine("public static class Views");
+            cw.InBrace(() =>
             {
-                var close = (new string(' ', (i * 4)) + "}");
-                sb.AppendLine(close);
-            }
+                var last = Array.Empty<string>();
+                var processed = new HashSet<string>();
 
-            var x = Math.Min(last.Length, parts.Length) - 2;
-            while (x >= 0 && last[x] != parts[x])
-            {
-                var close = (new string(' ', ((x + 2) * 4)) + "}");
-                x--;
-                sb.AppendLine(close);
-                sb.AppendLine();
-            }
+                foreach (var file in files)
+                {
+                    var path = file[rootDir.Length..];
+                    var name = getName(file);
+                    if (name.StartsWith("App_Code/", StringComparison.OrdinalIgnoreCase) ||
+                        name.EndsWith("_ViewStart", StringComparison.OrdinalIgnoreCase) ||
+                        name.EndsWith("_ViewImports", StringComparison.OrdinalIgnoreCase) ||
+                        processed.Contains(name))
+                        continue;
 
-            for (var i = Math.Max(x + 1, 0); i < parts.Length - 1; i++)
-            {
-                var indent = new string(' ', (i + 2) * 4);
-                var u = parts[i];
-                if (i > 0 && parts[i - 1] == u ||
-                    i == 0 && parts[i] == "Views")
-                    u += "_";
-                sb.AppendLine(indent + "public static class " + u.Replace(".", "_"));
-                sb.AppendLine(indent + "{");
-            }
+                    processed.Add(name);
+                    var parts = name.Split(new char[] { '/' });
 
-            var n = parts[^1].Replace(".", "_", StringComparison.Ordinal);
-            if (parts.Length - 1 > 0 && parts[^2] == n)
-                n += "_";
+                    if (parts.Length == 0)
+                        continue;
 
-            sb.Append(new string(' ', (parts.Length + 1) * 4));
-            sb.AppendLine("public const string " + n + " = \"~/" + path.Replace(@"\", "/", StringComparison.Ordinal) + "\";");
+                    for (var i = last.Length; i > parts.Length; i--)
+                    {
+                        var close = (new string(' ', (i - 2) * 4) + "}");
+                        cw.IndentedLine(close);
+                    }
 
-            last = parts;
-        }
+                    var x = Math.Min(last.Length, parts.Length) - 2;
+                    while (x >= 0 && last[x] != parts[x])
+                    {
+                        var close = (new string(' ', x * 4) + "}");
+                        x--;
+                        cw.IndentedLine(close);
+                        cw.AppendLine();
+                    }
 
-        for (var i = last.Length - 1; i > 0; i--)
-        {
-            sb.AppendLine(new string(' ', ((i + 1) * 4)) + "}" + Environment.NewLine);
-        }
+                    for (var i = Math.Max(x + 1, 0); i < parts.Length - 1; i++)
+                    {
+                        var indent = new string(' ', i * 4);
+                        var u = parts[i];
+                        if (i > 0 && parts[i - 1] == u ||
+                            i == 0 && parts[i] == "Views")
+                            u += "_";
+                        cw.IndentedLine(indent + "public static class " + u.Replace(".", "_"));
+                        cw.IndentedLine(indent + "{");
+                    }
 
-        sb.AppendLine("    }");
-        sb.AppendLine("}");
+                    var n = parts[^1].Replace(".", "_", StringComparison.Ordinal);
+                    if (parts.Length - 1 > 0 && parts[^2] == n)
+                        n += "_";
+
+                    cw.Indented(new string(' ', (parts.Length - 1) * 4));
+                    cw.AppendLine("public const string " + n + " = \"~/" + path.Replace(@"\", "/", StringComparison.Ordinal) + "\";");
+
+                    last = parts;
+                }
+
+                for (var i = last.Length - 1; i > 0; i--)
+                {
+                    cw.IndentedLine(new string(' ', (i - 1) * 4) + "}" + Environment.NewLine);
+                }
+
+                TrimEnd(cw.Builder);
+                cw.AppendLine();
+            });
+
+        });
 
         MultipleOutputHelper.WriteFiles(fileSystem, outDir, new[]
         {
-            ("MVC.cs", sb.ToString())
+            ("MVC.cs", cw.ToString())
         }, deleteExtraPattern: null, endOfLine: config.EndOfLine);
+    }
+
+    private static StringBuilder TrimEnd(StringBuilder sb)
+    {
+        if (sb == null || sb.Length == 0) return sb;
+
+        int i = sb.Length - 1;
+
+        for (; i >= 0; i--)
+            if (!char.IsWhiteSpace(sb[i]))
+                break;
+
+        if (i < sb.Length - 1)
+            sb.Length = i + 1;
+
+        return sb;
     }
 }
