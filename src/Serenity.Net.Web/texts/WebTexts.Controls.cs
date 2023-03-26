1f650662bb366819543cfe1468f5f95bd96eac95
volkanceylan <volkanceylan@gmail.com>
2023-03-26 13:38:15 +0300

    merge small files into one

--- src/Serenity.Net.Web/texts/WebTexts.Controls.cs
+++ src/Serenity.Net.Web/texts/WebTexts.Controls.cs
@@ -5,5 +5,97 @@ internal static partial class WebTexts
 {
     public static partial class Controls
     {
+        public static class CheckTreeEditor
+        {
+            public static LocalText SelectAll = "Select All";
+        }
+
+        public static class ColumnPickerDialog
+        {
+            public static LocalText HiddenColumns = "Hidden Columns";
+            public static LocalText HideHint = "hide";
+            public static LocalText RestoreDefaults = "Restore Defaults";
+            public static LocalText ShowHint = "show";
+            public static LocalText Title = "Column Picker";
+            public static LocalText VisibleColumns = "Visible Columns";
+        }
+
+        public static class DateTimeEditor
+        {
+            public static LocalText SetToNow = "set to now";
+        }
+
+        public static class DataGrid
+        {
+            public static LocalText NewButton = "New";
+        }
+
+        public static class EntityDialog
+        {
+            public static LocalText DeleteConfirmation = "Delete record?";
+            public static LocalText UndeleteButton = "Undelete";
+            public static LocalText UndeleteConfirmation = "Undelete record?";
+            public static LocalText CloneButton = "Clone";
+            public static LocalText SaveSuccessMessage = "Save success";
+            public static LocalText SaveButton = "Save";
+            public static LocalText UpdateButton = "Update";
+            public static LocalText ApplyChangesButton = "Apply Changes";
+            public static LocalText DeleteButton = "Delete";
+            public static LocalText EditButton = "Edit";
+            public static LocalText LocalizationButton = "Localization";
+            public static LocalText LocalizationBack = "Back to Form";
+            public static LocalText LocalizationConfirmation = "Save changes to translations?";
+            public static LocalText NewRecordTitle = "New {0}";
+            public static LocalText EditRecordTitle = "Edit {0}{1}";
+            public static LocalText ViewRecordTitle = "View {0}{1}";
+        }
+
+        public static class EntityGrid
+        {
+            public static LocalText NewButton = "New {0}";
+            public static LocalText RefreshButton = "Refresh";
+            public static LocalText IncludeDeletedToggle = "display inactive records";
+        }
+
+        public static class ImageUpload
+        {
+            public static LocalText AddFileButton = "Select File";
+            public static LocalText DeleteButtonHint = "Remove";
+            public static LocalText ColorboxCurrent = "image {current} / {total}";
+            public static LocalText ColorboxPrior = "prior";
+            public static LocalText ColorboxNext = "next";
+            public static LocalText ColorboxClose = "close";
+        }
+
+        public static class Pager
+        {
+            public static LocalText Page = "Page";
+            public static LocalText PageStatus = "Showing {from} to {to} of {total} total records";
+            public static LocalText NoRowStatus = "No records";
+            public static LocalText LoadingStatus = "Please wait, loading data...";
+            public static LocalText DefaultLoadError = "An error occurred while loading data!";
+        }
+
+        public static class PropertyGrid
+        {
+            public static LocalText DefaultCategory = "Properties";
+            public static LocalText RequiredHint = "this field is required";
+        }
+
+        public static class QuickSearch
+        {
+            public static LocalText Placeholder = "search...";
+            public static LocalText Hint = "enter the text to search for...";
+            public static LocalText FieldSelection = "select the field to search on";
+        }
+
+        public static class SelectEditor
+        {
+            public static LocalText EmptyItemText = "--select--";
+            public static LocalText InplaceAdd = "Define New";
+            public static LocalText InplaceEdit = "Edit";
+            public static LocalText ClickToDefine = "*** Click to define a new one ***";
+            public static LocalText NoResultsClickToDefine = "*** No results. Click to define a new one ***";
+        }
     }
 }
