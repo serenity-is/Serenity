4c8808b8a3c480a3339e5b56d588d9adfd99b65c
volkanceylan <volkanceylan@gmail.com>
2023-04-04 22:19:37 +0300

    ignore tests from coverage report

--- tests/test-coverage.cmd
+++ tests/test-coverage.cmd
@@ -8,6 +8,6 @@ FOR /d /r . %%d IN ("TestResults") DO @IF EXIST "%%d" rd /s /q "%%d"
 dotnet test --collect:"XPlat Code Coverage;Format=json,cobertura" ..\src\Serenity.Net.sln
 
 dotnet tool restore
-dotnet reportgenerator -reports:**/coverage.cobertura.xml -targetdir:./TestResults/coverage
+dotnet reportgenerator -reports:**/coverage.cobertura.xml -targetdir:./TestResults/coverage -assemblyfilters:-*.Tests;-*.Test
 
 start ./TestResults/coverage/index.html
