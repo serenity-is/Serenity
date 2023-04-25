bbf287ce7827656a58a4a25368ca91d6ad1ff9a8
volkanceylan <volkanceylan@gmail.com>
2023-04-25 13:25:33 +0300

    fix android check

--- src/Serenity.Assets/wwwroot/Scripts/jquery.maskedinput.js
+++ src/Serenity.Assets/wwwroot/Scripts/jquery.maskedinput.js
@@ -116,7 +116,7 @@
                         if (!(e.ctrlKey || e.altKey || e.metaKey || 32 > k) && k && 13 !== k) {
                             if (pos.end - pos.begin !== 0 && (clearBuffer(pos.begin, pos.end), shiftL(pos.begin, pos.end - 1)), 
                             p = seekNext(pos.begin - 1), len > p && (c = String.fromCharCode(k), tests[p].test(c))) {
-                                if (shiftR(p), buffer[p] = c, writeBuffer(), next = seekNext(p), android) {
+                                if (shiftR(p), buffer[p] = c, writeBuffer(), next = seekNext(p), /android/.test(navigator.userAgent)) {
                                     var proxy = function() {
                                         $.proxy($.fn.caret, input, next)();
                                     };
