4d682526d03860841ea15adfdc2384b38cf1b697
volkanceylan <volkanceylan@gmail.com>
2023-05-28 00:32:47 +0300

    add comments for errorhandling

--- packages/corelib/src/q/errorhandling.ts
+++ packages/corelib/src/q/errorhandling.ts
@@ -5,29 +5,26 @@ import { ServiceError } from "./servicetypes";
 
 export namespace ErrorHandling {
 
+    /**
+     * Shows a service error as an alert dialog. If the error
+     * is null, has no message or code, it shows "??ERROR??".
+     */
     export function showServiceError(error: ServiceError) {
-        let msg: any;
-        if (error == null) {
-            msg = '??ERROR??';
-        }
-        else {
-            msg = error.Message;
-            if (msg == null) {
-                msg = error.Code;
-            }
-        }
-
-        alertDialog(msg);
+        alertDialog(error?.Message ?? error.Code ?? "??ERROR??");
     }
 
+    /**
+     * Runtime error handler that shows a runtime error as a notification
+     * by default only in development mode (@see isDevelopmentMode)
+     * This function is assigned as window.onerror handler in 
+     * ScriptInit.ts for Serenity applications so that developers
+     * can notice an error without having to check the browser console.
+     */
     export function runtimeErrorHandler(message: string, filename?: string,
         lineno?: number, colno?: number, error?: Error) {
         try {
-            var host = (window.location.host || "").toLowerCase();
-            if (host.indexOf("localhost") < 0 &&
-                host.indexOf("127.0.0.1") < 0)
+            if (!isDevelopmentMode())
                 return;
-
             var errorInfo = JSON.stringify(error || {});
 
             message =
@@ -45,9 +42,26 @@ export namespace ErrorHandling {
                 }
                 catch {
                 }
-            });
+            }, 0);
         }
         catch {
         }
+    }
+
+    /** 
+     * Determines if the current environment is development mode.
+     * The runtimeErrorHandler (window.onerror) shows error notifications only
+     * when this function returns true. The default implementation considers 
+     * the environment as development mode if the host is localhost, 127.0.0.1, ::1,
+     * or a domain name that ends with .local/.localhost.
+     * @returns true if the current environment is development mode, false otherwise. 
+     */
+    export function isDevelopmentMode() {
+        var host = (window.location.host ?? "").toLowerCase();
+        return (host != "localhost" &&
+            host != "127.0.0.1" &&
+            host != "::1" &&
+            !host.endsWith(".local") &&
+            !host.endsWith(".localhost"));
     }
 }
