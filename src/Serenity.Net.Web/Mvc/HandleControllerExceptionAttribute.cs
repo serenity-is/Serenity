d4c953ae4dd3f5c29fb844c75065aa99e8fabf0f
osmanaslancan <osmanaslancan24@gmail.com>
2023-07-25 15:03:12 +0300

    add HandleControllerExceptionAttribute

---
+++ src/Serenity.Net.Web/Mvc/HandleControllerExceptionAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Serenity.Services;
+
+/// <summary>
+/// An exception filter attribute to handle controller exceptions and return as view.
+/// </summary>
+public class HandleControllerExceptionAttribute : ExceptionFilterAttribute
+{
+    /// <inheritdoc/>
+    public override void OnException(ExceptionContext context)
+    {
+        context.ExceptionHandled = true;
+        var result = context.Exception.ConvertToResponse<ServiceResponse>(context.HttpContext);
+        context.Result = new ViewResult()
+        {
+            ViewName = "~/Views/Errors/ValidationError.cshtml",
+            ViewData = new ViewDataDictionary<ValidationError>(new EmptyModelMetadataProvider(), context.ModelState)
+            {
+                Model = new ValidationError(result.Error.Code, result.Error.Message),
+            }
+        };
+    }
+}
