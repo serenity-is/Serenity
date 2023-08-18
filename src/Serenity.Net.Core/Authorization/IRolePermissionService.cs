88ed33f983ec1609f5bbf010dc8df418c46a13ce
Osman Aslancan <osmanaslancan24@gmail.com>
2023-08-18 15:37:48 +0300

    Role permission service (#6940)
    * add IRolePermissionService
    
    * update role permission service

---
+++ src/Serenity.Net.Core/Authorization/IRolePermissionService.cs
@@ -0,0 +1,15 @@
+namespace Serenity.Net.Core.Authorization;
+
+/// <summary>
+/// Role Permission Service abstraction 
+/// </summary>
+public interface IRolePermissionService
+{
+    /// <summary>
+    /// Return if given role has given permission.
+    /// </summary>
+    /// <param name="role">Role Key or Name</param>
+    /// <param name="permission">Permission Key</param>
+    /// <returns></returns>
+    bool HasPermission(string role, string permission);
+}
