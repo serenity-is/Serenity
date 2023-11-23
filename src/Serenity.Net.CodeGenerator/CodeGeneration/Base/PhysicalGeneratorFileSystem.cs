22c59d02c18e1a543c4d7c7a6b1f17671a4ab7fa
volkanceylan <volkanceylan@gmail.com>
2023-11-23 19:16:13 +0300

    remove unused function

--- src/Serenity.Net.CodeGenerator/CodeGeneration/Base/PhysicalGeneratorFileSystem.cs
+++ src/Serenity.Net.CodeGenerator/CodeGeneration/Base/PhysicalGeneratorFileSystem.cs
@@ -2,10 +2,4 @@ namespace Serenity.CodeGeneration;
 
 public class PhysicalGeneratorFileSystem : PhysicalFileSystem
 {
-    public DateTime GetLastWriteTime(string path)
-    {
-        return System.IO.File.GetLastWriteTime(path);
-    }
-
-
 }
