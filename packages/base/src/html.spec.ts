c70de49bab1345ccfd49ed8acbf5c002b186e6dc
volkanceylan <volkanceylan@gmail.com>
2023-11-30 18:59:32 +0300

    Add tests for toggleClass function

--- packages/base/src/html.spec.ts
+++ packages/base/src/html.spec.ts
@@ -1,4 +1,4 @@
-﻿import { htmlEncode } from "./html";
+﻿import { htmlEncode, toggleClass } from "./html";
 
 describe("htmlEncode", () => {
     it("encodes html", () => {
@@ -17,4 +17,78 @@ describe("htmlEncode", () => {
         expect(htmlEncode(true)).toBe("true");
         expect(htmlEncode(false)).toBe("false");
     });
+});
+
+describe("toggleClass", () => {
+    it("does nothing if class is null or empty", () => {
+        var el = document.createElement("div");
+        el.setAttribute("class", "test1 test2");
+
+        toggleClass(el, null, true);
+        expect(el.getAttribute("class")).toBe("test1 test2");
+
+        toggleClass(el, undefined, false);
+        expect(el.getAttribute("class")).toBe("test1 test2");
+
+        toggleClass(el, '', false);
+        expect(el.getAttribute("class")).toBe("test1 test2");
+    });
+
+    it("can toggle single class", () => {
+        var el = document.createElement("div");
+        el.setAttribute("class", "test1 test2");
+
+        toggleClass(el, "test1");
+        expect(el.getAttribute("class")).toBe("test2");
+
+        toggleClass(el, "test3");
+        expect(el.getAttribute("class")).toBe("test2 test3");
+
+        toggleClass(el, "test3");
+        expect(el.getAttribute("class")).toBe("test2");
+    });
+
+    it("can toggle multiple classes", () => {
+        var el = document.createElement("div");
+        el.setAttribute("class", "test1 test2");
+
+        toggleClass(el, "test1 test2");
+        expect(el.getAttribute("class")).toBe("");
+
+        toggleClass(el, "test3 test4");
+        expect(el.getAttribute("class")).toBe("test3 test4");
+
+        toggleClass(el, "test5 test3 test6");
+        expect(el.getAttribute("class")).toBe("test4 test5 test6");
+    });
+
+    it("adds classes if third parameter is true", () => {
+        var el = document.createElement("div");
+        el.setAttribute("class", "test1 test2");
+
+        toggleClass(el, "test1 test2", true);
+        expect(el.getAttribute("class")).toBe("test1 test2");
+
+        toggleClass(el, "test3 test4", true);
+        expect(el.getAttribute("class")).toBe("test1 test2 test3 test4");
+
+        toggleClass(el, "test5 test3 test6", true);
+        expect(el.getAttribute("class")).toBe("test1 test2 test3 test4 test5 test6");
+    });
+
+    it("removes classes if third parameter is false", () => {
+        var el = document.createElement("div");
+        el.setAttribute("class", "test1 test2");
+
+        toggleClass(el, "test1 test2", false);
+        expect(el.getAttribute("class")).toBe("");
+    });
+
+    it("ignores whitespace", () => {
+        var el = document.createElement("div");
+        el.setAttribute("class", "test1 test2");
+
+        toggleClass(el, "test1      test2", false);
+        expect(el.getAttribute("class")).toBe("");
+    });       
 });
