4f11657f632161e03ee2cb1ae93061bc8136f3a8
volkanceylan <volkanceylan@gmail.com>
2023-12-05 15:05:13 +0300

    Add tests for notify methods

--- packages/base/src/notify.ts
+++ packages/base/src/notify.ts
@@ -9,17 +9,13 @@ export let defaultNotifyOptions: ToastrOptions = {
     positionClass: 'position-toast toast-top-full-width'
 }
 
-export function positionToastContainer(create: boolean, options?: ToastrOptions) {
-    if (typeof toastr === 'undefined') {
-        return;
-    }
-
-    let container = toastr.getContainer(options, create);
-    if (!container || !container.classList.contains('position-toast'))
+export function positionToastContainer(options?: ToastrOptions) {
+    let container = toastr.getContainer(options, true);
+    if (!container || !container.classList.contains('position-toast') || typeof document === "undefined" || !document.body)
         return;
 
-    let dialogs = Array.from(typeof document !== "undefined" ? (document?.body?.children ?? []) : []);
-    let dialogIndex =  dialogs.findIndex(x => x.matches('.ui-dialog, .modal.in, .modal.show') && !x.matches('[style*="display:none"], [style*="display: none"] *, .hidden'));
+    let dialogs = Array.from(document.body.children);
+    let dialogIndex =  dialogs.findIndex(x => x.matches('.ui-dialog, .modal.in, .modal.show') && !x.matches('[style*="display:none"], [style*="display: none"], .hidden'));
     let dialog = dialogs[dialogIndex];
     if (dialog) {
         const { top, left, right } = dialog.getBoundingClientRect();
@@ -27,7 +23,7 @@ export function positionToastContainer(create: boolean, options?: ToastrOptions)
         container.style.position = 'absolute';
         container.style.top = top + 28 + 'px';
         container.style.left = left + 6 + 'px';
-        container.style.width = right - (left - 12) + 'px';
+        container.style.width = Math.max(((right - left) - 12), 150) + 'px';
     }
     else if (container.classList.contains('positioned-toast')) {
         container.classList.remove('positioned-toast');
@@ -40,7 +36,7 @@ export function positionToastContainer(create: boolean, options?: ToastrOptions)
 
 function getToastrOptions(options: ToastrOptions) {
     options = Object.assign(Object.assign({}, defaultNotifyOptions), options);
-    positionToastContainer(true, options);
+    positionToastContainer(options);
     return options;
 }
 
@@ -48,18 +44,18 @@ function showToast(type: "error" | "info" | "success" | "warning", message: stri
     return toastr[type](message, title, getToastrOptions(options));
 }
 
-export function notifyWarning(message: string, title?: string, options?: ToastrOptions): void {
-    showToast('warning', message, title, getToastrOptions(options));
-}
-
-export function notifySuccess(message: string, title?: string, options?: ToastrOptions): void {
-    showToast('success', message, title, getToastrOptions(options));
+export function notifyError(message: string, title?: string, options?: ToastrOptions): void {
+    showToast('error', message, title, options);
 }
 
 export function notifyInfo(message: string, title?: string, options?: ToastrOptions): void {
-    showToast('info', message, title, getToastrOptions(options));
+    showToast('info', message, title, options);
 }
 
-export function notifyError(message: string, title?: string, options?: ToastrOptions): void {
-    showToast('error', message, title, getToastrOptions(options));
+export function notifySuccess(message: string, title?: string, options?: ToastrOptions): void {
+    showToast('success', message, title, options);
+}
+
+export function notifyWarning(message: string, title?: string, options?: ToastrOptions): void {
+    showToast('warning', message, title, options);
 }
