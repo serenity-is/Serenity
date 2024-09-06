f9ee2a854bb175022fb95ed21f4839aa98a0327d
volkanceylan <volkanceylan@gmail.com>
2024-09-06 16:12:55 +0300

    Convert emailaddresseditor to jsx

--- packages/corelib/src/ui/editors/emailaddresseditor.tsx
+++ packages/corelib/src/ui/editors/emailaddresseditor.tsx
@@ -1,12 +1,11 @@
-﻿import { Fluent } from "../../base";
-import { Decorators } from "../../types/decorators";
+﻿import { Decorators } from "../../types/decorators";
 import { EditorProps } from "./editorwidget";
 import { StringEditor } from "./stringeditor";
 
 @Decorators.registerEditor('Serenity.EmailAddressEditor')
 export class EmailAddressEditor<P = {}> extends StringEditor<P> {
 
-    static override createDefaultElement() { return Fluent("input").attr("type", "email").getNode(); }
+    static override createDefaultElement() { return <input type="email" /> as HTMLInputElement; }
 
     constructor(props: EditorProps<P>) {
         super(props);
