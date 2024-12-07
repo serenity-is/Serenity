f53f9dcbb64fef203b689f661d6297b240d205c6
volkanceylan <volkanceylan@gmail.com>
2024-12-07 17:35:04 +0300

    Remove reactpatch as we no longer use global react object

--- packages/corelib/src/patch/index.ts
+++ packages/corelib/src/patch/index.ts
@@ -1,9 +1,5 @@
 ﻿import { Fluent } from "../base";
 import { jQueryPatch } from "./jquerypatch";
-import { reactPatch } from "./reactpatch";
 
 !jQueryPatch() && Fluent.ready(jQueryPatch);
-reactPatch();
-
 export { jQueryPatch } from "./jquerypatch";
-export { reactPatch } from "./reactpatch";
