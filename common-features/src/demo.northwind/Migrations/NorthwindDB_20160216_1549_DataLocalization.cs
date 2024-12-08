bef5c5395c72c67d6cb52b4ff9a875f405668326
volkanceylan <volkanceylan@gmail.com>
2024-12-08 14:26:30 +0300

    Modify initial migration so it creates a string field

--- common-features/src/demo.northwind/Migrations/NorthwindDB_20160216_1549_DataLocalization.cs
+++ common-features/src/demo.northwind/Migrations/NorthwindDB_20160216_1549_DataLocalization.cs
@@ -10,14 +10,14 @@ public class NorthwindDB_20160216_1549_DataLocalization : AutoReversingMigration
         Create.Table("CategoryLang")
             .WithColumn("ID").AsInt32().IdentityKey(this)
             .WithColumn("CategoryID").AsInt32().NotNullable()
-            .WithColumn("LanguageID").AsInt32().NotNullable()
+            .WithColumn("LanguageID").AsString(11).NotNullable()
             .WithColumn("CategoryName").AsString(15).Nullable()
             .WithColumn("Description").AsString(int.MaxValue).Nullable();
 
         Create.Table("ProductLang")
             .WithColumn("ID").AsInt32().IdentityKey(this)
             .WithColumn("ProductID").AsInt32().NotNullable()
-            .WithColumn("LanguageID").AsInt32().NotNullable()
+            .WithColumn("LanguageID").AsString(11).NotNullable()
             .WithColumn("ProductName").AsString(40).Nullable();
     }
 }
