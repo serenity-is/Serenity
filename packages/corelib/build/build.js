3da656b1b824fc324cf81d2126f992835bfd7958
volkanceylan <volkanceylan@gmail.com>
2024-12-26 13:58:11 +0300

    Update writeIfChanged again

--- packages/corelib/build/build.js
+++ packages/corelib/build/build.js
@@ -45,22 +45,26 @@ export function importAsGlobalsPlugin(mapping) {
     };
 }
 
-export function writeIfChanged() {
+function writeIfChanged() {
     return {
         name: "write-if-changed",
         setup(build) {
             const write = build.initialOptions.write;
             build.initialOptions.write = false;
             build.onEnd(result => {
-                if (write === undefined || write) {
-                    result.outputFiles?.forEach(file => {
-                        if (!existsSync(file.path) ||
-                            readFileSync(file.path, 'utf8') !== file.text) {
-                            mkdirSync(dirname(file.path), { recursive: true });
-                            writeFileSync(file.path, file.text);
-                        }
-                    });
-                }
+                if (!(write === undefined || write))
+                    return;
+                result.outputFiles?.forEach(file => {
+                    if (existsSync(file.path)) {
+                        const old = readFileSync(file.path);
+                        if (old.equals(file.contents))
+                            return;
+                    }
+                    else {
+                        mkdirSync(dirname(file.path), { recursive: true });
+                    }
+                    writeFileSync(file.path, file.text);
+                });
             });
         }
     };
@@ -124,5 +128,4 @@ await esbuild.build({
     plugins: [importAsGlobalsPlugin({
         "@serenity-is/sleekgrid": "Slick"
     }), writeIfChanged()]
-
 });
