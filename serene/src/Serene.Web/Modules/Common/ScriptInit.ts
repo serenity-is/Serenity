ec752c85b458dcaf7091635abc67252c03b6baec
volkanceylan <volkanceylan@gmail.com>
2025-07-14 18:19:15 +0300

    **`[Breaking Change]`** Removing jquery-ui.js from Serenity.Assets. If you still need this legacy script (e.g. in appsettings.bundles.json) you may install it via libman / npm. Flatpickr is now also used in Serene inplace of jquery ui datepicker.

--- serene/src/Serene.Web/Modules/Common/ScriptInit.ts
+++ serene/src/Serene.Web/Modules/Common/ScriptInit.ts
@@ -1,11 +1,21 @@
 import { Config, ErrorHandling, HtmlContentEditor, TranslationConfig } from "@serenity-is/corelib";
 import { gridDefaults } from "@serenity-is/sleekgrid";
 import { getLanguageList } from "./Helpers/LanguageList";
+import flatpickr from "flatpickr";
+import "flatpickr/dist/l10n";
 
 Config.rootNamespaces.push('Serene');
 TranslationConfig.getLanguageList = getLanguageList;
 HtmlContentEditor.CKEditorBasePath = "~/Serenity.Assets/Scripts/ckeditor/";
 gridDefaults.useCssVars = false;
+
+let culture = (document.documentElement?.lang || 'en').toLowerCase();
+if (flatpickr.l10ns[culture]) {
+    flatpickr.localize(flatpickr.l10ns[culture]);
+} else {
+    culture = culture.split('-')[0];
+    flatpickr.l10ns[culture] && flatpickr.localize(flatpickr.l10ns[culture]);
+}
 
 window.onerror = ErrorHandling.runtimeErrorHandler;
 window.addEventListener('unhandledrejection', ErrorHandling.unhandledRejectionHandler);
