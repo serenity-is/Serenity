65a3ce92e072616212f6ffb2028a90fa4e247184
volkanceylan <volkanceylan@gmail.com>
2025-07-14 13:45:38 +0300

    **`[Breaking Change]`** Removing jquery.colorbox from Serenity.Assets. If you still need this legacy script (e.g. in appsettings.bundles.json) you may install it via libman.json.

--- serene/src/Serene.Web/Modules/Common/ScriptInit.ts
+++ serene/src/Serene.Web/Modules/Common/ScriptInit.ts
@@ -1,4 +1,4 @@
-import { Config, ErrorHandling, HtmlContentEditor, TranslationConfig, getjQuery } from "@serenity-is/corelib";
+import { Config, ErrorHandling, HtmlContentEditor, TranslationConfig } from "@serenity-is/corelib";
 import { gridDefaults } from "@serenity-is/sleekgrid";
 import { getLanguageList } from "./Helpers/LanguageList";
 
@@ -6,12 +6,6 @@ Config.rootNamespaces.push('Serene');
 TranslationConfig.getLanguageList = getLanguageList;
 HtmlContentEditor.CKEditorBasePath = "~/Serenity.Assets/Scripts/ckeditor/";
 gridDefaults.useCssVars = false;
-
-let $ = getjQuery();
-if ($?.fn?.colorbox) {
-    $.fn.colorbox.settings.maxWidth = "95%";
-    $.fn.colorbox.settings.maxHeight = "95%";
-}
 
 window.onerror = ErrorHandling.runtimeErrorHandler;
 window.addEventListener('unhandledrejection', ErrorHandling.unhandledRejectionHandler);
