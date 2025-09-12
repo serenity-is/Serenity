fb84b432a644ac26035749982f3539a10d194b97
volkanceylan <volkanceylan@gmail.com>
2025-09-12 20:34:37 +0300

    Sort Serene namespaces.ts

--- serene/src/Serene.Web/Modules/ServerTypes/Namespaces.ts
+++ serene/src/Serene.Web/Modules/ServerTypes/Namespaces.ts
@@ -1,4 +1,4 @@
-﻿export const MembershipNS: "Serene.Membership" = "Serene.Membership";
-export const nsMembership: "Serene.Membership." = "Serene.Membership.";
-export const AdministrationNS: "Serene.Administration" = "Serene.Administration";
+﻿export const AdministrationNS: "Serene.Administration" = "Serene.Administration";
 export const nsAdministration: "Serene.Administration." = "Serene.Administration.";
+export const MembershipNS: "Serene.Membership" = "Serene.Membership";
+export const nsMembership: "Serene.Membership." = "Serene.Membership.";
