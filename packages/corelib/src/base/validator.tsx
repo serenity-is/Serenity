1e2d0e86fea2c5c3f27c89d572c4fa816879e917
volkanceylan <volkanceylan@gmail.com>
2025-09-26 00:25:42 +0300

    Try adding a length check

--- packages/corelib/src/base/validator.tsx
+++ packages/corelib/src/base/validator.tsx
@@ -261,7 +261,7 @@ const EMAIL_INTERNATIONAL_REGEX = new RegExp("^((([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\
     "\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(\\\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|" +
     "[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([a-z]|\\d|" +
     "[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])" +
-    "([a-z\\d\\.\\-_~\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.)" +
+    "([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.)" +
     "+(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|" +
     "[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))$", "i");
 
@@ -481,8 +481,11 @@ export class Validator {
          */
         email: (value) => {
             if (!value || typeof value !== "string") {
-                return true;
+                return false;
             }
+
+            if (value.length > 1000)
+                return false;
 
             if (!Config.emailAllowOnlyAscii) {
                 return EMAIL_INTERNATIONAL_REGEX.test(value);
