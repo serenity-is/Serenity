85cd465f501dcfc2cfd14a13f7751ff352d21599
volkanceylan <volkanceylan@gmail.com>
2025-10-10 09:31:49 +0300

    Install DOMPurify in Serene and set it as default sanitizer

--- serene/src/Serene.Web/Modules/Common/ScriptInit.ts
+++ serene/src/Serene.Web/Modules/Common/ScriptInit.ts
@@ -3,10 +3,11 @@ import { gridDefaults } from "@serenity-is/sleekgrid";
 import flatpickr from "flatpickr";
 import "flatpickr/dist/l10n";
 import { getLanguageList } from "./Helpers/LanguageList";
+import DOMPurify from "dompurify";
 
 Config.rootNamespaces.push('Serene');
 TranslationConfig.getLanguageList = getLanguageList;
-gridDefaults.useCssVars = false;
+gridDefaults.sanitizer = (globalThis.DOMPurify = DOMPurify).sanitize;
 
 let culture = (document.documentElement?.lang || 'en').toLowerCase();
 if (flatpickr.l10ns[culture]) {
