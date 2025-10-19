c43bd82eb9ebd4f1679ba33d1d7423a31da81a18
volkanceylan <volkanceylan@gmail.com>
2025-10-19 17:31:15 +0300

    Dts file, use dts-bundle-generator from workspace

--- packages/sleekdom/dist/index.d.ts
+++ packages/sleekdom/dist/index.d.ts
@@ -31,7 +31,7 @@ export type Ref<T> = RefCallback<T> | RefObject<T> | null;
 export interface SignalLike<T> {
 	value: T;
 	peek(): T;
-	subscribe(fn: (value: T) => void): ((() => void) | null | undefined | void);
+	subscribe(fn: (value: T) => void): ((() => void) | null);
 }
 export type SignalOrValue<T> = T | SignalLike<T>;
 export type ComponentChild = string | number | Iterable<ComponentChild> | Array<ComponentChild> | {
@@ -2851,6 +2851,32 @@ export declare class Component<T = any> {
 	};
 	render(): JSXElement | null;
 }
+/**
+ * Called when an element is getting disposed. This will invoke all registered disposing listeners for the element.
+ * This is normally called automatically when `disposing` event is dispatched on the element.
+ * It may also be called internally by Fluent events while disposing an element by `remove`, or `empty` (for descendants).
+ * You should not normally call this method directly.
+ * @param target The element that is being disposed.
+ */
+export declare const onElementDisposing: (target: EventTarget) => void;
+/**
+ * Adds a disposing listener to an element. Note that the listener itself is not added as an event listener,
+ * but will be called when the `disposing` event is dispatched on the element, along with other disposing listeners.
+ * @param target The element to add the listener to.
+ * @param handler The disposing listener to add.
+ * @returns The element that the listener was added to.
+ */
+export declare function addDisposingListener<T extends EventTarget>(target: T, handler: () => void): T;
+/**
+ * Removes a disposing listener from an element. Note that this does not remove an event listener from the element,
+ * but removes the listener from the list of disposing listeners that will be called when the `disposing` event
+ * is dispatched on the element. If no more disposing listeners remain, the `disposing` event listener is also
+ * removed from the element.
+ * @param target The element to remove the listener from.
+ * @param handler The disposing listener to remove.
+ * @returns The element that the listener was removed from.
+ */
+export declare function removeDisposingListener<T extends EventTarget>(target: T, handler: () => void): T;
 export declare function useClassList(initialValue?: ClassNames): BasicClassList;
 export declare function useText(initialValue?: string): readonly [
 	Text,
