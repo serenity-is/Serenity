ac694bde2c862ec46cea24df4d3e467bb32aee09
volkanceylan <volkanceylan@gmail.com>
2025-11-18 17:38:04 +0300

    Update readme for packages

--- packages/corelib/README.md
+++ packages/corelib/README.md
@@ -1,4 +1,4 @@
-# Serenity Core Library (Classic UI)
+# Serenity Core Library
 
 This is the package containing core TypeScript classes and functions used in Serenity applications.
 
@@ -14,6 +14,6 @@ in your projects created from `Serene` or `StartSharp` template:
 }
 ```
 
-If you have "./node_modules/.dotnet/serenity.corelib" in the version value, it means you are using this library directly via the Serenity.Corelib NuGet package reference in your project file instead of NPM. This is recommended to avoid version inconsistencies.
+If you have `"./node_modules/.dotnet/serenity.corelib"` in the version value, it means you are using this library directly via the `Serenity.Corelib` NuGet package reference in your project file instead of NPM. This is recommended to avoid version inconsistencies.
 
 If you have a version number for this package in package.json, it should be equal or as close as possible to Serenity NuGet package versions in your project file.
--- packages/corelib/src/index.ts
+++ packages/corelib/src/index.ts
@@ -1,23 +1,22 @@
 ﻿/** 
- * ## Serenity Core Library
+ * ## Serenity Core Library (@serenity-is/corelib)
  * 
- * This is the package containing core TypeScript classes and functions used in Serenity applications. 
+ * This is the package containing core TypeScript classes and functions used in Serenity applications.
  * 
- * It should be installed by default in your projects created from `Serene` or `StartSharp` template:
+ * This should be installed by default in your projects created from `Serene` or `StartSharp` template:
  * 
  * ```json
  * {
  *   "dependencies": {
  *     // ...
- *     "@serenity-is/corelib": "latest"
+ *     "@serenity-is/corelib": "./node_modules/.dotnet/serenity.corelib"
  *   }
  * }
  * ```
  * 
- * The version number for this package should be equal or as close as possible to Serenity NuGet package versions in your project file.
+ * If you have `"./node_modules/.dotnet/serenity.corelib"` in the version value, it means you are using this library directly via the `Serenity.Corelib` NuGet package reference in your project file instead of NPM. This is recommended to avoid version inconsistencies.
  * 
- * > When using classic namespaces instead of the ESM modules, the types and functions in this module are directly available from the global `Serenity` and `Q` namespaces.
- * > e.g. `Serenity.EntityGrid`
+ * If you have a version number for this package in package.json, it should be equal or as close as possible to Serenity NuGet package versions in your project file.
  * @packageDocumentation
  */
 
