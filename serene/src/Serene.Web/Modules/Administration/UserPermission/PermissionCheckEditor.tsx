a23340794770b761d7ac57dfd65af4da3433e0be
Victor Tomaili <victor@serenity.is>
2025-12-12 20:29:14 +0300

    Removed unnecessary isDefaultPrevented condition in PermissionCheckEditor onClick event

--- serene/src/Serene.Web/Modules/Administration/UserPermission/PermissionCheckEditor.tsx
+++ serene/src/Serene.Web/Modules/Administration/UserPermission/PermissionCheckEditor.tsx
@@ -162,11 +162,10 @@ export class PermissionCheckEditor<P extends PermissionCheckEditorOptions = Perm
     protected override onClick(e: Event, row: number, cell: number): void {
         super.onClick(e, row, cell);
 
-        if (!Fluent.isDefaultPrevented(e))
-            SlickTreeHelper.toggleClick(e, row, cell, this.view, (x: any) => x.Key);
-
         if (Fluent.isDefaultPrevented(e))
             return;
+
+        SlickTreeHelper.toggleClick(e, row, cell, this.view, (x: any) => x.Key);
 
         const target = Fluent(e.target);
         let grant = target.hasClass('grant');
