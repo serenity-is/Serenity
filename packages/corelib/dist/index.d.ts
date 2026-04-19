f0c45f2f24a12718591b76c1646f48658e119e3f
volkanceylan <volkanceylan@gmail.com>
2026-04-19 20:34:01 +0300

    Update dts

--- packages/corelib/dist/index.d.ts
+++ packages/corelib/dist/index.d.ts
@@ -3992,7 +3992,7 @@ export declare class RemoteView<TItem = any> implements IRemoteView<TItem> {
 	getRows(): (TItem | Group<any> | GroupTotals<any>)[];
 	getLength(): number;
 	getItem(i: number): any;
-	getItemMetadata(row: number): ItemMetadata<TItem>;
+	getItemMetadata(row: number): ItemMetadata<any>;
 	private expandCollapseAllGroups;
 	collapseAllGroups(level?: number): void;
 	expandAllGroups(level?: number): void;
