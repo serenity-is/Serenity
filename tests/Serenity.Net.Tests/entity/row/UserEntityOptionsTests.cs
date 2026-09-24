e14c2611c6eb5d5e0d8932c9ccf06b8f8853bec0
volkanceylan <volkanceylan@gmail.com>
2026-09-24 18:19:16 +0300

    Add more tests for UserEntityOptions

--- tests/Serenity.Net.Tests/entity/row/UserEntityOptionsTests.cs
+++ tests/Serenity.Net.Tests/entity/row/UserEntityOptionsTests.cs
@@ -2,6 +2,89 @@ namespace Serenity.Data;
 
 public class UserEntityOptionsTests
 {
+    private class IntUserRow : Row<IntUserRow.RowFields>
+    {
+        public class RowFields : RowFieldsBase;
+
+        [IdProperty]
+        public int? Id { get; set; }
+    }
+
+    private class LongUserRow : Row<LongUserRow.RowFields>
+    {
+        public class RowFields : RowFieldsBase;
+
+        [IdProperty]
+        public long? Id { get; set; }
+    }
+
+    private class GuidUserRow : Row<GuidUserRow.RowFields>
+    {
+        public class RowFields : RowFieldsBase;
+
+        [IdProperty]
+        public Guid? Id { get; set; }
+    }
+
+    private class NoIdUserRow : Row<NoIdUserRow.RowFields>
+    {
+        public class RowFields : RowFieldsBase;
+    }
+
+    private class UnsupportedIdUserRow : Row<UnsupportedIdUserRow.RowFields>
+    {
+        public class RowFields : RowFieldsBase;
+
+        [IdProperty]
+        public decimal? Id { get; set; }
+    }
+
+    [Theory]
+    [InlineData(typeof(IntUserRow), typeof(Int32Field))]
+    [InlineData(typeof(LongUserRow), typeof(Int64Field))]
+    [InlineData(typeof(GuidUserRow), typeof(GuidField))]
+    [InlineData(typeof(OriginPropertyTests.ConfiguredUserRow), typeof(StringField))]
+    public void RowType_InfersIdFieldType(Type rowType, Type expectedFieldType)
+    {
+        var options = new UserEntityOptions { RowType = rowType };
+
+        Assert.Equal(expectedFieldType, options.IdFieldType);
+    }
+
+    [Fact]
+    public void RowType_InfersColumnMetadataAndTableName()
+    {
+        var options = new UserEntityOptions
+        {
+            RowType = typeof(OriginPropertyTests.ConfiguredUserRow)
+        };
+
+        Assert.Equal("UsersFromRow", options.TableName);
+        Assert.Equal("UserKey", options.IdColumnName);
+        Assert.Equal(40, options.IdColumnSize);
+    }
+
+    [Fact]
+    public void RowType_UsesDefaultsWhenMetadataIsMissing()
+    {
+        var options = new UserEntityOptions { RowType = typeof(IntUserRow) };
+
+        Assert.Equal("Users", options.TableName);
+        Assert.Equal("Id", options.IdColumnName);
+        Assert.Null(options.IdColumnSize);
+    }
+
+    [Theory]
+    [InlineData(typeof(string), "UserRowSettings.RowType must be a type that implements IRow.")]
+    [InlineData(typeof(NoIdUserRow), "UserRowSettings.RowType must have a property with the [IdProperty] attribute.")]
+    [InlineData(typeof(UnsupportedIdUserRow), "UserRowSettings.RowType must have an Id property of type int, long, Guid or string.")]
+    public void RowType_RejectsInvalidTypes(Type rowType, string expectedMessage)
+    {
+        var exception = Assert.Throws<ArgumentException>(() => new UserEntityOptions { RowType = rowType });
+
+        Assert.Equal(expectedMessage, exception.Message);
+    }
+
     [Fact]
     public void AssignFrom_CopiesAllSettings()
     {
